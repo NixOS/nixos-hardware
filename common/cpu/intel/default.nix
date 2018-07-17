f428f17fa09c77c7c4f3e8a79137da458c30f1b4
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-07-17 15:20:22 +0300

    common/cpu/intel: modesetting is the default

--- common/cpu/intel/default.nix
+++ common/cpu/intel/default.nix
@@ -2,8 +2,7 @@
 
 {
   hardware.cpu.intel.updateMicrocode = lib.mkDefault true;
-  services.xserver.videoDrivers = [ "modesetting" ];
-
+  
   hardware.opengl.extraPackages = with pkgs; [
     vaapiIntel
     vaapiVdpau
