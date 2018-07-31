34c04e153f4677a180282ac994bf8e5f1014a993
Ashley Gillman <gillmanash@gmail.com>
2018-07-31 12:00:40 +0100

    Remove canTouchEfiVariables (dangerous) and specify working kernelPackages and pusleaudio.

--- dell/e7240/default.nix
+++ dell/e7240/default.nix
@@ -7,5 +7,10 @@
   ];
 
   boot.loader.systemd-boot.enable = lib.mkDefault true;
-  boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
+
+  hardware.pulseaudio.enable = true;
+
+  # on current latest (4.17) have suspend issues
+  # https://bugzilla.redhat.com/show_bug.cgi?id=1597481
+  boot.kernelPackages = pkgs.linuxPackages_4_14;
 }
