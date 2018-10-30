3d337906f538d3fe29198c7d9a24753a89b44fe3
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-10-30 19:38:51 +0000

    common/pc: disable ath3k if non-free firmware is not available

--- common/pc/default.nix
+++ common/pc/default.nix
@@ -1,5 +1,9 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 {
+  boot.blacklistedKernelModules = lib.optionals (!config.hardware.enableRedistributableFirmware) [
+    "ath3k"
+  ];
+
   services.xserver.libinput.enable = lib.mkDefault true;
 }
