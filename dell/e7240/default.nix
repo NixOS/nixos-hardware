1f87fb7a6306d4aa78176469c1cfbc60f50c70b5
Ashley Gillman <gillmanash@gmail.com>
2019-02-20 14:52:29 +1000

    Remove opinionated configuration.

--- dell/e7240/default.nix
+++ dell/e7240/default.nix
@@ -5,12 +5,4 @@
     ../../../common/cpu/intel
     ../../../common/pc/laptop
   ];
-
-  boot.loader.systemd-boot.enable = lib.mkDefault true;
-
-  hardware.pulseaudio.enable = true;
-
-  # on current latest (4.17) have suspend issues
-  # https://bugzilla.redhat.com/show_bug.cgi?id=1597481
-  boot.kernelPackages = pkgs.linuxPackages_4_14;
 }
