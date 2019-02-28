ff48a287e01ae6fb133e90782a58e9072f5f04f7
ash lea <ashlea@protonmail.com>
2019-02-28 14:31:14 -0500

    t450s: fix trackpoint configuration at boot

--- lenovo/thinkpad/t450s/default.nix
+++ lenovo/thinkpad/t450s/default.nix
@@ -6,4 +6,15 @@
     ../../../common/pc/laptop/acpi_call.nix
     ../.
   ];
+
+  systemd.paths.trackpoint = {
+    pathConfig = {
+      PathExists = "/sys/devices/rmi4-00/rmi4-00.fn03/serio2";
+      Unit = "trackpoint.service";
+    };
+  };
+
+  systemd.services.trackpoint.script = ''
+    ${config.systemd.package}/bin/udevadm trigger --attr-match=name="${config.hardware.trackpoint.device}"
+  '';
 }
