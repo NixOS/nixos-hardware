16d6316bfcb8a83d01ac9cfaf921ee011f3ab0a3
Paul Myjavec <pauly@myjavec.com>
2020-05-12 14:19:59 +0900

    Some updates as per PR feedback.
    Ensure the change covers all bases, including making the terminal look
    good on HiDPI displays.

--- common/pc/display/hidpi.nix
+++ common/pc/display/hidpi.nix
@@ -1,13 +1,17 @@
-# Shared configuration which most (if not all) HiDPI setups should have.
-{ config, lib, pkgs, ... }:
+{ pkgs, lib, ... }: {
+  console.font = lib.mkDefault
+    "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
 
-{
+  # Needed when doing cryptsetup
+  console.earlySetup = lib.mkDefault true;
+  boot.loader.systemd-boot.consoleMode = lib.mkDefault "max";
+
   services.xserver.dpi = lib.mkDefault 196;
   fonts.fontconfig.dpi = lib.mkDefault 196;
+  environment.variables = lib.mkDefault {
+    GDK_SCALE = "2";
+    GDK_DPI_SCALE = "0.5";
+    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
 
-  # Fix sizes of GTK/GNOME ui elements
-  environment.variables = {
-    GDK_SCALE = lib.mkDefault "2";
-    GDK_DPI_SCALE= lib.mkDefault "0.5";
   };
 }
