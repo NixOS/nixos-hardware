cc72f59d5a405ee2e812944e5ebab06f0ed18475
bahaynes <haynesben@gmail.com>
2020-07-29 20:51:08 -0400

    Removed tlp configuration.
    Should be set correctly after https://github.com/NixOS/nixpkgs/pull/94064
    is merged.

--- dell/xps/15-9500/default.nix
+++ dell/xps/15-9500/default.nix
@@ -18,7 +18,4 @@ in
   # Thermald doesn't have a default config for the 9500 yet, the one in this repo
   # was generated with dptfxtract-static (https://github.com/intel/dptfxtract)
   services.thermald.configFile = lib.mkDefault thermald-conf;
-
-  # Set the tlp config to powersave explictly. TLP is enabled in common/pc/laptop.
-  services.tlp.extraConfig = lib.mkDefault "CPU_SCALING_GOVERNOR_ON_AC=powersave\nCPU_SCALING_GOVERNOR_ON_BAT=powersave";
 }
