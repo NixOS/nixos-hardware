b175484c5ad7379b00e90b97fb182bb9de07e19d
Vincent Cui <wenvincent.cui@gmail.com>
2020-09-23 20:08:23 +0200

    fix lenovo thinkpad e470

--- lenovo/thinkpad/e470/default.nix
+++ lenovo/thinkpad/e470/default.nix
@@ -4,8 +4,16 @@
   imports = [
     ../.
     ../../../common/cpu/intel/kaby-lake
+    ../../../common/gpu/nvidia.nix
   ];
 
-  # see https://github.com/NixOS/nixpkgs/issues/69289
+  hardware.nvidia.prime = {
+    # Bus ID of the Intel GPU.
+    intelBusId = lib.mkDefault "PCI:0:2:0";
+
+    # Bus ID of the NVIDIA GPU.
+    nvidiaBusId = lib.mkDefault "PCI:1:0:0";
+  };
+
   boot.kernelPackages = pkgs.linuxPackages_latest;
 }
