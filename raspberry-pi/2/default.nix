986f101a6691f2cdd6f931aa006fc4fe5337ed1d
Jörg Thalheim <joerg@thalheim.io>
2020-10-20 06:25:14 +0200

    raspberry-2: fix evaluation warning

--- raspberry-pi/2/default.nix
+++ raspberry-pi/2/default.nix
@@ -1,9 +1,11 @@
 { lib, pkgs, ...}:
 
+let
+  hasConsoleExtraTTYs = lib.versionAtLeast (lib.versions.majorMinor lib.version) "21.03";
+in
 {
   boot = {
     consoleLogLevel = lib.mkDefault 7;
-    extraTTYs = [ "ttyAMA0" ];
     kernelPackages = lib.mkDefault pkgs.linuxPackages_rpi2;
     kernelParams = [
       "dwc_otg.lpm_enable=0"
@@ -16,10 +18,13 @@
       generationsDir.enable = lib.mkDefault false;
       raspberryPi = {
         enable = lib.mkDefault true;
-	version = lib.mkDefault 2;
+        version = lib.mkDefault 2;
       };
     };
+    extraTTYs = lib.mkIf (!hasConsoleExtraTTYs) [ "ttyAMA0" ];
   };
+
+  console.extraTTYs = lib.mkIf hasConsoleExtraTTYs [ "ttyAMA0" ];
 
   nix.buildCores = 4;
 
