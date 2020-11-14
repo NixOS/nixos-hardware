631011a23c8ec5a8c7b1bc419a654ad9de50392a
mitchmindtree <mitchell.nordine@gmail.com>
2020-11-14 18:27:04 +0100

    Add necessary fw and alsa conf for audio. Add patch for AX500.

--- dell/xps/13-9310/default.nix
+++ dell/xps/13-9310/default.nix
@@ -1,7 +1,30 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
   imports = [
     ../../../common/cpu/intel
     ../../../common/pc/laptop
   ];
+
+  # Necessary for audio.
+  # https://bbs.archlinux.org/viewtopic.php?pid=1933643#p1933643
+  hardware.firmware = [ pkgs.sof-firmware ];
+
+  # Confirmed necessary to get audio working as of 2020-11-13:
+  # https://bbs.archlinux.org/viewtopic.php?pid=1933643#p1933643
+  boot.extraModprobeConfig = ''
+    options snd-intel-dspcfg dsp_driver=1
+  '';
+
+  # TODO: Remove this once landed in kernel.
+  # Apply kernel patch for xps 9310 wifi bug.
+  # https://patchwork.kernel.org/project/linux-wireless/patch/1605121102-14352-1-git-send-email-kvalo@codeaurora.org/
+  boot.kernelPatches = [
+    {
+      name = "ath11k-qca6390-xps9310";
+      patch = builtins.fetchurl "https://patchwork.kernel.org/project/linux-wireless/patch/1605121102-14352-1-git-send-email-kvalo@codeaurora.org/raw/";
+    }
+  ];
+
+  # Allows for updating firmware via `fwupdmgr`.
+  # services.fwupd.enable = true;
 }
