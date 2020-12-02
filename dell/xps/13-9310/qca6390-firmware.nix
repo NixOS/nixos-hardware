cdea79566b2b577371c21e35160fce965eed4de3
mitchmindtree <mail@mitchellnordine.com>
2020-12-02 15:50:24 +0100

    Use `fetchFromGitHub` to retrieve qca6390 fw more efficiently
    Addresses @terinjokes comment [here][1].
    
    [1]: https://github.com/NixOS/nixos-hardware/pull/207/files/906cd8b20cad90a9da37fae5c17527b9ae4e3c42#pullrequestreview-542698197

--- dell/xps/13-9310/qca6390-firmware.nix
+++ dell/xps/13-9310/qca6390-firmware.nix
@@ -1,16 +1,17 @@
 # Based on instructions from kvalo at: http://lists.infradead.org/pipermail/ath11k/2020-November/000537.html
 # The xps/13-9360/qca6174-firmware.nix was a useful reference for how to setup this module.
-{ stdenv }:
+{ stdenv, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
   name = "${target}-firmware-${version}";
   version = "${branch}-00042";
   branch = "master";
   target = "QCA6390";
-  src = builtins.fetchGit {
-    url = "https://github.com/kvalo/ath11k-firmware.git";
-    ref = "master";
+  src = fetchFromGitHub {
+    owner = "kvalo";
+    repo = "ath11k-firmware";
     rev = "45a6c45a19799d3b06fc2287d5ba44e19ee0aa00";
+    sha256 = "1slfjzy2b9zi8744gyw8piz9gfvrh8s38wmyzzqj525iy76zn4qv";
   };
   buildCommand = ''
     mkdir -p $out/lib/firmware/ath11k/${target}/hw2.0/
@@ -24,7 +25,8 @@ stdenv.mkDerivation rec {
       This derivation is based on the instructions provided by kvalo in:
       http://lists.infradead.org/pipermail/ath11k/2020-November/000537.html
     '';
-    homepage = "https://github.com/kvalo/ath11k-firmware/tree/master/QCA6390/hw2.0";
+    homepage =
+      "https://github.com/kvalo/ath11k-firmware/tree/master/QCA6390/hw2.0";
     license = licenses.unfreeRedistributable;
     maintainers = with maintainers; [ mitchmindtree ];
     platforms = platforms.linux;
