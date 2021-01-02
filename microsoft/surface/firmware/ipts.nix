233c69d363947a1d15de6f8baa95c27489358218
mexisme <wildjim+dev@kiwinet.org>
2021-01-03 10:36:55 +1300

    Use the upstream repo to provide the ipts firmware

--- microsoft/surface/firmware/ipts.nix
+++ microsoft/surface/firmware/ipts.nix
@@ -1,10 +1,13 @@
-{stdenv, pkgs}:
+{ stdenv, pkgs }:
+let
+  repos = (pkgs.callPackage ../repos.nix {});
+in
 stdenv.mkDerivation {
   name = "microsoft-surface-ipts-firmware";
-  src = ./intel/ipts;
+  src = repos.surface-ipts-firmware;
   priority = 1;
   installPhase = ''
     mkdir -p $out/lib/firmware/intel/ipts
-    cp -r $src/* $out/lib/firmware/intel/ipts
+    cp -r $src/firmware/intel/ipts/* $out/lib/firmware/intel/ipts
   '';
 }
