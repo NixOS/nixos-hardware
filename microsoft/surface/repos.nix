c72a04b7f5732fefb2d702255f12a2bfe2f35dcc
mexisme <wildjim+dev@kiwinet.org>
2021-01-23 14:27:44 +1300

    Remove obsolete surface-ipts-firmware repo entry

--- microsoft/surface/repos.nix
+++ microsoft/surface/repos.nix
@@ -6,12 +6,6 @@
     sha256="0h8624d7ix1p6ysw9bllmnnwnv164z8xkx56zj3vdczn91vmqcf9";
   };
 
-  surface-ipts-firmware = fetchgit {
-    url="https://github.com/linux-surface/surface-ipts-firmware.git";
-    rev="bd5093318d2550d5d668241d0d34df4be3fc03ab";
-    sha256="0q7k52yk3jdliizl3cq1m3zbbqsx360wxybnbgkaq46rawzhw5bk";
-  };
-
   libwacom-surface = fetchgit {
     url="https://github.com/linux-surface/libwacom-surface.git";
     rev="12628e069298ac2e409501d423a1bf8567b5d1bb";
