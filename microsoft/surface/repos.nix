a8d7159b6be1002bca2c5fd82a81d19ece41c55f
mexisme <wildjim+dev@kiwinet.org>
2021-01-23 14:32:20 +1300

    Remove the libwacom-surface repos
    - This will be worked on independently, and hopefully added to nixpkgs, instead.

--- microsoft/surface/repos.nix
+++ microsoft/surface/repos.nix
@@ -5,10 +5,4 @@
     rev="25ab2cf75e5eda5ab9739db1907300010c06dacf";
     sha256="0h8624d7ix1p6ysw9bllmnnwnv164z8xkx56zj3vdczn91vmqcf9";
   };
-
-  libwacom-surface = fetchgit {
-    url="https://github.com/linux-surface/libwacom-surface.git";
-    rev="12628e069298ac2e409501d423a1bf8567b5d1bb";
-    sha256="1lydyfx33crd0qvzdyqyp39d0d9fnsplch6429sagdsn6a139ap0";
-  };
 }
