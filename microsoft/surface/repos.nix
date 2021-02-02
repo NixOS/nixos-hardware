bbb5dec0e353f841612a1c3a3f933c56ede418b2
mexisme <wildjim+dev@kiwinet.org>
2021-02-02 23:58:51 +1300

    Use kernel 5.10.10 and latest linux-surface patches

--- microsoft/surface/repos.nix
+++ microsoft/surface/repos.nix
@@ -2,7 +2,7 @@
 {
   linux-surface = fetchgit {
     url="https://github.com/linux-surface/linux-surface.git";
-    rev="25ab2cf75e5eda5ab9739db1907300010c06dacf";
-    sha256="0h8624d7ix1p6ysw9bllmnnwnv164z8xkx56zj3vdczn91vmqcf9";
+    rev="74e3a9cd99dd21c362f9a674d0bb42a824c39bd8";
+    sha256="09andw78kmz9yd37j0r00y1za2j9n0x6g47b1gyijsglj9yzr7lm";
   };
 }
