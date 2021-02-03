d3cd9a2229c0b699eab4922c081be4607f134cd1
Winter <winter@winter.cafe>
2021-02-02 19:13:14 -0500

    microsoft-surface: add surface-control

--- microsoft/surface/default.nix
+++ microsoft/surface/default.nix
@@ -1,8 +1,7 @@
-{ config, lib, pkgs, ... }:
-{
-  imports = [
-    ./kernel
-    ./firmware
-    ./hardware_configuration.nix
-  ];
+{ config, lib, pkgs, ... }: {
+  imports = [ ./kernel ./firmware ./hardware_configuration.nix ];
+
+  environment.systemPackages = with pkgs; [ surface-control ];
+  users.groups.surface-control = {};
+  services.udev.packages = [ pkgs.surface-control ];
 }
