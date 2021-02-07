f9378480c06c78a8d8f2788785082f1fe709a398
mexisme <wildjim+dev@kiwinet.org>
2021-02-07 15:49:02 +1300

    Fix the output hash for the additional source

--- microsoft/surface/firmware/ath10k.nix
+++ microsoft/surface/firmware/ath10k.nix
@@ -4,14 +4,12 @@ let
   ath10k = ./ath10k;
   # ath10k = repos.ath10k-firmware;
 in
-
 firmwareLinuxNonfree.overrideAttrs (old: rec {
   pname = "microsoft-surface-go-firmware-linux-nonfree";
   srcs = [
     firmwareLinuxNonfree.src
     ath10k
   ];
-
   sourceRoot = firmwareLinuxNonfree.src;
   priority = 1;
 
@@ -25,4 +23,6 @@ firmwareLinuxNonfree.overrideAttrs (old: rec {
     cp ${ath10k}/K1535_Debian/board.bin $out/lib/firmware/ath10k/QCA6174/hw2.1/
     cp ${ath10k}/K1535_Debian/board.bin $out/lib/firmware/ath10k/QCA6174/hw3.0/
   '';
+
+  outputHash = "1nc56qii96dfvxnv3ad3lxz2rzyqcbldk0h9rbm3l2pgamkvj8dw";
 })
