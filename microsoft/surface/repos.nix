311c6bf0efc0c97babd7d31ba3af068082151a09
mexisme <wildjim+dev@kiwinet.org>
2021-02-24 08:46:33 +1300

    Ref to backup repo for Surface Go firmware

--- microsoft/surface/repos.nix
+++ microsoft/surface/repos.nix
@@ -12,10 +12,15 @@
     sha256="0l8wfj8z4jbb31nzqkaxisby0n6061ix01c5di9bq66iby59j8py";
   };
 
-    surface-go-ath10k-firmware = fetchurl {
+  surface-go-ath10k-firmware_upstream = fetchurl {
     url="https://support.killernetworking.com/K1535_Debian/board.bin";
     # url="https://www.killernetworking.com/support/K1535_Debian/board.bin";
     sha256="0l8wfj8z4jbb31nzqkaxisby0n6061ix01c5di9bq66iby59j8py";
   };
 
+  surface-go-ath10k-firmware_backup = fetchgit {
+    url="git@github.com:mexisme/linux-surface_ath10k-firmware.git";
+    rev="74e5409e699383d6ca2bc4da4a8433d16f3850b1";
+    sha256="169vgvxpgad9anmchs22fj5qm6ahzjfdnwhd8pc280q705vx6pjk";
+  };
 }
