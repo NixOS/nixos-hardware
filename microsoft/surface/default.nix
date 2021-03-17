bf19677a5f377b5306cff1429b04d0023b80dd3d
Winter <winter@winter.cafe>
2021-03-17 15:46:55 -0400

    nixfmt

--- microsoft/surface/default.nix
+++ microsoft/surface/default.nix
@@ -2,6 +2,6 @@
   imports = [ ./kernel ./firmware ./hardware_configuration.nix ];
 
   environment.systemPackages = with pkgs; [ surface-control ];
-  users.groups.surface-control = {};
+  users.groups.surface-control = { };
   services.udev.packages = [ pkgs.surface-control ];
 }
