0616faa41cd1544cf9abdbefb228cd51a8c0a3d1
Eric Drechsel <eric@pdxhub.org>
2021-03-19 14:31:58 -0700

    .

--- apple/macbook-pro/2-2/default.nix
+++ apple/macbook-pro/2-2/default.nix
@@ -8,8 +8,12 @@
   ];
 
   # TODO:
+  #  - [ ] Pin kernel to ensure patch succeeds
+  #  - [ ] Find how to get source dir
   #  - [ ] build kernel with radeon vbios loading support
+  #        https://discourse.nixos.org/t/how-to-install-a-linux-kernel-patch/6889
   #  - [ ] put firmware in the right place
-  #  [[https://bbs.archlinux.org/viewtopic.php?pid=1810437#p1810437][Load custom Radeon firmware for Macbook Pro / Kernel & Hardware / Arch Linux Forums]]
+  #        - Load custom Radeon firmware for Macbook Pro / Kernel & Hardware / Arch Linux Forums https://bbs.archlinux.org/viewtopic.php?pid=1810437#p1810437
+  #        - https://discourse.nixos.org/t/trying-to-include-custom-firmware-but-it-doesnt-appear-under-run-current-system/3044
   boot.kernelParams = ["nomodeset"];
 }
