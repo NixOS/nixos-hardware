da67c368536184763833099f833829c9422b181e
Jörg Thalheim <joerg@thalheim.io>
2021-05-01 08:14:51 +0200

    add lenovo-thinkpad-x13

---
+++ lenovo/thinkpad/x13/default.nix
@@ -0,0 +1,12 @@
+{ config, lib, ... }: {
+  # Reference to hardware: https://certification.ubuntu.com/hardware/202004-27844
+  imports = [
+    ../.
+    ../../../common/cpu/intel
+    ../../../common/pc/laptop/acpi_call.nix
+    ../../../common/pc/laptop/ssd
+  ];
+
+  # Somehow psmouse does not load automatically on boot for me
+  boot.kernelModules = [ "psmouse" ];
+}
