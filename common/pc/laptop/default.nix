e4ce746d5b21ee068152754ae7e71201ca48890d
Bryan Honof <bryanhonof@gmail.com>
2021-05-26 18:54:50 +0200

    Add check for power-profiles-daemon.enable
    Gnome 40 now uses power-profile-daemon
    https://gitlab.freedesktop.org/hadess/power-profiles-daemon ,
    which clashes with tlp. This check will disable tlp whenever it finds
    that the power-profiles-daemon is activated.

--- common/pc/laptop/default.nix
+++ common/pc/laptop/default.nix
@@ -3,5 +3,12 @@
 {
   imports = [ ../. ];
 
-  services.tlp.enable = lib.mkDefault true;
+  # Gnome 40 introduced a new way of managing power, without tlp.
+  # However, these 2 services clash when enabled simultaneously.
+  # https://github.com/NixOS/nixos-hardware/issues/260
+  services.tlp.enable =
+    if config.services.power-profiles-daemon.enable == true then
+      false
+    else
+      true;
 }
