1fe5b7686d83ab6fc3af7304332ee69940300999
Bryan Honof <bryanhonof@gmail.com>
2021-05-27 00:15:49 +0200

    Update common/pc/laptop/default.nix
    Co-authored-by: Jörg Thalheim <Mic92@users.noreply.github.com>

--- common/pc/laptop/default.nix
+++ common/pc/laptop/default.nix
@@ -6,9 +6,5 @@
   # Gnome 40 introduced a new way of managing power, without tlp.
   # However, these 2 services clash when enabled simultaneously.
   # https://github.com/NixOS/nixos-hardware/issues/260
-  services.tlp.enable = lib.mkDefault
-    (if config.services.power-profiles-daemon.enable == true then
-      false
-    else
-      true);
+  services.tlp.enable = lib.mkDefault !config.services.power-profiles-daemon.enable;
 }
