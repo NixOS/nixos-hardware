2bc26b3c5c571b3609e3a50f35e67419c7688085
Bryan Honof <bryanhonof@gmail.com>
2021-05-26 19:00:01 +0200

    Add lib.mkDefault to the statement

--- common/pc/laptop/default.nix
+++ common/pc/laptop/default.nix
@@ -6,9 +6,9 @@
   # Gnome 40 introduced a new way of managing power, without tlp.
   # However, these 2 services clash when enabled simultaneously.
   # https://github.com/NixOS/nixos-hardware/issues/260
-  services.tlp.enable =
-    if config.services.power-profiles-daemon.enable == true then
+  services.tlp.enable = lib.mkDefault
+    (if config.services.power-profiles-daemon.enable == true then
       false
     else
-      true;
+      true);
 }
