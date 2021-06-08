dd0f60f5b607a9f4dfb26faf72703a03178e1e33
Pavel Nazarov <nazarov.pn@gmail.com>
2021-06-08 10:53:08 +0300

    add nvidia fixes and modules options

--- lenovo/thinkpad/p1/3th-gen/nvidia.nix
+++ lenovo/thinkpad/p1/3th-gen/nvidia.nix
@@ -3,11 +3,23 @@
   hardware.nvidia.modesetting.enable = true;
   hardware.opengl.driSupport32Bit = true;
   hardware.opengl.enable = true;
+  # otherwise offloading will not work
+  hardware.nvidia.nvidiaPersistenced = true;
 
   hardware.nvidia.prime = {
     # Bus ID of the Intel GPU.
     intelBusId = lib.mkDefault "PCI:0:2:0";
     # Bus ID of the NVIDIA GPU.
     nvidiaBusId = lib.mkDefault "PCI:1:0:0";
+  };
+
+  # set output source to Nvidia for HDMI port
+  systemd.user.services.xrandr-outputsource = {
+    script = ''
+      ${pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource NVIDIA-G0 modesetting
+    '';
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    enable = true;
   };
 }
--- lenovo/thinkpad/p1/default.nix
+++ lenovo/thinkpad/p1/default.nix
@@ -9,9 +9,12 @@
   # Need to set Thunderbolt to "BIOS Assist Mode"
   # https://forums.lenovo.com/t5/Other-Linux-Discussions/T480-CPU-temperature-and-fan-speed-under-linux/m-p/4114832
   boot.kernelParams = [ "acpi_backlight=native" ];
+  boot.kernelModules = [ "thinkpad_acpi fan_control=1 software_mute=0" ];
+  boot.extraModprobeConfig = "options thinkpad_acpi fan_control=1 software_mute=0";
 
   # Emulate mouse wheel on trackpoint
   # hardware.trackpoint.emulateWheel = true;
 
   services.fprintd.enable = true;
+  services.thinkfan.enable = true;
 }
