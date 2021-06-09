59a171c8332cdb700e8e27f40d0f9c7d73c9cc36
Andy Richardson <andy.john.richardson@gmail.com>
2021-06-09 12:46:01 +0100

    Fix 9500 wifi support

--- dell/xps/15-9500/default.nix
+++ dell/xps/15-9500/default.nix
@@ -18,4 +18,11 @@ in
   # Thermald doesn't have a default config for the 9500 yet, the one in this repo
   # was generated with dptfxtract-static (https://github.com/intel/dptfxtract)
   services.thermald.configFile = lib.mkDefault thermald-conf;
+  
+  # WiFi speed is slow and crashes by default
+  # disable_11ax - doesn't actually disable AX but will fix the speed and crashes
+  # power_save - works well on this card
+  boot.extraModprobeConfig = ''
+    options iwlwifi power_save=1 disable_11ax=1
+  '';
 }
