d2d9a58a5c03ea15b401c186508c171c07f9c4f1
Jörg Thalheim <Mic92@users.noreply.github.com>
2021-08-21 09:22:25 +0100

    remove systemd-boot from hardware profiles (#307)
    - We should not enable canTouchEfiVariables by default as this
      wears out the EFI storage.
    - We should not set systemd-boot as default. This is up to the user to
      decide. There are exceptions when hardware only supports specific
      bootloaders so.

--- dell/xps/15-9560/xps-common.nix
+++ dell/xps/15-9560/xps-common.nix
@@ -1,10 +1,6 @@
 { lib, ... }:
 
 {
-
-  # Boot loader
-  boot.loader.systemd-boot.enable = lib.mkDefault true;
-  boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
   boot.kernelParams = lib.mkDefault [ "acpi_rev_override" ];
 
   # This will save you money and possibly your life!
