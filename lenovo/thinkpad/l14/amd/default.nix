03eba5720afdea051ba32c2c06c9424c3c2f300e
Julian Stecklina <js@alien8.de>
2021-08-25 23:28:28 +0200

    lenovo/thinkpad/l14/amd: drop workaround for old kernels
    ... because the latest supported NixOS runs on Linux 5.10 by
    default.

--- lenovo/thinkpad/l14/amd/default.nix
+++ lenovo/thinkpad/l14/amd/default.nix
@@ -20,8 +20,4 @@
     # driver to kernel panic.
     "iommu=soft"
   ];
-
-  # As of writing this, Linux 5.8 is the oldest kernel that is still
-  # supported and has decent Renoir support.
-  boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "5.8") pkgs.linuxPackages_latest;
 }
