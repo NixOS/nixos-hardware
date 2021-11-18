6479f584f7247f43eba2f37304bcb085f8ae8995
Pasquale <p3dimaria@hotmail.it>
2021-11-19 00:01:56 +0100

    Added ROG Strix G733QS

---
+++ asus/rog-strix/g733qs/default.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, lib, ... }:
+{
+  imports = [
+    ../../../common/cpu/amd
+    ../../../common/gpu/nvidia.nix
+    ../../../common/pc/laptop
+    ../../../common/pc/ssd
+    ../../battery.nix
+  ];
+
+  # fixing audio by overriding pins as suggested in
+  # https://www.reddit.com/r/ASUS/comments/mfokva/asus_strix_scar_17_g733qs_and_linux/
+  hardware.firmware = [
+    (pkgs.runCommand "jack-retask" { } ''
+      install -D ${./hda-jack-retask.fw} $out/lib/firmware/hda-jack-retask.fw
+    '')
+  ];
+  boot.extraModprobeConfig = ''
+    options snd-hda-intel patch=hda-jack-retask.fw
+  '';
+  # before 5.12 it would interpret every keystroke as the power button
+  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+
+  hardware.nvidia.prime = {
+    offload.enable = lib.mkDefault true;
+    amdgpuBusId = "PCI:5:0:0";
+    nvidiaBusId = "PCI:1:0:1";
+  };
+}
