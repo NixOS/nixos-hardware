3e4d52da0a4734225d292667a735dcc67dcef551
kiana-S <kiana.a.sheibani@gmail.com>
2022-01-24 22:41:12 -0500

    surface/kernel: 5.13.4 -> 5.16.11

--- microsoft/surface/kernel/default.nix
+++ microsoft/surface/kernel/default.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, ... }:
 
 {
-  boot.kernelPackages = pkgs.callPackage ./linux-5.13.4 { };
+  boot.kernelPackages = pkgs.callPackage ./linux-5.16.11.nix { };
 }
--- microsoft/surface/repos.nix
+++ microsoft/surface/repos.nix
@@ -1,9 +1,9 @@
 { lib, pkgs, fetchFromGitHub, fetchurl }: {
-  linux-surface = fetchFromGitHub {
+  linux-surface-kernel = fetchFromGitHub {
     owner = "linux-surface";
-    repo = "linux-surface";
-    rev = "4130746c31606aebaccef58997b650beda9049f7";
-    sha256 = "0r74z0hj3rg8bn2arh2zj8p7awk7fi14gpyrs5h44lwxw4351xky";
+    repo = "kernel";
+    rev = "db94c89f56d6ceae03ca3802e11197f48e6c539f";
+    sha256 = "0c58ri0i9gdb4w7l361pnkvq6ap17kmgnxngh0bcdmgn4dc88wx2";
   };
 
   ath10k-firmware = fetchFromGitHub {
@@ -11,12 +11,6 @@
     repo = "ath10k-firmware";
     rev = "c987e38cbdb90dcb4e477d5dd21de66c77996435";
     sha256 = "16a67baxlga8vb43zbby2s7kpp4488vczg3manmr9g3wxnhhb9n3";
-  };
-
-  surface-go-ath10k-firmware_upstream = fetchurl {
-    url = "https://support.killernetworking.com/K1535_Debian/board.bin";
-    # url="https://www.killernetworking.com/support/K1535_Debian/board.bin";
-    sha256 = "0l8wfj8z4jbb31nzqkaxisby0n6061ix01c5di9bq66iby59j8py";
   };
 
   surface-go-ath10k-firmware_backup = fetchFromGitHub {
