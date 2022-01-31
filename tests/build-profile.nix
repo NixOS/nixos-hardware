f8eff138d838f8415e2be1614955de25104082bd
Jörg Thalheim <joerg@thalheim.io>
2022-01-31 06:05:05 +0100

    fix test profile

--- tests/build-profile.nix
+++ tests/build-profile.nix
@@ -1,8 +1,8 @@
 { profile }:
 
 let
-  shim = {
-    boot.loader.systemd-boot.enable = true;
+  shim = { config, ... }: {
+    boot.loader.systemd-boot.enable = !config.boot.loader.generic-extlinux-compatible.enable && !config.boot.loader.raspberryPi.enable;
 
     fileSystems."/" = {
       device = "/dev/disk/by-uuid/00000000-0000-0000-0000-000000000000";
