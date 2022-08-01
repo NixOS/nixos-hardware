a01a1095e4226dcb3ba68937831c7cc90a8eedea
Jörg Thalheim <joerg@thalheim.io>
2022-08-01 14:14:12 +0200

    amd pstate: small typo

--- common/cpu/amd/pstate.nix
+++ common/cpu/amd/pstate.nix
@@ -1,6 +1,6 @@
 { lib, config, ... }: {
   # Enables the amd cpu scaling https://www.kernel.org/doc/html/latest/admin-guide/pm/amd-pstate.html
-  # On recent AMD cupus this can be more energy efficient
+  # On recent AMD CPUs this can be more energy efficient.
 
   imports = [ ./. ];
   boot = lib.mkIf (lib.versionAtLeast config.boot.kernelPackages.kernel.version "5.17") {
