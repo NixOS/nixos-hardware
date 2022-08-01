68a27c2b00fd6e4ed23b7093c226ef604fdf4f0f
pasqui23 <pasqui23@users.noreply.github.com>
2022-08-01 12:03:29 +0200

    Clarify kernel version condition
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- common/cpu/amd/pstate.nix
+++ common/cpu/amd/pstate.nix
@@ -1,9 +1,9 @@
 { lib, config, ... }: {
-  #Enables the amd cpu scaling https://www.kernel.org/doc/html/latest/admin-guide/pm/amd-pstate.html
-  #On recent AMD cupus this can be more energy efficient
+  # Enables the amd cpu scaling https://www.kernel.org/doc/html/latest/admin-guide/pm/amd-pstate.html
+  # On recent AMD cupus this can be more energy efficient
 
   imports = [ ./. ];
-  boot = lib.mkIf (!lib.versionOlder config.boot.kernelPackages.kernel.version "5.17") {
+  boot = lib.mkIf (lib.versionAtLeast config.boot.kernelPackages.kernel.version "5.17") {
     kernelParams = [ "initcall_blacklist=acpi_cpufreq_init" ];
     kernelModules = [ "amd-pstate" ];
   };
