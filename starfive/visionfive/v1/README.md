68e08aa56b38fb6758ffc859b065c88634e13046
Madoura <madouura@gmail.com>
2022-10-08 10:27:03 -0500

    starfive/visionfive/v1: init

---
+++ starfive/visionfive/v1/README.md
@@ -0,0 +1,25 @@
+### Build the SD image
+- ``nix-build "<nixpkgs/nixos>" -A config.system.build.sdImage -I nixos-config=iso.nix``
+
+- ``iso.nix``
+    ```nix
+    { config, ... }:
+
+    {
+    imports = [ <nixos-hardware/starfive/visionfive/v1/sd-image-installer.nix> ];
+    # or imports = [ "${nixos-hardware-directory}/starfive/visionfive/v1/sd-image-installer.nix" ];
+
+    nixpkgs.crossSystem = {
+        config = "riscv64-unknown-linux-gnu";
+        system = "riscv64-linux";
+    };
+    }
+    ```
+
+### Relevant documentation
+- Flashing
+- - https://doc-en.rvspace.org/VisionFive/Quick_Start_Guide/VisionFive_QSG/hardware_connection.html
+- - https://doc-en.rvspace.org/VisionFive/Quick_Start_Guide/VisionFive_QSG/using_xmodem1.html
+- Recovery
+- - https://doc-en.rvspace.org/VisionFive/Quick_Start_Guide/VisionFive_QSG/hardware_setup.html
+- - https://doc-en.rvspace.org/VisionFive/Quick_Start_Guide/VisionFive_QSG/for_maclinux4.html
---
+++ starfive/visionfive/v1/default.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, ...}:
+
+{
+  hardware.deviceTree.name = lib.mkDefault "starfive/jh7100-starfive-visionfive-v1.dtb";
+  systemd.services."serial-getty@hvc0".enable = lib.mkDefault false;
+  environment.systemPackages = with pkgs; lib.mkDefault [ mtdutils ];
+
+  boot = {
+    # Force no ZFS (from nixos/modules/profiles/base.nix) until updated to kernel 6.0
+    supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
+    consoleLogLevel = lib.mkDefault 7;
+    kernelPackages = lib.mkDefault (pkgs.callPackage ./linux.nix { inherit (config.boot) kernelPatches; });
+
+    kernelParams = lib.mkDefault [
+      "console=tty0"
+      "console=ttyS0,115200n8"
+      "earlycon=sbi"
+    ];
+
+    initrd.kernelModules = lib.mkDefault [
+      "dw-axi-dmac-platform"
+      "dw_mmc-pltfm"
+      "spi-dw-mmio"
+    ];
+
+    loader = {
+      grub.enable = lib.mkDefault false;
+      generic-extlinux-compatible.enable = lib.mkDefault true;
+    };
+  };
+}
