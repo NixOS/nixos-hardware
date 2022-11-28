158f86bb326dd3529f288bb476464f61679a41c3
mexisme <wildjim+dev@kiwinet.org>
2022-11-28 20:23:51 +1300

    DRY the MS Surface kernel module

--- microsoft/surface/kernel/default.nix
+++ microsoft/surface/kernel/default.nix
@@ -1,5 +1,37 @@
-{ config, lib, pkgs, ... }:
+{ lib,
+  buildLinux,
+  callPackage,
+  linuxPackagesFor,
+  ...
+}:
 
-{
-  boot.kernelPackages = pkgs.callPackage ./linux-5.16.11.nix { };
+# To test the kernel build:
+# nix-build -E "with import <nixpkgs> {}; (pkgs.callPackage ./. {}).kernel"
+
+let
+  inherit (lib) kernel recurseIntoAttrs;
+  repos = callPackage ../repos.nix {};
+
+  linuxPackage =
+    { version,
+      modDirVersion ? version,
+      branch,
+      src,
+      kernelPatches ? [],
+      ...
+    }: let
+      buildLinux' = buildLinux {
+        inherit version src kernelPatches;
+        modDirVersion = version;
+        extraMeta.branch = branch;
+      };
+      linuxPackagesFor' = (linuxPackagesFor buildLinux');
+    in recurseIntoAttrs linuxPackagesFor';
+
+  linux-5_16_11 = linuxPackage (
+    callPackage ./linux-5.16.11.nix { inherit repos; }
+  );
+
+in {
+  boot.kernelPackages = linux-5_16_11;
 }
