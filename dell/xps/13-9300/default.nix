b1582825dda721e4feeeec782b34bf9af9f7f5f3
mexisme <wildjim+dev@kiwinet.org>
2022-12-05 12:41:54 +1300

    Support for the Dell XPS 13, 9300 model

---
+++ dell/xps/13-9300/default.nix
@@ -0,0 +1,29 @@
+{ config, lib, ... }:
+
+let
+  inherit (lib) mkDefault;
+
+in {
+  imports = [
+    ../../../common/cpu/intel
+    ../../../common/gpu/intel.nix
+    ../../../common/pc/laptop
+    ../../../common/pc/laptop/acpi_call.nix
+    ../../../common/pc/ssd
+  ];
+
+  # Force S3 sleep mode. See README.wiki for details.
+  boot.kernelParams = [ "mem_sleep_default=deep" ];
+
+  # Touchpad goes over i2c, and the psmouse module interferes with it
+  boot.blacklistedKernelModules = [ "psmouse" ];
+
+  # Includes the Wi-Fi and Bluetooth firmware for the QCA6390.
+  hardware.enableRedistributableFirmware = mkDefault true;
+
+  # Allows for updating firmware via `fwupdmgr`.
+  services.fwupd.enable = mkDefault true;
+
+  # This will save you money and possibly your life!
+  services.thermald.enable = mkDefault true;
+}
