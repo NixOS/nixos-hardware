5c29b44d6cad610e6771cdc3829d234ff5fbfc63
Markus Kohlhase <markus.kohlhase@slowtec.de>
2022-12-19 18:07:58 +0100

    Update lenovo/thinkpad/p16s/amd/gen1
    - Use linux kernel 6.1
    - Import CPU and GPU modules
    - Import acpi_call

--- lenovo/thinkpad/p16s/amd/gen1/default.nix
+++ lenovo/thinkpad/p16s/amd/gen1/default.nix
@@ -1,6 +1,13 @@
 { config, lib, pkgs, ... }:
 
 {
+
+  imports = [
+    ../../../../../common/cpu/amd
+    ../../../../../common/gpu/amd
+    ../../../../../common/pc/laptop/acpi_call.nix
+  ];
+
   # For mainline support of rtw89 wireless networking
-  boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "5.16") pkgs.linuxPackages_latest;
+  boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "5.16") pkgs.linuxPackages_6_1;
 }
