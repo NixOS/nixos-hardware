993e6c8726f6c75bab73236d1bce94a2e79160f2
suliman <suliman.p2019@gmail.com>
2022-12-29 16:50:43 +0300

    removo TLP

--- lenovo/yoga/6/13ALC6/default.nix
+++ lenovo/yoga/6/13ALC6/default.nix
@@ -16,23 +16,5 @@
 
   # energy savings
   hardware.bluetooth.powerOnBoot = lib.mkDefault false;
-  services.power-profiles-daemon.enable = false;
-  services.tlp.enable = lib.mkDefault true;
   boot.kernelParams = ["mem_sleep_default=deep" "pcie_aspm.policy=powersupersave"];
-  powerManagement.enable = lib.mkDefault true;
-  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
-  services.tlp.settings = {
-    # CPU
-    CPU_SCALING_GOVERNOR_ON_BAT="ondemand";
-    CPU_SCALING_GOVERNOR_ON_AC="performance";
-    CPU_BOOST_ON_AC=1;
-    CPU_BOOST_ON_BAT=0;
-    # Stop charging battery at 60%, ideapad_laptop driver required
-    #STOP_CHARGE_THRESH_BAT0=1;
-    # Stop charging battery at 100%
-    STOP_CHARGE_THRESH_BAT0=0;
-    # GPU
-    RADEON_DPM_PERF_LEVEL_ON_AC="auto";
-    RADEON_DPM_PERF_LEVEL_ON_BAT="low";
-  };
 }
