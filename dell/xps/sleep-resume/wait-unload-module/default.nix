f66b55a2c4f5bccd153d4d360080b66a29a438a1
mexisme <wildjim+dev@kiwinet.org>
2022-12-30 10:43:17 +1300

    Script for unloading modules, and waiting for completion
    Designed to be used to reload certain drivers on Dell laptops, which
    won't otherwise work correctly after resuming from sleep.

---
+++ dell/xps/sleep-resume/wait-unload-module/default.nix
@@ -0,0 +1,48 @@
+{ lib,
+  kmod,
+  writeShellApplication,
+  ...
+}:
+
+writeShellApplication {
+  name = "wait-unload-module";
+  runtimeInputs = [ kmod ]
+  text = ''
+    unload_module() {
+      local module="$1"
+
+      if modprobe -r "${module}" 2>&1; then
+        echo ok
+      else
+        echo fail
+      fi
+    }
+
+    # NOTE: If using "systemd-inhibit", please use "--mode=delay" to minimise the risk of sleep
+    #       being completely blocked.
+    main() {
+      local module="$1"
+
+      # TODO: Change to a timeout, rather than a "sleep"?
+      while sleep 1; do
+        output="$(unload_module "${module}")"
+        case "${output}" in
+          *is\ in\ use*)
+            :noop
+            ;;
+          *ok*)
+            return 0
+            ;;
+          *)
+            echo "'modprobe':
+            echo "${output}"
+            echo "(Exiting)"
+            return 1
+            ;;
+        esac
+      done
+    }
+
+    main "$@"
+  '';
+}
