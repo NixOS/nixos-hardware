80e8d29a9cd605f6230640f57b8f2a7833b6249f
mexisme <wildjim+dev@kiwinet.org>
2023-01-11 02:24:06 +1300

    Update Surface Go to depend on other PC modules
    - SSD
    - Intel Kaby Lake
    - Intel GPU

--- microsoft/surface/surface-go/default.nix
+++ microsoft/surface/surface-go/default.nix
@@ -6,10 +6,18 @@ in {
   imports = [
     ../common
     ./firmware/ath10k
+    ../../../common/pc
+    ../../../common/pc/ssd
+    ../../../common/cpu/intel
+    ../../../common/cpu/intel/kaby-lake
+  ];
+
+  boot.kernelParams = [
+    "i915.enable_rc6=1"
+    "i915.modeset=1"
   ];
 
   boot.extraModprobeConfig = mkDefault ''
-    options i915 enable_fbc=1 enable_rc6=1 modeset=1
     options snd_hda_intel power_save=1
     options snd_ac97_codec power_save=1
     options iwlwifi power_save=Y
