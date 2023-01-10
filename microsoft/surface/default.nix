cca1f04705171080e18f8f270fc98d168811054f
mexisme <wildjim+dev@kiwinet.org>
2023-01-10 16:13:38 +1300

    Move the last of the code into ./microsoft/common/old

--- microsoft/surface/default.nix
+++ microsoft/surface/default.nix
@@ -5,18 +5,6 @@ let
 
 in {
   imports = [
-    ./common
-    ./surface-go/firmware/ath10k
+    ./old
   ];
-
-  boot.extraModprobeConfig = mkDefault ''
-    options i915 enable_fbc=1 enable_rc6=1 modeset=1
-    options snd_hda_intel power_save=1
-    options snd_ac97_codec power_save=1
-    options iwlwifi power_save=Y
-    options iwldvm force_cam=N
-  '';
-
-  microsoft-surface.surface-control.enable = true;
-  microsoft-surface.ipts.enable = true;
 }
