391211b3511b3f3a2669691cfe54480eb17f3565
mexisme <wildjim+dev@kiwinet.org>
2023-01-10 15:43:25 +1300

    Extract _actually_ common code out of microsoft/surface/default.nix

--- microsoft/surface/common/default.nix
+++ microsoft/surface/common/default.nix
@@ -1,7 +1,22 @@
-{ ... }:
+{ lib, ... }:
 
-{
+let
+  inherit (lib) mkDefault;
+
+in {
   imports = [
     ./kernel
   ];
+
+  microsoft-surface.kernelVersion = mkDefault "6.0.11";
+
+  # Seems to be required to properly enable S0ix "Modern Standby":
+  boot.kernelParams = mkDefault [ "mem_sleep_default=deep" ];
+
+  # NOTE: Check the README before enabling TLP:
+  services.tlp.enable = mkDefault false;
+
+  # i.e. needed for wifi firmware, see https://github.com/NixOS/nixos-hardware/issues/364
+  hardware.enableRedistributableFirmware = mkDefault true;
+  hardware.sensor.iio.enable = mkDefault true;
 }
