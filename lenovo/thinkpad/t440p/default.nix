759a06ec4a4dbcb1f33ebab074477a6e49a9dba7
surfaceflinger <nat@nekopon.pl>
2023-01-20 21:35:06 +0100

    lenovo/thinkpad/t440p: force load thinkpad_acpi
    thinkpad_acpi doesn't load automatically on corebooted Lenovo ThinkPads T440p (but works fine if force_loaded). Doesn't affect users with stock firmware.

--- lenovo/thinkpad/t440p/default.nix
+++ lenovo/thinkpad/t440p/default.nix
@@ -9,6 +9,7 @@
   boot = {
     extraModprobeConfig = lib.mkDefault ''
       options bbswitch use_acpi_to_detect_card_state=1
+      options thinkpad_acpi force_load=1 fan_control=1
     '';
     # TODO: probably enable tcsd? Is this line necessary?
     kernelModules = [ "tpm-rng" ];
