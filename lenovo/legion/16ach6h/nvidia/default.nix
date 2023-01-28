be60a34c1a6c3aa3e878fb5030e44c6d0f316af9
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2023-01-28 23:49:25 +0800

    16ach6h: disable amdvlk and rocm-opencl for nvidia-only mode

--- lenovo/legion/16ach6h/nvidia/default.nix
+++ lenovo/legion/16ach6h/nvidia/default.nix
@@ -3,5 +3,11 @@
 {
   imports = [ ../hybrid ];
   services.xserver.videoDrivers = [ "nvidia" ]; # This will override services.xserver.videoDrivers = lib.mkDefault [ "amdgpu" "nvidia" ];
-  hardware.nvidia.prime.offload.enable = false;
+  hardware = {
+    nvidia.prime.offload.enable = false;
+    amdgpu = {
+      amdvlk = false;
+      opencl = false;
+    };
+  };
 }
