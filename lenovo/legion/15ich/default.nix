68136fdd0a77626de0207c152f8c116ff8da6bb7
Winston R. Milling <Winston@Milli.ng>
2023-01-29 14:48:35 -0600

    Lenovo Y530-15ICH Initial Commit

---
+++ lenovo/legion/15ich/default.nix
@@ -0,0 +1,25 @@
+{ lib, ... }:
+
+{
+  imports = [
+    ../../../common/cpu/intel
+    ../../../common/gpu/nvidia/prime.nix
+    ../../../common/pc/laptop
+    ../../../common/pc/laptop/ssd
+    ../../../common/pc/laptop/hdd
+  ];
+
+  hardware = {
+    nvidia = {
+      modesetting.enable = lib.mkDefault true;
+      powerManagement.enable = lib.mkDefault true;
+
+      prime = {
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+  };
+
+  services.thermald.enable = lib.mkDefault true;
+}
