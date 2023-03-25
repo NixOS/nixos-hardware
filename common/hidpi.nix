e0a605d82574018a7e671416f1838eef9341fa5d
Jörg Thalheim <joerg@thalheim.io>
2023-03-25 11:19:48 +0100

    don't set bigger console fonts on newer nixos systems

--- common/hidpi.nix
+++ common/hidpi.nix
@@ -1,12 +1,5 @@
 { lib, pkgs, ... }:
-let
+lib.optionalAttrs (lib.versionOlder (lib.versions.majorMinor lib.version) "23.05") {
   # This option is removed from NixOS 23.05 and up
-  nixosVersion = lib.versions.majorMinor lib.version;
-  config = if lib.versionOlder nixosVersion "23.05" then {
-    hardware.video.hidpi.enable = lib.mkDefault true;
-  } else {
-    # Just set the console font, don't mess with the font settings
-    console.font = lib.mkDefault "${pkgs.terminus_font}/share/consolefonts/ter-v32n.psf.gz";
-    console.earlySetup = lib.mkDefault true;
-  };
-in config
+  hardware.video.hidpi.enable = lib.mkDefault true;
+}
