5fc0f2352069cadea929afe201532b8673488ab3
Steffen Weitz <weitz.steffen@gmail.com>
2023-03-27 21:18:10 +0200

    Add module import to flake

--- flake.nix
+++ flake.nix
@@ -168,6 +168,7 @@
 
       common-cpu-amd = import ./common/cpu/amd;
       common-cpu-amd-pstate = import ./common/cpu/amd/pstate.nix;
+      common-cpu-amd-raphael-igpu = import ./common/cpu/amd/raphael/igpu.nix;
       common-cpu-intel = import ./common/cpu/intel;
       common-cpu-intel-cpu-only = import ./common/cpu/intel/cpu-only.nix;
       common-cpu-intel-kaby-lake = import ./common/cpu/intel/kaby-lake;
