b13c895f11f41e7f64d150074845252a927c56c5
Tom Parker-Shemilt <tom.parkershemilt@doxy.me>
2023-04-29 11:18:48 +0100

    Add config for Dell XPS 15 9520

---
+++ dell/xps/15-9520/default.nix
@@ -0,0 +1,18 @@
+{ lib, ... }:
+{
+  imports = [
+    ../../../common/cpu/intel
+    ../../../common/pc/laptop
+    ../../../common/pc/laptop/ssd
+  ];
+
+  # This will save you money and possibly your life!
+  services.thermald.enable = lib.mkDefault true;
+  
+  # WiFi speed is slow and crashes by default (https://bugzilla.kernel.org/show_bug.cgi?id=213381)
+  # disable_11ax - required until ax driver support is fixed
+  # power_save - works well on this card
+  boot.extraModprobeConfig = ''
+    options iwlwifi power_save=1 disable_11ax=1
+  '';
+}
