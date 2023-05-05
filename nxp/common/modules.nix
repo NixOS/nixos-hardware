c84a8f242331c513a310aff7ebf83cabd11de3bc
Jörg Thalheim <joerg@thalheim.io>
2023-05-05 08:17:52 +0200

    nxp-imx8: drop kernel overlay
    With overlays one cannot re-use nixpkgs across different machines,
    which is a performance issues. In this instance it's not really needed.

--- nxp/common/modules.nix
+++ nxp/common/modules.nix
@@ -1,12 +1,8 @@
 { pkgs, lib, ... }: {
-  nixpkgs.overlays = [
-    (import ./overlay.nix)
-  ];
-
   nixpkgs.hostPlatform = "aarch64-linux";
 
   boot = {
-    kernelPackages = pkgs.linuxPackagesFor pkgs.linux_imx8;
+    kernelPackages = pkgs.linuxPackagesFor (pkgs.callPackage ./bsp/linux-imx8.nix { });
     kernelParams = [ "console=ttyLP0,115200n8" ];
     loader.grub.enable = lib.mkDefault true;
     initrd.includeDefaultModules = lib.mkForce false;
