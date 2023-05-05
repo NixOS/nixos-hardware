db08b1f13fe125d3dfeac89745ea3f362442eede
Jörg Thalheim <joerg@thalheim.io>
2023-05-05 08:45:09 +0200

    tests: fix conflicts with profiles using grub

--- tests/build-profile.nix
+++ tests/build-profile.nix
@@ -3,6 +3,8 @@
 let
   shim = { config, ... }: {
     boot.loader.systemd-boot.enable = !config.boot.loader.generic-extlinux-compatible.enable && !config.boot.loader.raspberryPi.enable;
+    # we forcefully disable grub here just for testing purposes, even though some profiles might still use grub in the end.
+    boot.loader.grub.enable = false;
 
     fileSystems."/" = {
       device = "/dev/disk/by-uuid/00000000-0000-0000-0000-000000000000";
