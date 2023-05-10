c256df331235ce369fdd49c00989fdaa95942934
Alexandre Acebedo <alexandre@acebedo.fr>
2023-05-10 20:03:02 +0200

    cpu/amd/pstate: change the pstate mode to active

--- common/cpu/amd/pstate.nix
+++ common/cpu/amd/pstate.nix
@@ -1,6 +1,6 @@
-{ lib, config, ... }: 
+{ lib, config, ... }:
 let
-   kver = config.boot.kernelPackages.kernel.version;
+  kver = config.boot.kernelPackages.kernel.version;
 in
 {
   # Enables the amd cpu scaling https://www.kernel.org/doc/html/latest/admin-guide/pm/amd-pstate.html
@@ -8,15 +8,25 @@ in
 
   imports = [ ./. ];
   boot = lib.mkMerge [
-    (lib.mkIf (
-      (lib.versionAtLeast kver "5.17")
-      && (lib.versionOlder kver "6.1")
-    ) {
-      kernelParams = [ "initcall_blacklist=acpi_cpufreq_init" ];
-      kernelModules = [ "amd-pstate" ];
-    })
-    (lib.mkIf (lib.versionAtLeast kver "6.1") {
-      kernelParams = [ "amd_pstate=passive" ];  
+    (lib.mkIf
+      (
+        (lib.versionAtLeast kver "5.17")
+        && (lib.versionOlder kver "6.1")
+      )
+      {
+        kernelParams = [ "initcall_blacklist=acpi_cpufreq_init" ];
+        kernelModules = [ "amd-pstate" ];
+      })
+    (lib.mkIf
+      (
+        (lib.versionAtLeast kver "6.1")
+        && (lib.versionOlder kver "6.3")
+      )
+      {
+        kernelParams = [ "amd_pstate=passive" ];
+      })
+    (lib.mkIf (lib.versionAtLeast kver "6.3") {
+      kernelParams = [ "amd_pstate=active" ];
     })
   ];
 }
