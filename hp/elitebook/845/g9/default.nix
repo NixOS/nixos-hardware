bbacfd60126930faee6801c44bcda186151a22d6
Simon Guest <simon.guest@tesujimath.org>
2023-05-12 10:17:26 +1200

    PR review comment

--- hp/elitebook/845/g9/default.nix
+++ hp/elitebook/845/g9/default.nix
@@ -13,7 +13,7 @@
 
   hardware.enableRedistributableFirmware = lib.mkDefault true;
   boot.kernelModules = [ "synaptics_usb" ];
-  boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux_latest;
+  boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.3") (lib.mkDefault pkgs.linuxPackages_latest);
 
   # disable Scatter/Gather APU recently enabled by default,
   # which results in white screen after display reconfiguration
