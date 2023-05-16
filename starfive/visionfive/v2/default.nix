ff35653b1493d6ae27acb04bf6851e095b23ffea
Nick Cao <nickcao@nichi.co>
2023-05-16 19:42:48 +0800

    starfive visionfive2: update kernel to 6.4.0

--- starfive/visionfive/v2/default.nix
+++ starfive/visionfive/v2/default.nix
@@ -4,7 +4,7 @@
     supportedFilesystems =
       lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
     consoleLogLevel = lib.mkDefault 7;
-    kernelPackages = lib.mkDefault (pkgs.callPackage ./linux-6.3.nix {
+    kernelPackages = lib.mkDefault (pkgs.callPackage ./linux-6.4.nix {
       inherit (config.boot) kernelPatches;
     });
 
