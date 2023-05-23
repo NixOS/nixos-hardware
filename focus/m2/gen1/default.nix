ee1055d80ce5fcfe556e4c7185d9afcf922a7caf
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-05-23 07:29:46 +0200

    [FocusGen1M2] Disable TPM interrupt due to upstream bug

--- focus/m2/gen1/default.nix
+++ focus/m2/gen1/default.nix
@@ -11,6 +11,10 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelParams = [
+    "tpm_tis.interrupts=0" # Upstream bug: https://bugzilla.kernel.org/show_bug.cgi?id=204121
+    # without this, kacpid worker thread consumes 100% cpu on kernels 5.15.111 and up
+  ];
   boot.blacklistedKernelModules = [ "i2c_nvidia_gpu" ];
 
   hardware.nvidia.modesetting.enable = lib.mkDefault true;
