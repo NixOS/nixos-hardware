289a5af77e624b29b9efd06de262b324c8018abe
Space Banana <tomasbessa@protonmail.com>
2023-06-28 00:55:39 +0100

    Additional blocked nvidia kernel modules

--- common/gpu/nvidia/disable.nix
+++ common/gpu/nvidia/disable.nix
@@ -22,5 +22,5 @@
     # Remove NVIDIA VGA/3D controller devices
     ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
   '';
-  boot.blacklistedKernelModules = lib.mkDefault [ "nouveau" "nvidia" ];
+  boot.blacklistedKernelModules = lib.mkDefault [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
 }
