e5ea58213358b9037a00d0b1c2bcc5e4b0d46151
Ganga Ram <Ganga.Ram@tii.ae>
2023-07-05 11:56:44 +0400

    Microchip uboot build failure with nixpkgs 23.05
    Fix for https://github.com/NixOS/nixpkgs/issues/235179
    Update Microchip uboot version to linux4microchip+fpga-2023.06
    
    Signed-off-by: Ganga Ram <Ganga.Ram@tii.ae>

--- microchip/common/bsp/uboot.nix
+++ microchip/common/bsp/uboot.nix
@@ -8,20 +8,23 @@ with pkgs; let
 in
 buildUBoot rec {
   pname = "uboot";
-  version = "linux4microchip+fpga-2023.02";
+  version = "linux4microchip+fpga-2023.06";
 
   src = fetchFromGitHub {
     owner = "polarfire-soc";
     repo = "u-boot";
-    rev = "b356a897b11ef19dcbe7870530f23f3a978c1714";
-    sha256 = "sha256-ouNLnDBeEsaY/xr5tAVBUtLlj0eylWbKdlU+bQ2Ciq4=";
+    rev = "7e19f9dff788025403ac6a34d9acf8736eef32ff";
+    sha256 = "sha256-1qmifjjNxPOUWRgZdQk6Ld5KGQk/PypSRK/ILPSsTLs";
   };
 
   extraMakeFlags = [
           "OPENSBI=${opensbi}/share/opensbi/lp64/generic/firmware/fw_dynamic.bin"
   ];
 
-  patches = [ ./patches/0001-Boot-environment-for-Microchip-Iciclle-Kit.patch ];
+  patches = [
+    ./patches/0001-Boot-environment-for-Microchip-Iciclle-Kit.patch
+    ./patches/0002-Riscv-Fix-build-against-binutils-2.38.patch
+  ];
   defconfig = "${targetBoard}_defconfig";
   enableParallelBuilding = true;
   extraMeta.platforms = ["riscv64-linux"];
