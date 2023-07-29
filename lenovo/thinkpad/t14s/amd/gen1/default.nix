6804927dfb890979905de459f9d4a97c1837ded3
dadada <dadada@dadada.li>
2023-07-29 18:36:29 +0200

    p14s, t14s: remove linux-firmware version check
    nixpkgs is currently using the `unstable-` version
    
    See https://github.com/NixOS/nixpkgs/commit/beb6196e9ed1f59fa9c8884b2b9e2d0eafb67bbc

--- lenovo/thinkpad/t14s/amd/gen1/default.nix
+++ lenovo/thinkpad/t14s/amd/gen1/default.nix
@@ -1,12 +1,8 @@
-
 { lib, pkgs, ... }:
-
 {
   imports = [
     ../.
   ];
-  # Wifi support
-  hardware.firmware = lib.mkIf (lib.versionOlder pkgs.linux-firmware.version "20230210") [ pkgs.rtw89-firmware ];
 
   # For mainline support of rtw89 wireless networking
   boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "5.16") pkgs.linuxPackages_latest;
