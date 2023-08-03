6dcf1381ec3a77901bbd8b014b8a0e29157f9688
Matthieu Dubuget <matthieu.dubuget@gmail.com>
2023-08-03 21:50:38 +0200

    Adding Dell XPS 13 9333

---
+++ dell/xps/13-9333/default.nix
@@ -0,0 +1,15 @@
+{ lib, pkgs, ... }: {
+  imports = [
+    ../../../common/cpu/intel
+    ../../../common/pc/laptop
+    ../../../common/pc/ssd
+  ];
+
+  # Required to allow the touchpad to work
+  boot.kernelParams = [ "i8042.nopnp=1" ];
+  boot.blacklistedKernelModules = [ "i2c_hid" "i2c_hid_acpi" ];
+  boot.kernelModules = [ "synaptics_i2c"];
+
+  # Allows for updating firmware via `fwupdmgr`.
+  services.fwupd.enable = true;
+}
