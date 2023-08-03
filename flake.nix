df6673231f19e091b3202b1815b3531150a49fa8
Matthieu Dubuget <matthieu.dubuget@gmail.com>
2023-08-03 21:50:38 +0200

    Adding Dell XPS 13 9333

--- flake.nix
+++ flake.nix
@@ -39,6 +39,7 @@
       dell-xps-13-7390 = import ./dell/xps/13-7390;
       dell-xps-13-9300 = import ./dell/xps/13-9300;
       dell-xps-13-9310 = import ./dell/xps/13-9310;
+      dell-xps-13-9333 = import ./dell/xps/13-9333;
       dell-xps-13-9343 = import ./dell/xps/13-9343;
       dell-xps-13-9350 = import ./dell/xps/13-9350;
       dell-xps-13-9360 = import ./dell/xps/13-9360;
