c8cd4f7ef455de718b9c74a24862cc457b8dcf5d
Olof Sandell <83674246+osandell@users.noreply.github.com>
2023-08-09 21:36:53 +0200

    feat: integrate into t2 module

--- apple/t2/default.nix
+++ apple/t2/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   audioFiles = pkgs.fetchFromGitHub {
@@ -11,45 +11,85 @@ let
   overrideAudioFiles = package: pluginsPath:
     package.overrideAttrs (new: old: {
       preConfigurePhases = old.preConfigurePhases or [ ] ++ [ "postPatchPhase" ];
-
       postPatchPhase = ''
         cp -r ${audioFiles}/files/{profile-sets,paths} ${pluginsPath}/alsa/mixer/
       '';
     });
+
+  pipewirePackage = overrideAudioFiles pkgs.pipewire "spa/plugins/";
+
+  apple-set-os-loader-installer = pkgs.stdenv.mkDerivation rec {
+    name = "apple-set-os-loader-installer-1.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "Redecorating";
+      repo = "apple_set_os-loader";
+      rev = "r33.9856dc4";
+      sha256 = "hvwqfoF989PfDRrwU0BMi69nFjPeOmSaD6vR6jIRK2Y=";
+    };
+    buildInputs = [ pkgs.gnu-efi ];
+    buildPhase = ''
+      substituteInPlace Makefile --replace "/usr" '$(GNU_EFI)'
+      export GNU_EFI=${pkgs.gnu-efi}
+      make
+    '';
+    installPhase = ''
+      install -D bootx64_silent.efi $out/bootx64.efi
+    '';
+  };
+
+  t2Cfg = config.appleT2Config;
+
 in
 {
-  # For keyboard and touchbar
-  boot.kernelPackages = with pkgs; recurseIntoAttrs (linuxPackagesFor (callPackage ./pkgs/linux-t2.nix { }));
-  boot.initrd.kernelModules = [ "apple-bce" ];
-
-  # For audio
-  boot.kernelParams = [ "pcie_ports=compat" "intel_iommu=on" "iommu=pt" ];
-  services.udev.extraRules = builtins.readFile (pkgs.substitute {
-    src = "${audioFiles}/files/91-audio-custom.rules";
-    replacements = [ "--replace" "/usr/bin/sed" "${pkgs.gnused}/bin/sed" ];
-  });
-
-  hardware.pulseaudio.package = overrideAudioFiles pkgs.pulseaudio "src/modules/";
-
-  services.pipewire = rec {
-    package = overrideAudioFiles pkgs.pipewire "spa/plugins/";
-
-    wireplumber.package = pkgs.wireplumber.override {
-      pipewire = package;
+  options = {
+    appleT2Config.enableAppleSetOsLoader = lib.mkOption {
+      default = false;
+      type = lib.types.bool;
+      description = "Whether to enable the appleSetOsLoader activation script.";
     };
   };
 
-  # Make sure post-resume.service exists
-  powerManagement.enable = true;
+  config = {
+    # For keyboard and touchbar
+    boot.kernelPackages = with pkgs; recurseIntoAttrs (linuxPackagesFor (callPackage ./pkgs/linux-t2.nix { }));
+    boot.initrd.kernelModules = [ "apple-bce" ];
 
-  systemd.services.fix-keyboard-backlight-and-touchbar = {
-    path = [ pkgs.kmod ];
-    serviceConfig.ExecStart = ''${pkgs.systemd}/bin/systemd-inhibit --what=sleep --why="fixing keyboard backlight and touchbar must finish before sleep" --mode=delay ${./fix-keyboard-backlight-and-touchbar.sh}'';
-    serviceConfig.Type = "oneshot";
-    description = "reload touchbar driver and restart upower";
-    # must run at boot (and not too early), and after suspend
-    wantedBy = [ "display-manager.service" "post-resume.target" ];
-    # prevent running before suspend
-    after = [ "post-resume.target" ];
+    # For audio
+    boot.kernelParams = [ "pcie_ports=compat" "intel_iommu=on" "iommu=pt" ];
+    services.udev.extraRules = builtins.readFile (pkgs.substitute {
+      src = "${audioFiles}/files/91-audio-custom.rules";
+      replacements = [ "--replace" "/usr/bin/sed" "${pkgs.gnused}/bin/sed" ];
+    });
+
+    hardware.pulseaudio.package = overrideAudioFiles pkgs.pulseaudio "src/modules/";
+
+    services.pipewire.package = pipewirePackage;
+    services.pipewire.wireplumber.package = pkgs.wireplumber.override {
+      pipewire = pipewirePackage;
+    };
+
+    # Make sure post-resume.service exists
+    powerManagement.enable = true;
+
+    systemd.services.fix-keyboard-backlight-and-touchbar = {
+      path = [ pkgs.kmod ];
+      serviceConfig.ExecStart = ''${pkgs.systemd}/bin/systemd-inhibit --what=sleep --why="fixing keyboard backlight and touchbar must finish before sleep" --mode=delay ${./fix-keyboard-backlight-and-touchbar.sh}'';
+      serviceConfig.Type = "oneshot";
+      description = "reload touchbar driver and restart upower";
+      wantedBy = [ "display-manager.service" "post-resume.target" ];
+      after = [ "post-resume.target" ];
+    };
+
+    # Activation script to install apple-set-os-loader in order to unlock the iGPU 
+    system.activationScripts.appleSetOsLoader = lib.optionalString t2Cfg.enableAppleSetOsLoader ''
+      if [[ -e /boot/efi/EFI/BOOT/bootx64_original.efi ]]; then
+        true # It's already installed, no action required
+      elif [[ -e /boot/efi/EFI/BOOT/BOOTX64.EFI ]]; then
+        mv /boot/efi/EFI/BOOT/BOOTX64.EFI /boot/efi/EFI/BOOT/bootx64_original.efi
+        cp ${apple-set-os-loader-installer}/bootx64.efi /boot/efi/EFI/BOOT/bootx64.efi
+      else
+        echo "Error: /boot/efi/EFI/BOOT/BOOTX64.EFI is missing" >&2
+      fi
+    '';
   };
 }
