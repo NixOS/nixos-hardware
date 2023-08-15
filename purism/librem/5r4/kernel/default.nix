6a1fb21810cc075f33d55a134c84d1039db10291
Sophie Tauchert <sophie@999eagle.moe>
2023-08-15 23:36:16 +0200

    purism librem5r4: update kernel to 6.4.5

--- purism/librem/5r4/kernel/default.nix
+++ purism/librem/5r4/kernel/default.nix
@@ -1,4 +1,4 @@
 final: prev: {
-  linuxPackages_librem5_6_1_10 = final.linuxPackagesFor (final.callPackage ./6.1.10.nix { });
-  linuxPackages_librem5 = final.linuxPackages_librem5_6_1_10;
+  linuxPackages_librem5_6_4_5 = final.linuxPackagesFor (final.callPackage ./6.4.5.nix { });
+  linuxPackages_librem5 = final.linuxPackages_librem5_6_4_5;
 }
