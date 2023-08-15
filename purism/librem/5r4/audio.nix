add6fa551ef664d3ff4a36f30ef6928e4bd09dbd
Sophie Tauchert <sophie@999eagle.moe>
2023-08-16 00:23:36 +0200

    purism librem5r4: add configuration option for audio

---
+++ purism/librem/5r4/audio.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+{
+  config = lib.mkIf config.hardware.librem5.audio {
+    assertions = [{
+      assertion = config.hardware.pulseaudio.enable;
+      message = "Call audio on Librem5 requires pulse audio to be enabled through `hardware.pulseaudio.enable`.";
+    }];
+    hardware.pulseaudio = {
+      enable = true;
+      # this is required to correctly configure the modem as PA source/sink
+      extraConfig = ''
+        .include ${pkgs.librem5-base}/etc/pulse/librem5.pa
+      '';
+    };
+
+    services.dbus.packages = [ pkgs.callaudiod ];
+  };
+}
