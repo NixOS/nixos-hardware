4fb5bddf60eb22e615a5b5c1e22a5b77c3c8737e
Sophie Tauchert <sophie@999eagle.moe>
2023-09-04 15:37:10 +0200

    purism librem5r4 linux: 6.4.5-librem5 -> 6.4.14-librem5

--- purism/librem/5r4/kernel/default.nix
+++ purism/librem/5r4/kernel/default.nix
@@ -1,4 +1,4 @@
 final: prev: {
-  linuxPackages_librem5_6_4_5 = final.linuxPackagesFor (final.callPackage ./6.4.5.nix { });
-  linuxPackages_librem5 = final.linuxPackages_librem5_6_4_5;
+  linuxPackages_librem5_6_4_14 = final.linuxPackagesFor (final.callPackage ./6.4.14.nix { });
+  linuxPackages_librem5 = final.linuxPackages_librem5_6_4_14;
 }
