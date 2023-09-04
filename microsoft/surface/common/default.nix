4cd9ced7fa0c10668a321546912adb8cbdc9369b
Stig Palmquist <git@stig.io>
2023-09-04 16:07:13 +0200

    surface: linux 6.4.12 -> 6.4.14

--- microsoft/surface/common/default.nix
+++ microsoft/surface/common/default.nix
@@ -10,7 +10,7 @@ in {
     ./surface-control
   ];
 
-  microsoft-surface.kernelVersion = mkDefault "6.4.12";
+  microsoft-surface.kernelVersion = mkDefault "6.4.14";
 
   # Seems to be required to properly enable S0ix "Modern Standby":
   boot.kernelParams = mkDefault [ "mem_sleep_default=deep" ];
