2d1a0da5e3fdb1585874273907f152c1ca5ba242
Oxters Wyzgowski <oxters168@gmail.com>
2023-09-11 07:38:26 -0400

    asus/zephyrus/ga401: fix keymapping
    Mapped fn+LeftArrow to be home key and fn+RightArrow to be end key.
    The Asus Zephyrus GA401 does not have any home end functionality originally.

--- asus/zephyrus/ga401/default.nix
+++ asus/zephyrus/ga401/default.nix
@@ -26,10 +26,11 @@
   services = {
     asusd.enable = lib.mkDefault true;
 
-    # fixes mic mute button
     udev.extraHwdb = ''
       evdev:name:*:dmi:bvn*:bvr*:bd*:svnASUS*:pn*:*
-       KEYBOARD_KEY_ff31007c=f20
+       KEYBOARD_KEY_ff31007c=f20    # fixes mic mute button
+       KEYBOARD_KEY_ff3100b2=home   # Set fn+LeftArrow as Home
+       KEYBOARD_KEY_ff3100b3=end    # Set fn+RightArrow as End
     '';
   };
 }
