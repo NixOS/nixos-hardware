8032cb7a80044ef797667a8bbfff01efd36a5100
Astrid Yu <astrid@astrid.tech>
2023-09-11 14:27:30 -0700

    microsoft/surface: Bump kernel 6.1.18 -> 6.1.53

--- microsoft/surface/common/kernel/linux-6.1.53/default.nix
+++ microsoft/surface/common/kernel/linux-6.1.53/default.nix
@@ -8,7 +8,7 @@ let
 
   cfg = config.microsoft-surface;
 
-  version = "6.1.18";
+  version = "6.1.53";
   extraMeta.branch = "6.1";
   patchDir = repos.linux-surface + "/patches/${extraMeta.branch}";
   kernelPatches = pkgs.callPackage ./patches.nix {
@@ -20,17 +20,17 @@ let
     inherit version extraMeta kernelPatches;
     src = fetchurl {
       url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-      sha256 = "sha256-hCrBXv8Ob7DBUP34P09qr2tMEjnc+MFOIidiDsCuFB4=";
+      sha256 = "sha256-X1fgoEgQ0k8rGo/JVFEkH4BTDmeHF+2g9FEExtx47X4=";
     };
   };
 
 
 in {
   options.microsoft-surface.kernelVersion = mkOption {
-    type = types.enum [ "6.1.18" ];
+    type = types.enum [ "6.1.53" ];
   };
 
-  config = mkIf (cfg.kernelVersion == "6.1.18") {
+  config = mkIf (cfg.kernelVersion == "6.1.53") {
     boot = {
       inherit kernelPackages;
     };
