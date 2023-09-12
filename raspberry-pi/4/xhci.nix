f8d3c754626e0ff3a0b8906fc0a22f9bda20c6a2
Andrii Panasiuk <andrew.panassiouk@gmail.com>
2023-09-12 17:21:37 +0300

    Implement dt overlay for enabling built-in xhci controller

---
+++ raspberry-pi/4/xhci.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.hardware.raspberry-pi."4".xhci;
+in {
+  options.hardware = {
+    raspberry-pi."4".xhci = {
+      enable = lib.mkEnableOption ''
+        Enable builtin XHCI controller for USB with otg_mode=1 in config.txt
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    hardware.deviceTree = {
+      overlays = [
+        {
+          name = "enable-xhci";
+          dtsText = ''
+            /dts-v1/;
+            /plugin/;
+
+            / {
+              compatible = "brcm,bcm2711";
+              fragment@0 {
+                //target-path = "/scb/xhci@7e9c0000";
+                target = <&xhci>;
+                __overlay__ {
+                  status = "okay";
+                };
+              };
+            };
+          '';
+        }
+      ];
+    };
+  };
+}
