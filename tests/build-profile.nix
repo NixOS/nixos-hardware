cf88c13ca636e7520bb6922cb31a800075657c75
Jörg Thalheim <joerg@thalheim.io>
2023-09-19 06:55:17 +0200

    tests: set linux_latest as the default kernel

--- tests/build-profile.nix
+++ tests/build-profile.nix
@@ -1,10 +1,14 @@
 { profile }:
 
 let
-  shim = { config, ... }: {
+  shim = { config, lib, pkgs, ... }: {
     boot.loader.systemd-boot.enable = !config.boot.loader.generic-extlinux-compatible.enable && !config.boot.loader.raspberryPi.enable;
     # we forcefully disable grub here just for testing purposes, even though some profiles might still use grub in the end.
     boot.loader.grub.enable = false;
+
+    # so we can have assertions that require a certain minimum kernel version,
+    # We use a priority of 1200 here, which is higher than the default of 1000
+    boot.kernelPackages = lib.mkOverride 1200 pkgs.linuxPackages_latest;
 
     fileSystems."/" = {
       device = "/dev/disk/by-uuid/00000000-0000-0000-0000-000000000000";
