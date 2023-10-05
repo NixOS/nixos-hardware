a149e3d37e77543205f73017015fe22632907ff9
Kiara Grouwstra <kiara@bij1.org>
2023-10-06 01:17:51 +0200

    Lenovo IdeaPad Slim 5: init note this is to be distinguished from e.g. the slim 5i, which uses intel

--- flake.nix
+++ flake.nix
@@ -77,6 +77,7 @@
       intel-nuc-8i7beh = import ./intel/nuc/8i7beh;
       lenovo-ideapad-15arh05 = import ./lenovo/ideapad/15arh05;
       lenovo-ideapad-z510 = import ./lenovo/ideapad/z510;
+      lenovo-ideapad-slim-5 = import ./lenovo/ideapad/slim-5;
       lenovo-legion-15ach6 = import ./lenovo/legion/15ach6;
       lenovo-legion-15arh05h = import ./lenovo/legion/15arh05h;
       lenovo-legion-16ach6h = import ./lenovo/legion/16ach6h;
