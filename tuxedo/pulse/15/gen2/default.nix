256f598a1b48455fd8b844d6e29cda08b9674c5b
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-10-09 15:42:46 +0200

    fix(tuxedo/pulse/15/gen2): properly suspend the system

--- tuxedo/pulse/15/gen2/default.nix
+++ tuxedo/pulse/15/gen2/default.nix
@@ -1,7 +1,20 @@
 {
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     ../../../../common/cpu/amd
     ../../../../common/gpu/amd
     ../../../../common/pc/laptop/ssd
   ];
+
+  services.udev.extraRules = lib.concatStringsSep "\n" (
+    ["# Properly suspend the system."]
+    ++ (
+      map
+      (device: "SUBSYSTEM==\"pci\", ACTION==\"add\", ATTR{vendor}==\"0x144d\", ATTR{device}==\"${device}\", RUN+=\"${pkgs.bash}/bin/sh -c '${pkgs.coreutils}/bin/echo 0 | ${pkgs.coreutils}/bin/tee /sys/bus/pci/devices/$kernel/d3cold_allowed'\"")
+      ["0xa80a" "0xa808"]
+    )
+  );
 }
