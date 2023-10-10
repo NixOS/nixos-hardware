ccf6368108fd52c7c9436df62c82ed0fbe0b844d
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-10-10 18:10:02 +0200

    refactor(tuxedo/pulse/15/gen2): revert device expression simplification
    This reverts commit 6eeede605f8d181b105c40f3837c57714d65d061.

--- tuxedo/pulse/15/gen2/default.nix
+++ tuxedo/pulse/15/gen2/default.nix
@@ -15,7 +15,7 @@
     ["# Properly suspend the system."]
     ++ (
       map
-      (device: ''SUBSYSTEM=="pci", ACTION=="add", ATTR{vendor}=="0x144d", ATTR{device}=="${device}", RUN+="${pkgs.runtimeShell} -c 'echo 0 > /sys/bus/pci/devices/$kernel/d3cold_allowed'"'')
+      (device: "SUBSYSTEM==\"pci\", ACTION==\"add\", ATTR{vendor}==\"0x144d\", ATTR{device}==\"${device}\", RUN+=\"${pkgs.bash}/bin/sh -c '${pkgs.coreutils}/bin/echo 0 | ${pkgs.coreutils}/bin/tee /sys/bus/pci/devices/$kernel/d3cold_allowed'\"")
       ["0xa80a" "0xa808"]
     )
   );
