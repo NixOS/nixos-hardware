c1ebe3b471bdb097c07579748713b51fd835098d
Mic92 <96200+Mic92@users.noreply.github.com>
2023-10-11 18:07:00 +0000

    init: omen-16-n0005ne (#749) added omen 16-n0005ne profile
    * removed /common/pc/ssd
    
    * added /common/pc/ssd
    
    * removed /common/pc/laptop from omen 16 config
    
    * added /common/pc/laptop
    
    * Merge remote-tracking branch 'upstream/master'
    
    * Merge remote-tracking branch 'upstream/master'
    
    * chore: Add DELL XPS 9510
    
    * chore: Add Dell XPS 15 9510
    
    * starfive visionfive2: update u-boot to SDK version v3.7.5
    
    * added omen 16-n0005ne profile
    
    * removed /common/pc/ssd
    
    * added /common/pc/ssd
    
    * removed /common/pc/laptop from omen 16 config
    
    * added /common/pc/laptop
    
    * Merge branch 'master' of github.com:GamingTesh/nixos-hardware
    
    * fixed error in flake.nix caused by git

---
+++ omen/16-n0005ne/default.nix
@@ -0,0 +1,15 @@
+{ lib, pkgs, ... }:
+
+{
+  imports = [
+    ../../common/cpu/amd
+    ../../common/gpu/amd
+    ../../common/pc/laptop
+    ../../common/pc/ssd
+  ];
+
+  # Enables ACPI platform profiles
+  boot = lib.mkIf (lib.versionAtLeast pkgs.linux.version "6.1") {
+    kernelModules = [ "hp-wmi" ];
+  };
+}
