32264b21bb23fbc00672b3ed6fcfaa5489d03991
Sophie Tauchert <sophie@999eagle.moe>
2023-10-14 00:45:07 +0200

    purism/librem5r4: linuxPackages_librem5: 6.5.4-librem5 -> 6.5.6-librem5

--- purism/librem/5r4/kernel/default.nix
+++ purism/librem/5r4/kernel/default.nix
@@ -1,4 +1,3 @@
 final: prev: {
-  linuxPackages_librem5_6_5_4 = final.linuxPackagesFor (final.callPackage ./6.5.4.nix { });
-  linuxPackages_librem5 = final.linuxPackages_librem5_6_5_4;
+  linuxPackages_librem5 = final.linuxPackagesFor (final.callPackage ./kernel.nix { });
 }
--- purism/librem/5r4/kernel/kernel.nix
+++ purism/librem/5r4/kernel/kernel.nix
@@ -6,14 +6,14 @@
 buildLinux (args
   // rec {
   defconfig = "librem5_defconfig";
-  version = "6.5.4-librem5";
+  version = "6.5.6-librem5";
   modDirVersion = version;
   src = fetchFromGitLab {
     domain = "source.puri.sm";
     owner = "Librem5";
     repo = "linux";
-    rev = "pureos/6.5.4pureos1";
-    hash = "sha256-wPosmPiQiaODfxlxNA/PGqOIBKlEcO+pOPvdZn7R9T0=";
+    rev = "pureos/6.5.6pureos1";
+    hash = "sha256-hOv0oy31mbC+43sI1n1oqKl7TtT/Ivj6UhiW4maumCg=";
   };
   kernelPatches = [ ];
   structuredExtraConfig = with lib.kernel; {
