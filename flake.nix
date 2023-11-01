7a5ab49a854c1e7f692643266e1a455b2234b2a9
srestegosaurio <greenestegosaurio@protonmail.com>
2023-11-01 03:30:47 +0100

    feat(asus/rog-strix/g513im): added profile

--- flake.nix
+++ flake.nix
@@ -17,6 +17,7 @@
       asus-battery = import ./asus/battery.nix;
       asus-ally-rc71l = import ./asus/ally/rc71l;
       asus-fx504gd = import ./asus/fx504gd;
+      asus-rog-strix-g513im = import ./asus/rog-strix/g513im;
       asus-rog-strix-g733qs = import ./asus/rog-strix/g733qs;
       asus-zephyrus-ga401 = import ./asus/zephyrus/ga401;
       asus-zephyrus-ga402 = import ./asus/zephyrus/ga402;
