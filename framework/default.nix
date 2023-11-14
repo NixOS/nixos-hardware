b03735bd0098cda6c2cc5fa02301b82573baef9c
Naufal Fikri <naufal@naufik.net>
2023-11-14 22:34:58 +1100

    Fixed broken links in deprecation assertions for framework and surface modules.

--- framework/default.nix
+++ framework/default.nix
@@ -2,7 +2,7 @@
   assertions = [
     {
       assertion = false;
-      message = "Importing framework/ (default.nix) directly is deprecated! See https://github.com/NixOS/nixos-hardware/blob/master/framework/OLD-BEHAVIOUR-DEPRECATED.md for more details.";
+      message = "Importing framework/ (default.nix) directly is deprecated! See https://github.com/NixOS/nixos-hardware/blob/master/framework/OLD-BEHAVIOUR-DEPRECATION.md for more details.";
     }
   ];
 }
