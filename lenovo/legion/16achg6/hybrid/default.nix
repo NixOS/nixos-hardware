b927cb545a264518ebfe80af51d0e273629d161a
Alojzy Leszcz <aleszcz@pm.me>
2023-11-25 21:47:45 +0100

    Commenting out more stuff to find the one that breaks everything.

--- lenovo/legion/16achg6/hybrid/default.nix
+++ lenovo/legion/16achg6/hybrid/default.nix
@@ -4,8 +4,8 @@
   imports = [
     #../../../../common/cpu/amd
     #../../../../common/cpu/amd/pstate.nix
-    ../../../../common/gpu/amd
-    ../../../../common/gpu/nvidia/prime.nix
+    #../../../../common/gpu/amd
+    #../../../../common/gpu/nvidia/prime.nix
     #../../../../common/pc/laptop
     #../../../../common/pc/laptop/ssd
   ];
@@ -13,8 +13,15 @@
   hardware = {
     #amdgpu.loadInInitrd = lib.mkDefault false;
 
+    # opengl = {
+    #   enable = true;
+    #   driSupport = true;
+    #   driSupport32Bit = true;
+    # };
+
     nvidia = {
       modesetting.enable = lib.mkDefault true;
+      open = lib.mkDefault false;
       powerManagement.enable = lib.mkDefault true;
 
       prime = {
