be1f5336179465c126526ce125add31d4392f349
Alojzy Leszcz <aleszcz@pm.me>
2023-12-10 19:14:31 +0100

    Restoring specialisation.

--- lenovo/legion/16achg6/hybrid/default.nix
+++ lenovo/legion/16achg6/hybrid/default.nix
@@ -25,4 +25,12 @@
       };
     };
   };
+
+  specialisation = {
+    external-display.configuration = {
+      system.nixos.tags = [ "external-display" ];
+      hardware.nvidia.prime.offload.enable = lib.mkForce false;
+      hardware.nvidia.powerManagement.enable = lib.mkForce false;
+    };
+  };
 }
