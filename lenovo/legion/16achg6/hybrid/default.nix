7b3a5ee519e6b07ac8f6832a919a8d65fb199eb6
Alojzy Leszcz <aleszcz@pm.me>
2023-12-10 19:24:00 +0100

    One more attempt at removing specialisation.

--- lenovo/legion/16achg6/hybrid/default.nix
+++ lenovo/legion/16achg6/hybrid/default.nix
@@ -26,12 +26,4 @@
       };
     };
   };
-
-  specialisation = {
-    external-display.configuration = {
-      system.nixos.tags = [ "external-display" ];
-      hardware.nvidia.prime.offload.enable = lib.mkForce false;
-      hardware.nvidia.powerManagement.enable = lib.mkForce false;
-    };
-  };
 }
