e4ded1ec8e9c152910c2b077443e1d87cd81ad4a
Cryolitia <Cryolitia@gmail.com>
2023-12-25 09:04:38 +0800

    gpd-win-max-2-2023: init

--- flake.nix
+++ flake.nix
@@ -78,6 +78,7 @@
       gpd-p2-max = import ./gpd/p2-max;
       gpd-pocket-3 = import ./gpd/pocket-3;
       gpd-win-2 = import ./gpd/win-2;
+      gpd-win-max-2-2023 = import ./gpd/win-max-2/2023;
       hp-elitebook-2560p = import ./hp/elitebook/2560p;
       hp-elitebook-845g7 = import ./hp/elitebook/845/g7;
       hp-elitebook-845g9 = import ./hp/elitebook/845/g9;
