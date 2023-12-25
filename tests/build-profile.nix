65753f5d1144e3bf468a6bfe464011b12c840792
Jörg Thalheim <joerg@thalheim.io>
2023-12-25 22:45:38 +0100

    speed up ci using nix-eval-jobs

--- tests/build-profile.nix
+++ tests/build-profile.nix
@@ -1,7 +1,9 @@
-{ profile }:
+{ profile, pkgs }:
 
-let
-  shim = { config, lib, pkgs, ... }: {
+(pkgs.nixos [
+  profile
+  ({ config, lib, ... }: {
+    nixpkgs.pkgs = pkgs;
     boot.loader.systemd-boot.enable = !config.boot.loader.generic-extlinux-compatible.enable && !config.boot.loader.raspberryPi.enable;
     # we forcefully disable grub here just for testing purposes, even though some profiles might still use grub in the end.
     boot.loader.grub.enable = false;
@@ -14,13 +16,6 @@ let
       device = "/dev/disk/by-uuid/00000000-0000-0000-0000-000000000000";
       fsType = "btrfs";
     };
-
-    nixpkgs.config = {
-      allowBroken = true;
-      allowUnfree = true;
-      nvidia.acceptLicense = true;
-    };
-  };
-in (import <nixpkgs/nixos> {
-  configuration.imports = [ profile shim ];
-}).system
+    system.stateVersion = lib.version;
+  })
+]).config.system.build.toplevel
