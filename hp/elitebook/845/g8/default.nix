f752581d6723a10da7dfe843e917a3b5e4d8115a
Keanu Kerr <keanu@kerr.us>
2024-01-01 13:19:05 +0100

    Added file for HP EliteBook 845 G8 and updated flake and README

---
+++ hp/elitebook/845/g8/default.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib, ... }:
+
+{
+  imports =
+    [
+      ../../../../common/cpu/amd
+      ../../../../common/cpu/amd/pstate.nix
+      ../../../../common/gpu/amd
+      ../../../../common/pc/laptop
+      ../../../../common/pc/laptop/acpi_call.nix
+      ../../../../common/pc/laptop/ssd
+    ];
+
+  hardware.enableRedistributableFirmware = lib.mkDefault true;
+  boot.kernelModules = [ "synaptics_usb" ];
+  boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.3") (lib.mkDefault pkgs.linuxPackages_latest);
+
+  # disable Scatter/Gather APU recently enabled by default,
+  # which results in white screen after display reconfiguration
+  boot.kernelParams = [ "amdgpu.sg_display=0" ];
+
+  services.xserver = {
+    videoDrivers = [ "amdgpu" ];
+  };
+}
