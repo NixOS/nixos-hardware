8b34e972fb99dd7561b6a8d3aacdfb36878c1c5a
Keanu Kerr <keanu@kerr.us>
2024-01-01 13:19:05 +0100

    Added file for HP EliteBook 845 G8 and updated flake and README

--- flake.nix
+++ flake.nix
@@ -82,6 +82,7 @@
       gpd-win-max-2-2023 = import ./gpd/win-max-2/2023;
       hp-elitebook-2560p = import ./hp/elitebook/2560p;
       hp-elitebook-845g7 = import ./hp/elitebook/845/g7;
+      hp-elitebook-845g8 = import ./hp/elitebook/845/g8;
       hp-elitebook-845g9 = import ./hp/elitebook/845/g9;
       hp-notebook-14-df0023 = import ./hp/notebook/14-df0023;
       intel-nuc-8i7beh = import ./intel/nuc/8i7beh;
