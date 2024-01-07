d908ebd46a3cbe7dd81270ea48367a1274bdc7f0
Cryolitia <Cryolitia@gmail.com>
2024-01-07 19:31:20 +0800

    surface: linux 6.1.62 -> 6.1.71

--- microsoft/surface/common/kernel/linux-6.1.x/default.nix
+++ microsoft/surface/common/kernel/linux-6.1.x/default.nix
@@ -8,7 +8,7 @@ let
 
   cfg = config.microsoft-surface;
 
-  version = "6.1.62";
+  version = "6.1.71";
   majorVersion = "6.1";
   patchDir = repos.linux-surface + "/patches/${majorVersion}";
   kernelPatches = pkgs.callPackage ./patches.nix {
@@ -21,7 +21,7 @@ let
     extraMeta.branch = majorVersion;
     src = fetchurl {
       url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-      sha256 = "sha256-uf1hb6zWvs/O74i5vnGNDxZiXKs/6B0ROEgCpwkehew=";
+      sha256 = "sha256-Lfd03VP5/9Tlfr+ATPWXcJKV32owT+Jh0lIgoTS38EE=";
     };
   };
 
