7208c89eb314d7e8d055c2362c98d8f2b7ca33dc
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2024-01-12 23:14:09 -0500

    include modules in early load

--- asus/fa706iu/default.nix
+++ asus/fa706iu/default.nix
@@ -7,12 +7,19 @@
     ../../common/pc/laptop  
   ];
   # These are the BusID's for this device.
-  hardware.nvidia.prime = {
-    offload.enable = lib.mkDefault true;
-    amdgpuBusId = "PCI:6:0:0";
-    nvidiaBusId = "PCI:1:0:0";
+  hardware.nvidia = {
+    modesetting.enable = lib.mkDefault true;
+    powerManagement.enable = lib.mkDefault false;
+    powerManagement.finegrained = lib.mkDefault false;
+    open = lib.mkDefault false;
+    nvidiaSettings = lib.mkDefault true;
+    package = lib.mkDefault config.boot.kernelPackages.nvidiaPackages.stable;
+    prime = {
+      offload.enable = lib.mkDefault true;
+      amdgpuBusId = "PCI:6:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
-  # force kernel to use iGPU first
-  boot.initrd.kernelModules = [ "amdgpu" ];
-
+  #Early KMS
+  boot.initrd.kernelModules = [ "amdgpu" "nvidia"];
 }
