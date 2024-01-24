abff72bb97ac85cdd192f32aecbed914ead928db
Stig Palmquist <git@stig.io>
2024-01-24 09:25:36 +0100

    surface: linux 6.6.10 -> 6.6.13

--- microsoft/surface/common/kernel/linux-6.6.x/default.nix
+++ microsoft/surface/common/kernel/linux-6.6.x/default.nix
@@ -8,7 +8,7 @@ let
 
   cfg = config.microsoft-surface;
 
-  version = "6.6.10";
+  version = "6.6.13";
   majorVersion = "6.6";
   patchDir = repos.linux-surface + "/patches/${majorVersion}";
   kernelPatches = pkgs.callPackage ./patches.nix {
@@ -21,7 +21,7 @@ let
     extraMeta.branch = majorVersion;
     src = fetchurl {
       url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-      sha256 = "sha256-nuYn5MEJrsf8o+2liY6B0gGvLH6y99nX2UwfDhIFVGw=";
+      sha256 = "sha256-iLiefdQerU46seQRyLuNWSV1rPgVzx3zwNxX4uiCwLw=";
     };
   };
 
