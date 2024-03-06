efa221766cb2fd882d3973e89f1a5e6571dbf74f
Nire Bryce <nire@computernope.net>
2024-03-06 16:13:20 -0500

    gigabyte b550 suspend fix
    fixes b550 suspend bug by setting up systemd services that disable GPP0 and GPP8 in /proc/acpi/wakeup

---
+++ gigabyte/b550/b550-fix-suspend.nix
@@ -0,0 +1,40 @@
+{ lib } :
+
+{
+  systemd.services.bugfixSuspend-GPP0 = {
+    enable            = lib.mkDefault true;
+    description       = "Fix crash on wakeup from suspend/hibernate (b550 bugfix)";
+    unitConfig = {
+      Type            = "oneshot";
+    };
+    serviceConfig = {
+      User            = "root"; # root may not be necessary
+      ExecStart       = "-${pkgs.bash}/bin/bash -c 'echo 'GPP8' > /proc/acpi/wakeup'";
+      RemainAfterExit = "yes";  # required to not toggle when `nixos-rebuild switch` is ran
+      
+    };
+    wantedBy = ["multi-user.target"];
+  };
+
+  systemd.services.bugfixSuspend-GPP8 = {
+    enable            = lib.mkDefault true;
+    description       = "Fix crash on wakeup from suspend/hibernate (b550 bugfix)";
+    unitConfig = {
+      Type            = "oneshot";
+    };
+    serviceConfig = {
+      User            = "root";
+      ExecStart       = "-${pkgs.bash}/bin/bash -c 'echo 'GPP8' > /proc/acpi/wakeup'";
+      RemainAfterExit = "yes";
+    };
+    wantedBy = ["multi-user.target"];
+  };
+
+}
+
+
+
+
+
+
+
