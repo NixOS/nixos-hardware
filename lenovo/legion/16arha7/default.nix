5e3dc04ade0b1a6016dc7f52b0415cd7f1ae2517
Andre <andre@8bitbuddhism.com>
2024-04-06 20:20:54 -0400

    Add Lenovo Legion SLim 7 Gen 7 (AMD) 16ARHA7

---
+++ lenovo/legion/16arha7/default.nix
@@ -0,0 +1,24 @@
+{ config, ... }:
+
+let
+  lenovo-speaker-fix = pkgs.callPackage ./audio/lenovo-16ARHA7_speaker-fix.nix {
+    # Make sure the module targets the same kernel as your system is using.
+    inherit (config.boot.kernelPackages) kernel;
+  };
+in
+{
+  imports = [
+    ../../../common/cpu/amd
+    ../../../common/cpu/amd/pstate.nix
+    ../../../common/gpu/amd
+    ../../../common/pc/laptop
+    ../../../common/pc/laptop/ssd
+  ];
+
+  boot = {
+    extraModulePackages = [ lenovo-speaker-fix ];
+  };
+  
+  # √(2560² + 1600²) px / 16 in ≃ 189 dpi
+  services.xserver.dpi = 189;
+}
