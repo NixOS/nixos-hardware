3ee0f630ce13287cc37197e32150690e4d07709a
Mic92 <96200+Mic92@users.noreply.github.com>
2024-04-10 14:40:48 +0000

    Lenovo ThinkPad Z13 Gen 2 (#844) lenovo-thinkpad-z: remove HiDPI
    * lenovo-thinkpad-z: move z13 to gen1/z13, leave generic settings
    
    * lenovo-thinkpad-z-gen2: add z13
    
    * Update lenovo/thinkpad/z/gen2/default.nix
    
    * Update lenovo/thinkpad/z/gen2/default.nix

---
+++ lenovo/thinkpad/z13/default.nix
@@ -0,0 +1,10 @@
+{
+  assertions = [
+    {
+      assertion = true;
+      message = ''
+        Lenovo Z-series received a second generation so the hardware configuration has been split by generation. For the Z13 Gen 1 config, change from `lenovo-thinkpad-z13` to `lenovo-thinkpad-z13-gen1`.
+      '';
+    }
+  ];
+}
