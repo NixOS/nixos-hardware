1bcf716420ac6152edcd1277c743292da6b87529
Nick Cao <nickcao@nichi.co>
2024-04-26 21:25:26 -0400

    starfive visionfive2: use nixpkgs default u-boot version

--- starfive/visionfive/v2/uboot.nix
+++ starfive/visionfive/v2/uboot.nix
@@ -1,29 +1,9 @@
-{ lib
-, fetchFromGitHub
-, buildUBoot
-, buildPackages
+{ buildUBoot
 , opensbi
 }:
 
-buildUBoot rec {
-  version = "2024.01";
-
-  src = fetchFromGitHub {
-    owner = "u-boot";
-    repo = "u-boot";
-    rev = "refs/tags/v${version}";
-    hash = "sha256-0Da7Czy9cpQ+D5EICc3/QSZhAdCBsmeMvBgykYhAQFw=";
-  };
-
-  # workaround for https://github.com/NixOS/nixpkgs/pull/146634
-  # uboot: only apply raspberry pi patches to raspberry pi builds
-  patches = [ ];
-
+buildUBoot {
   extraMakeFlags = [
-    # workaround for https://github.com/NixOS/nixpkgs/pull/277997
-    # buildUBoot: specify absolute path of dtc, fix building u-boot 2023.10+
-    "DTC=${lib.getExe buildPackages.dtc}"
-
     "OPENSBI=${opensbi}/share/opensbi/lp64/generic/firmware/fw_dynamic.bin"
   ];
 
