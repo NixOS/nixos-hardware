1a770577450681a835733a0f50a87a71ff5e4ee0
katsu-chan <44003480+katsu-chan@users.noreply.github.com>
2024-04-28 20:57:14 +0300

    added thinkpad a470

---
+++ lenovo/thinkpad/a475/default.nix
@@ -0,0 +1,16 @@
+{ ... }:
+{
+  imports = [
+    ../../../common/gpu/amd
+    ../../../common/cpu/amd
+    ../../../common/cpu/amd/pstate.nix
+    ../../../common/pc/laptop/acpi_call.nix
+    ../../../common/pc/ssd
+    ../../../common/pc/laptop/hdd
+    #../../../common/hidpi.nix                #hidpi
+    ../.
+  ];
+
+  boot.kernelParams = [ "i8042.nomux=1" "i8042.reset" ]; # Fix trackpoint and touchpad working only after reboot
+}
+
