69c3b823047bade33157cc6218ae55b7c031d9ee
Mic92 <96200+Mic92@users.noreply.github.com>
2024-05-26 08:04:14 +0000

    Added config for Huawei Matebook X Pro (2020) (#957)
    Created a config file for Huawei Matebook X Pro (2020). This file should work on all versions other than MACHC-WAH9L as that one does not include the Nvidia MX250 dGPU.

---
+++ huawei/machc-wa/default.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: {
+  imports = [
+    ../../common/cpu/intel
+    ../../common/cpu/intel/comet-lake
+    ../../common/gpu/nvidia
+    ../../common/gpu/nvidia/prime.nix
+    ../../common/hidpi.nix
+    ../../common/pc/laptop
+    ../../common/pc/ssd
+  ];
+
+  hardware.nvidia = {
+    modesetting.enable = lib.mkDefault true;
+    open = lib.mkDefault false;
+    nvidiaSettings = lib.mkDefault true;
+    prime = {
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+  services.power-profiles-daemon.enable = lib.mkDefault true;
+}
