1e3c2a85da63a4eb6dae4fa978064e47bcdbbe74
Nick Cao <nickcao@nichi.co>
2024-05-28 18:18:08 -0400

    Revert "starfive visionfive2: enable required drivers"
    This reverts commit 03e1d2d5842a730713ab1fdbf0f25d3735019b8c.

--- starfive/visionfive/v2/default.nix
+++ starfive/visionfive/v2/default.nix
@@ -2,15 +2,6 @@
   boot = {
     consoleLogLevel = lib.mkDefault 7;
     kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
-    kernelPatches = [{
-      name = "JH7110";
-      patch = pkgs.fetchpatch {
-        # https://patchwork.kernel.org/project/linux-riscv/patch/20240506034627.66765-1-hal.feng@starfivetech.com/
-        name = "v2-riscv-defconfig-Enable-StarFive-JH7110-drivers.patch";
-        url = "https://patchwork.kernel.org/series/850668/mbox/";
-        hash = "sha256-81Brj61TOUoyVBxrXQh2VU3gTh/1V3uo33ic3sJPk2w=";
-      };
-    }];
 
     kernelParams =
       lib.mkDefault [ "console=tty0" "console=ttyS0,115200n8" "earlycon=sbi" ];
