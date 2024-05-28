9e0603134617ec23ce67e1806e905ca07abc92d3
Nick Cao <nickcao@nichi.co>
2024-05-28 18:18:08 -0400

    Revert "starfive visionfive2: use mainline kernel"
    This reverts commit 413452614f52f3f5e7038f8456b6c0082a5594dc.

--- starfive/visionfive/v2/default.nix
+++ starfive/visionfive/v2/default.nix
@@ -1,7 +1,12 @@
 { config, lib, pkgs, ... }: {
   boot = {
+    # Force no ZFS (from nixos/modules/profiles/base.nix) until updated to kernel 6.0
+    supportedFilesystems =
+      lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
     consoleLogLevel = lib.mkDefault 7;
-    kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+    kernelPackages = lib.mkDefault (pkgs.callPackage ./linux-6.6.nix {
+      inherit (config.boot) kernelPatches;
+    });
 
     kernelParams =
       lib.mkDefault [ "console=tty0" "console=ttyS0,115200n8" "earlycon=sbi" ];
