69fe256333b66ba6814290a0e1fda7cdba61807d
Nick Cao <nickcao@nichi.co>
2024-05-30 19:58:53 -0400

    starfive visionfive2: let u-boot set device tree name
    u-boot is able to automatically set the correct device tree name
    on VisionFive 2 boards by reading their EEPROM since v2023.10.
    
    This allows our image to work with either revision of the boards.
    
    Reference: https://github.com/u-boot/u-boot/commit/16dbe3d9d45527f67d479535a22dc4054ae93e99

--- starfive/visionfive/v2/default.nix
+++ starfive/visionfive/v2/default.nix
@@ -18,7 +18,4 @@
       generic-extlinux-compatible.enable = lib.mkDefault true;
     };
   };
-
-  hardware.deviceTree.name =
-    lib.mkDefault "starfive/jh7110-starfive-visionfive-2-v1.3b.dtb";
 }
