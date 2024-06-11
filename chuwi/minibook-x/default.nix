43e369c6d57ea78632147ed1cd2ffc8ac54d97c5
Jörg Thalheim <Mic92@users.noreply.github.com>
2024-06-11 11:06:17 +0200

    chuwi/minibook-x: add comment why we specify kernelParams

--- chuwi/minibook-x/default.nix
+++ chuwi/minibook-x/default.nix
@@ -5,6 +5,7 @@
     ../../common/pc/laptop/ssd
     ../../common/hidpi.nix
   ];
+  # Fixes the display being rotated 90 degrees.
   boot.kernelParams =
     [ "fbcon=rotate:1" "video=DSI-1:panel_orientation=right_side_up" ];
 }
