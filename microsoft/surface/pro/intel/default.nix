95f921c92b460bbd4446ca9ebbb84e4aa249faad
Flameopathic <64027365+Flameopathic@users.noreply.github.com>
2024-06-17 19:53:46 -0400

    feat: battery and thermal configuration for surface pro intel

--- microsoft/surface/pro/intel/default.nix
+++ microsoft/surface/pro/intel/default.nix
@@ -1,5 +1,7 @@
 { lib, pkgs, ... }:
 
+with lib;
+
 # This module is intended to support the Surface Pro range, specifically those with Intel CPUs.
 # It's expected it will work equally well on many other Surface models, but they may need further
 # config changes to work well.
@@ -11,8 +13,14 @@
     ../../../../common/pc/ssd
     # The Intel CPU module auto-includes Intel's GPU:
     ../../../../common/cpu/intel
+    ../../../../common/pc/laptop
   ];
 
-  services.iptsd.enable = lib.mkDefault true;
+  services.iptsd.enable = mkDefault true;
   environment.systemPackages = [ pkgs.surface-control ];
+
+  services.thermald = [
+    enable = mkDefault true;
+    configFile = mkDefault ./thermal-conf.xml;
+  ];
 }
