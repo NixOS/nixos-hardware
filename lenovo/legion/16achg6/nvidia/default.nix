bcc76ced4c473853d1f195c32d84b67b7250c8ae
Jörg Thalheim <joerg@thalheim.io>
2024-06-20 16:05:41 +0200

    gpu/amd: drop hardware.amdgpu.opencl option
    This now conflicts with nixpkgs.

--- lenovo/legion/16achg6/nvidia/default.nix
+++ lenovo/legion/16achg6/nvidia/default.nix
@@ -1,10 +1,12 @@
-{ ... }:
+{ lib, options, ... }:
 
 {
   imports = [ ../hybrid ];
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware = {
     nvidia.prime.offload.enable = false;
-    amdgpu.opencl = false;
+  } // lib.optionalAttrs (options ? amdgpu.opencl.enable) {
+    # introduced in https://github.com/NixOS/nixpkgs/pull/319865
+    amdgpu.opencl.enable = lib.mkDefault false;
   };
 }
