27487bcd12139b8b20442252e1fc3895d4394ce1
Flameopathic <64027365+Flameopathic@users.noreply.github.com>
2024-06-20 20:45:41 -0400

    change iptsd and system-control to nixpkgs versions - fixes iptsd bug

--- microsoft/surface/surface-pro-intel/default.nix
+++ microsoft/surface/surface-pro-intel/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 # This module is intended to support the Surface Pro range, specifically those with Intel CPUs.
 # It's expected it will work equally well on many other Surface models, but they may need further
@@ -13,6 +13,6 @@
     ../../../common/cpu/intel
   ];
 
-  microsoft-surface.ipts.enable = true;
-  microsoft-surface.surface-control.enable = true;
+  services.iptsd.enable = lib.mkDefault true;
+  environment.systemPackages = [ pkgs.surface-control ];
 }
