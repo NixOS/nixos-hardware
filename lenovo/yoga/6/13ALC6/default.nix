ae13b3761c4994579b7d334e9d425096f411647c
Jörg Thalheim <joerg@thalheim.io>
2024-06-26 08:30:58 +0200

    lenovo/yoga/6/13ALC6: drop video acceleration driver
    Those are not useful for AMD.
    According to https://wiki.nixos.org/wiki/Accelerated_Video_Playback all
    we need is enabling opengl drivers.

--- lenovo/yoga/6/13ALC6/default.nix
+++ lenovo/yoga/6/13ALC6/default.nix
@@ -4,14 +4,9 @@
   imports = [
     ../../../thinkpad/yoga.nix
     ../../../../common/gpu/amd/default.nix
-    ../../../../common/gpu/24.05-compat.nix
   ];
 
   boot.initrd.kernelModules = [ "ideapad_laptop" ];
-  hardware.graphics.extraPackages = with pkgs; [
-    vaapiVdpau
-    libvdpau-va-gl
-  ];
 
   # latest kernel needed to make wifi work
   boot.kernelPackages =  lib.mkIf (lib.versionOlder pkgs.linux.version "5.16") pkgs.linuxPackages_latest;
