1c588f35570b2b7d871e13255ed0a002eb4c4f90
seth <getchoo@tuta.io>
2024-07-11 15:43:10 -0400

    common-gpu-nvidia: enable modesetting by default
    This is commonly required for many applications. As of
    https://github.com/NixOS/nixpkgs/pull/324921 this will also enable
    `nvidia-drm.fbdev=1`, fixing issues such as
    https://github.com/NixOS/nixpkgs/issues/302059

--- common/gpu/nvidia/default.nix
+++ common/gpu/nvidia/default.nix
@@ -3,4 +3,6 @@
 {
   imports = [ ../24.05-compat.nix ];
   services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
+  # TODO: this will be a default after https://github.com/NixOS/nixpkgs/pull/326369
+  hardware.nvidia.modesetting.enable = true;
 }
