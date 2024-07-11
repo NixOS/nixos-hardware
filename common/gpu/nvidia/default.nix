0123ced063737755d0b9f8ee232a07499df26f8a
Jörg Thalheim <Mic92@users.noreply.github.com>
2024-07-12 00:32:24 +0200

    common/gpu/nvidia: use lib.mkDefault for hardware.nvidia.modesetting

--- common/gpu/nvidia/default.nix
+++ common/gpu/nvidia/default.nix
@@ -4,5 +4,5 @@
   imports = [ ../24.05-compat.nix ];
   services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
   # TODO: this will be a default after https://github.com/NixOS/nixpkgs/pull/326369
-  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.modesetting.enable = lib.mkDefault true;
 }
