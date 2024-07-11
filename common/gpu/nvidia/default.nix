f18be960311a57d556e232100e5f700b5b3ac394
seth <getchoo@tuta.io>
2024-07-11 15:24:30 -0400

    common-gpu-nvidia: drop libva-vdpau-driver
    libva-vdpau-driver has not seen a new release in over a decade and is a
    common cause for bugs in some applications. NixOS has also been using
    the now preferred nvidia-vaapi-driver since
    https://github.com/NixOS/nixpkgs/pull/162660, so there isn't much reason
    to keep this

--- common/gpu/nvidia/default.nix
+++ common/gpu/nvidia/default.nix
@@ -1,13 +1,6 @@
-{ lib, pkgs, ... }:
+{ lib, ... }:
 
 {
   imports = [ ../24.05-compat.nix ];
   services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
-  hardware.graphics.extraPackages = [
-    (
-      if pkgs ? libva-vdpau-driver
-      then pkgs.libva-vdpau-driver
-      else pkgs.vaapiVdpau
-    )
-  ];
 }
