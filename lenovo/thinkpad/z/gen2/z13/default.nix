8e87f977f5f3017e1c01a05029cb2ccac3d47498
superherointj <5861043+superherointj@users.noreply.github.com>
2024-07-18 11:53:55 -0300

    lenovo-thinkpad-z13-gen2: disable sound to fix CI

--- lenovo/thinkpad/z/gen2/z13/default.nix
+++ lenovo/thinkpad/z/gen2/z13/default.nix
@@ -5,15 +5,16 @@
     ../../../../../lenovo/thinkpad/z/gen2
   ];
 
-  sound.extraConfig = ''
-    pcm.!default {
-        type plug
-        slave.pcm "hw:1,0"
-    }
+  # Fix-Me: The option definition `sound' no longer has any effect; please remove it.
+  # sound.extraConfig = ''
+  #   pcm.!default {
+  #       type plug
+  #       slave.pcm "hw:1,0"
+  #   }
 
-    ctl.!default {
-        type hw
-        card 1
-    }
-  '';
+  #   ctl.!default {
+  #       type hw
+  #       card 1
+  #   }
+  # '';
 }
