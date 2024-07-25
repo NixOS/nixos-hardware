NixOS/nixos-hardware

commit 3501b9c0962d0644041cd4e9243f817c1d7418ba

flake.nix: export paths instead of imported files

flake.nix
@@ -4,6 +4,7 @@
   outputs = _: {
     nixosModules = let
       deprecated = issue: name: value: builtins.trace "warning: ${name} flake output is deprecated and will be removed. See https://github.com/NixOS/nixos-hardware/issues/${issue} for more information" value;
+      import = path: path; # let the module system know what we are exporting
     in {
       acer-aspire-4810t = import ./acer/aspire/4810t;
       airis-n990 = import ./airis/n990;

commit 5a1460a2972171b076cfd5670b7540b55398eeda

Create asus-zephyrus-ga402x-amdgpu and asus-zephyrus-ga402x-nvidia entries
- Previous attr-set style providing "amdgpu" and "nvidia" is broken by PR #1046
- Add deprecation assertion for asus-zephyrus-ga402x

asus/zephyrus/ga402x/default.nix
@@ -1,15 +1,10 @@
-## When using from a Flake, you can access these via imports of the attr key, e.g:
-#
-# imports = [
-#   nixos-hardware.nixosModules.asus-zephyrus-ga402x.amdgpu
-# ];
-#
-## or:
-# imports = [
-#   nixos-hardware.nixosModules.asus-zephyrus-ga402x.nvidia
-# ];
+{ ... }:
 
 {
-  amdgpu = import ./amdgpu;
-  nvidia = import ./nvidia;
+  assertions = [
+    {
+      assertion = false;
+      message = "Importing asus/zephyrus/ga402x/ (default.nix) directly is deprecated! #TODO: More details";
+    }
+  ];
 }

flake.nix
@@ -34,6 +34,8 @@
       asus-zephyrus-ga401 = import ./asus/zephyrus/ga401;
       asus-zephyrus-ga402 = import ./asus/zephyrus/ga402;
       asus-zephyrus-ga402x = import ./asus/zephyrus/ga402x;
+      asus-zephyrus-ga402x-amdgpu = import ./asus/zephyrus/ga402x/amdgpu;
+      asus-zephyrus-ga402x-nvidia = import ./asus/zephyrus/ga402x/nvidia;
       asus-zephyrus-ga502 = import ./asus/zephyrus/ga502;
       asus-zephyrus-ga503 = import ./asus/zephyrus/ga503;
       asus-zephyrus-gu603h = import ./asus/zephyrus/gu603h;
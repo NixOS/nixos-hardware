5a1460a2972171b076cfd5670b7540b55398eeda
mexisme <wildjim+dev@kiwinet.org>
2024-07-22 00:10:45 +1200

    Create asus-zephyrus-ga402x-amdgpu and asus-zephyrus-ga402x-nvidia entries
    - Previous attr-set style providing "amdgpu" and "nvidia" is broken by PR #1046
    - Add deprecation assertion for asus-zephyrus-ga402x

--- asus/zephyrus/ga402x/default.nix
+++ asus/zephyrus/ga402x/default.nix
@@ -1,15 +1,10 @@
-## When using from a Flake, you can access these via imports of the attr key, e.g:
-#
-# imports = [
-#   nixos-hardware.nixosModules.asus-zephyrus-ga402x.amdgpu
-# ];
-#
-## or:
-# imports = [
-#   nixos-hardware.nixosModules.asus-zephyrus-ga402x.nvidia
-# ];
+{ ... }:
 
 {
-  amdgpu = import ./amdgpu;
-  nvidia = import ./nvidia;
+  assertions = [
+    {
+      assertion = false;
+      message = "Importing asus/zephyrus/ga402x/ (default.nix) directly is deprecated! #TODO: More details";
+    }
+  ];
 }
