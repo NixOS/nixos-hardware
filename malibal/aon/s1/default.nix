4268b735e9cc19567bbc25d0b8a918a9782d38ed
Dietrich Daroch <Dietrich@Daroch.me>
2024-08-21 14:00:38 -0700

    malibal/aon/s1: Add initial intel-only support.

---
+++ malibal/aon/s1/default.nix
@@ -0,0 +1,33 @@
+{ pkgs, lib, ... }:
+
+{
+  imports = [
+    ../../../common/pc/laptop
+    ../../../common/pc/laptop/ssd
+
+    ../../../common/cpu/intel
+
+    ../../../common/gpu/intel
+    ../../../common/gpu/nvidia/disable.nix
+  ];
+
+  boot = {
+    kernelParams = [
+      "mem_sleep_default=deep"
+      "i915.fastboot=1"
+    ];
+    kernelModules = [
+      "coretemp"
+    ];
+  };
+
+  powerManagement = {
+    powertop.enable = lib.mkDefault true;
+  };
+
+  services = {
+    fwupd = {
+      enable = lib.mkDefault true;
+    };
+  };
+}
