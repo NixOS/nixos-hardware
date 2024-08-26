7206d5f55365715406d3941d89d947a2f497ae48
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-08-26 13:09:14 +0200

    Apple iMac 14,2: init

---
+++ apple/imac/14-2/default.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ../.
+    ../../../common/gpu/intel
+    ../../../common/gpu/nvidia
+    ../../../common/hidpi.nix
+  ];
+
+  boot = {
+    initrd.kernelModules = [
+      "applesmc"
+      "applespi"
+      "intel_lpss_pci"
+      "spi_pxa2xx_platform"
+      "kvm-intel"
+    ];
+    blacklistedKernelModules = [
+      "b43"
+      "ssb"
+      "brcmfmac"
+      "brcmsmac"
+      "bcma"
+    ];
+    kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.0") pkgs.linuxPackages_latest;
+    extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+  };
+
+  hardware = {
+    bluetooth.enable = lib.mkDefault true;
+  };
+}
