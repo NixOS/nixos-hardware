45e5bcc7d5453058a6b0011ad807b28b26ebc4eb
Lyndon Sanche <lsanche@lyndeno.ca>
2024-09-03 15:11:05 -0600

    asus-zephyrus-ga401: Add nvidia architecture

--- asus/zephyrus/ga401/default.nix
+++ asus/zephyrus/ga401/default.nix
@@ -6,6 +6,7 @@
     ../../../common/cpu/amd/pstate.nix
     ../../../common/gpu/amd
     ../../../common/gpu/nvidia/prime.nix
+    ../../../common/gpu/nvidia/ampere
     ../../../common/pc/laptop
     ../../../common/pc/laptop/ssd
   ];
@@ -16,7 +17,7 @@
     modesetting.enable = lib.mkDefault true;
 
     dynamicBoost.enable = lib.mkDefault true;
-    
+
     prime = {
       amdgpuBusId = "PCI:4:0:0";
       nvidiaBusId = "PCI:1:0:0";
