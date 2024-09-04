e7cb2b9a63d7b1d6a699628ec23bf7cc9b290e58
Mic92 <96200+Mic92@users.noreply.github.com>
2024-09-04 17:23:06 +0000

    msigl65: add initial configuration (#1106) lenovo: fix unstable wifi on Yoga laptops
    Add extra modprobe options to fix unstable wifi on Yoga laptops.
    
    * Merge branch 'NixOS:master' into master
    
    * Add basic GPD Mini configuration.
    
    * Merge branch 'NixOS:master' into master
    
    * msi: add support for MSI GL65 10SDR-492
    
    * Switch to mkDefault for enabling options.
    
    * Merge branch 'msigl65'
    
    msi/gl65/10SDR-492: add initial configuration
    
    * Update paths for common configs.
    
    * Merge branch 'NixOS:master' into master

---
+++ msi/gl65/10SDR-492/default.nix
@@ -0,0 +1,26 @@
+{ lib, ... }:
+
+{
+  imports = [
+    ../../../common/pc/laptop/ssd
+    ../../../common/cpu/intel
+    ../../../common/gpu/intel/comet-lake
+    ../../../common/gpu/nvidia/prime.nix
+    ../../../common/gpu/nvidia/turing
+    ../../../common/pc/laptop
+  ];
+
+  hardware.bluetooth.enable = lib.mkDefault true;
+
+  hardware.graphics.enable = lib.mkDefault true;
+
+  hardware.nvidia = {
+    prime = {
+      # Bus ID of the Intel GPU.
+      intelBusId = lib.mkDefault "PCI:0:2:0";
+
+      # Bus ID of the NVIDIA GPU.
+      nvidiaBusId = lib.mkDefault "PCI:1:0:0";
+    };
+  };
+}
