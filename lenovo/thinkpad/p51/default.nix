2fc1e10076a377718bcfe8b24bc57da7fc211df1
Lyndon Sanche <lsanche@lyndeno.ca>
2024-09-04 11:21:52 -0600

    lenovo-thinkpad-p51: Add nvidia architecture

--- lenovo/thinkpad/p51/default.nix
+++ lenovo/thinkpad/p51/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ../../../common/gpu/24.05-compat.nix
     ../../../common/gpu/nvidia/prime.nix
+    ../../../common/gpu/nvidia/maxwell
     ../../../common/cpu/intel
     ../../../common/gpu/intel/kaby-lake
     ../../../common/pc/laptop/acpi_call.nix
