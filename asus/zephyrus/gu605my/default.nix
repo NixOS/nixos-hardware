fa76454bcfaa9cb96b53ea821538b349b0ceb78e
Relief Melone <relief.melone@gmail.com>
2024-09-06 20:09:06 +0200

    asus/zephyrus/gu605my: fixed architecture and added dynamic boost as enabled by default

--- asus/zephyrus/gu605my/default.nix
+++ asus/zephyrus/gu605my/default.nix
@@ -1,16 +1,20 @@
-{ ... }:
+{ lib, ... }:
 
 {
   imports = [
     ../../../common/cpu/intel
     ../../../common/gpu/nvidia/prime.nix
-    ../../../common/gpu/nvidia/ampere
+    ../../../common/gpu/nvidia/ada-lovelace
     ../../../common/pc/laptop
     ../../../common/pc/ssd
   ];
 
-  hardware.nvidia.prime = {
-    intelBusId = "PCI:0:2:0";
-    nvidiaBusId = "PCI:1:0:0";
+  hardware.nvidia = {
+    prime = {
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+
+    dynamicBoost.enable = lib.mkDefault true;
   };
 }
