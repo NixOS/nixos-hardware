c97623b428306711dc07dc68fd98b9f0b882ba3c
Matei Dibu <contact@mateidibu.dev>
2024-09-23 12:48:34 +0300

    dell-precision-5560: cleanup

--- dell/precision/5560/default.nix
+++ dell/precision/5560/default.nix
@@ -1,40 +1,25 @@
+{ lib, ... }:
 {
-  config,
-  lib,
-  ...
-}: {
   imports = [
     ../../../common/pc/laptop
     ../../../common/pc/laptop/ssd
     ../../../common/cpu/intel
     ../../../common/gpu/intel/tiger-lake
-    ../../../common/gpu/nvidia/prime.nix
     ../../../common/gpu/nvidia/turing
   ];
 
   hardware.enableRedistributableFirmware = lib.mkDefault true;
 
   boot = {
-    blacklistedKernelModules = ["nouveau"];
-    kernelModules = ["kvm-intel"];
-    kernelParams = ["i915.modeset=1"];
+    blacklistedKernelModules = [ "nouveau" ];
+    kernelModules = [ "kvm-intel" ];
+    kernelParams = [ "i915.modeset=1" ];
   };
 
-  hardware = {
-    graphics = {
-      enable = lib.mkDefault true;
-      enable32Bit = lib.mkDefault true;
-    };
-    nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
-      modesetting.enable = lib.mkDefault true;
-      open = lib.mkDefault false;
-      prime = {
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
-      };
-    };
-  };
+  services.xserver.videoDrivers = [ "nvidia" ];
 
-  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
+  hardware.nvidia.prime = {
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
 }
