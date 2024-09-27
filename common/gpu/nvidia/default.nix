4fdbfbb7edb29a8f3ff69aa5e89722432ae59542
Jörg Thalheim <joerg@thalheim.io>
2024-09-27 13:21:35 +0200

    nvidia: add module to initrd
    So it seems sometimes needed and sometimes not.
    So my question is, do we always want nvidia in initrd?
    At least for intel, this is the way to go.

--- common/gpu/nvidia/default.nix
+++ common/gpu/nvidia/default.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [ ../24.05-compat.nix ];
+  boot.initrd.kernelModules = [ "nvidia" ];
   services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
   # TODO: this will be a default after https://github.com/NixOS/nixpkgs/pull/326369
   hardware.nvidia.modesetting.enable = lib.mkDefault true;
