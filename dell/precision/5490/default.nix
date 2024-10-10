b2f6701f91a06e7e641eed8602ec5cd67cc8d4da
Erkka Heinilä <erkkahe@gmail.com>
2024-10-10 15:28:45 +0300

    dell-precision-5490: fix tests and update README

--- dell/precision/5490/default.nix
+++ dell/precision/5490/default.nix
@@ -7,6 +7,7 @@
   # or even better: boot.kernelParams = pkgs.linuxPackages_latest;
   boot.kernelParams = [ "i915.force_probe=7d55" ];
 
+  hardware.nvidia.open = true;
   hardware.nvidia.prime = {
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
