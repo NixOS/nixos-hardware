201900207ff5075f87f12c52209d4cd26862c52c
Cryolitia PukNgae <Cryolitia@gmail.com>
2024-10-13 08:10:23 +0800

    gpd-win-max-2-2023/bmi260: 0.0.2 -> 1.0.0

--- gpd/win-max-2/2023/bmi260/package.nix
+++ gpd/win-max-2/2023/bmi260/package.nix
@@ -1,28 +1,26 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, fetchurl
-, kernel
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  kernel,
 }:
 
 stdenv.mkDerivation (finalAttr: {
   pname = "bmi260";
-  version = "0.0.2";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "hhd-dev";
     repo = finalAttr.pname;
     rev = "v${finalAttr.version}";
-    hash = "sha256-J0npD75QqOGY1QUoznBjQ+jX28gq5u6b0JZOseclwE8=";
+    hash = "sha256-EFous0pPpCuVoCsFz6/4NryQRSH9Jw9Qng+RY1hiX1c=";
   };
 
   hardeningDisable = [ "pic" ];
 
   nativeBuildInputs = kernel.moduleBuildDependencies;
 
-  makeFlags = [
-    "KERNEL_SRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
-  ];
+  makeFlags = [ "KERNEL_SRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build" ];
 
   installPhase = ''
     runHook preInstall
@@ -34,8 +32,11 @@ stdenv.mkDerivation (finalAttr: {
 
   meta = with lib; {
     homepage = "https://github.com/hhd-dev/bmi260";
-    description = "A kernel module driver for the Bosch BMI260 IMU";
-    license = with licenses; [ bsd3 gpl2Only ];
+    description = "A kernel driver for the Bosch BMI260 IMU";
+    license = with licenses; [
+      bsd3
+      gpl2Only
+    ];
     maintainers = with maintainers; [ Cryolitia ];
     platforms = platforms.linux;
   };
