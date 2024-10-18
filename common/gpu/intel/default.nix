493ec094d427cd635eee1801afd5467b4f764ad6
Gavin John <gavinnjohn@gmail.com>
2024-10-18 11:38:25 -0700

    common/gpu/intel: nixfmt

--- common/gpu/intel/default.nix
+++ common/gpu/intel/default.nix
@@ -16,10 +16,12 @@
   };
   options.hardware.intelgpu.vaapiDriver = lib.mkOption {
     description = "Intel VAAPI driver to use (use null to use both)";
-    type = lib.types.nullOr (lib.types.enum [
-      "intel-vaapi-driver"
-      "intel-media-driver"
-    ]);
+    type = lib.types.nullOr (
+      lib.types.enum [
+        "intel-vaapi-driver"
+        "intel-media-driver"
+      ]
+    );
     default = null; # Use both drivers when we don't know which one to use
   };
   options.hardware.intelgpu.enableHybridCodec = lib.mkEnableOption "hybrid codec support for Intel GPUs";
@@ -37,52 +39,85 @@
 
     nixpkgs.overlays = lib.optionals config.hardware.intelgpu.enableHybridCodec [
       (
-        self: super: (
-          if pkgs?intel-vaapi-driver then {
-            intel-vaapi-driver = super.intel-vaapi-driver.override { enableHybridCodec = true; };
-          } else if pkgs?vaapiIntel then {
-            vaapiIntel = super.vaapiIntel.override { enableHybridCodec = true; };
-          } else
+        self: super:
+        (
+          if pkgs ? intel-vaapi-driver then
+            { intel-vaapi-driver = super.intel-vaapi-driver.override { enableHybridCodec = true; }; }
+          else if pkgs ? vaapiIntel then
+            { vaapiIntel = super.vaapiIntel.override { enableHybridCodec = true; }; }
+          else
             builtins.throw "Unable to find Intel VAAPI driver"
         )
       )
     ];
 
-    hardware.graphics.extraPackages = lib.optionals (config.hardware.intelgpu.vaapiDriver == "intel-vaapi-driver" || config.hardware.intelgpu.vaapiDriver == null) (with pkgs; [
-      (
-        if pkgs?intel-vaapi-driver then
-          intel-vaapi-driver
-        else if pkgs?vaapiIntel then
-          vaapiIntel
-        else
-          builtins.throw "Unable to find Intel VAAPI driver"
-      )
-    ]) ++ lib.optionals (config.hardware.intelgpu.vaapiDriver == "intel-media-driver" || config.hardware.intelgpu.vaapiDriver == null) (with pkgs; [
-      intel-media-driver
-      (
-        if pkgs?vpl-gpu-rt then
-          vpl-gpu-rt
-        else if pkgs?onevpl-intel-gpu then
-          onevpl-intel-gpu
-        else
-          builtins.throw "Unable to find OneAPI VAAPI driver"
-      )
-    ]);
+    hardware.graphics.extraPackages =
+      lib.optionals
+        (
+          config.hardware.intelgpu.vaapiDriver == "intel-vaapi-driver"
+          || config.hardware.intelgpu.vaapiDriver == null
+        )
+        (
+          with pkgs;
+          [
+            (
+              if pkgs ? intel-vaapi-driver then
+                intel-vaapi-driver
+              else if pkgs ? vaapiIntel then
+                vaapiIntel
+              else
+                builtins.throw "Unable to find Intel VAAPI driver"
+            )
+          ]
+        )
+      ++ lib.optionals
+        (
+          config.hardware.intelgpu.vaapiDriver == "intel-media-driver"
+          || config.hardware.intelgpu.vaapiDriver == null
+        )
+        (
+          with pkgs;
+          [
+            intel-media-driver
+            (
+              if pkgs ? vpl-gpu-rt then
+                vpl-gpu-rt
+              else if pkgs ? onevpl-intel-gpu then
+                onevpl-intel-gpu
+              else
+                builtins.throw "Unable to find OneAPI VAAPI driver"
+            )
+          ]
+        );
 
-    hardware.graphics.extraPackages32 = lib.optionals (config.hardware.intelgpu.vaapiDriver == "intel-vaapi-driver" || config.hardware.intelgpu.vaapiDriver == null) (with pkgs.driversi686Linux; [
-      (
-        if pkgs?intel-vaapi-driver then
-          intel-vaapi-driver
-        else if pkgs?vaapiIntel then
-          vaapiIntel
-        else
-          builtins.throw "Unable to find Intel VAAPI driver"
-      )
-    ]) ++ lib.optionals (config.hardware.intelgpu.vaapiDriver == "intel-media-driver" || config.hardware.intelgpu.vaapiDriver == null) (with pkgs.driversi686Linux; ([
-      intel-media-driver
-    ] ++ (lib.optionals (pkgs.driversi686Linux?vpl-gpu-rt) [
-      vpl-gpu-rt
-    ])));
+    hardware.graphics.extraPackages32 =
+      lib.optionals
+        (
+          config.hardware.intelgpu.vaapiDriver == "intel-vaapi-driver"
+          || config.hardware.intelgpu.vaapiDriver == null
+        )
+        (
+          with pkgs.driversi686Linux;
+          [
+            (
+              if pkgs ? intel-vaapi-driver then
+                intel-vaapi-driver
+              else if pkgs ? vaapiIntel then
+                vaapiIntel
+              else
+                builtins.throw "Unable to find Intel VAAPI driver"
+            )
+          ]
+        )
+      ++ lib.optionals
+        (
+          config.hardware.intelgpu.vaapiDriver == "intel-media-driver"
+          || config.hardware.intelgpu.vaapiDriver == null
+        )
+        (
+          with pkgs.driversi686Linux;
+          ([ intel-media-driver ] ++ (lib.optionals (pkgs.driversi686Linux ? vpl-gpu-rt) [ vpl-gpu-rt ]))
+        );
 
     assertions = [
       {
