6bda1efd86d1ef2817fe4c840bc75f9ee7ff81bf
Jörg Thalheim <Mic92@users.noreply.github.com>
2024-10-21 18:00:46 +0200

    Update common/gpu/intel/lunar-lake/default.nix
    Co-authored-by: Gavin John <gavinnjohn@gmail.com>

--- common/gpu/intel/lunar-lake/default.nix
+++ common/gpu/intel/lunar-lake/default.nix
@@ -1,6 +1,8 @@
 { config, lib, ... }:
 {
   imports = [ ../. ];
-  hardware.intelgpu.driver = lib.mkIf (lib.versionAtLeast config.boot.kernelPackages.kernel.version "6.8") "xe";
-  hardware.intelgpu.vaapiDriver = "intel-media-driver";
+  hardware.intelgpu = {
+    driver = lib.mkIf (lib.versionAtLeast config.boot.kernelPackages.kernel.version "6.8") "xe";
+    vaapiDriver = "intel-media-driver";
+  };
 }
