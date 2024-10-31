7ed4ada911e9374ce870062e76aa23333400c99d
onurmercury <141233985+onurmercury@users.noreply.github.com>
2024-10-31 11:55:20 +0300

    Fix "nividia" typo

--- common/gpu/nvidia/ampere/default.nix
+++ common/gpu/nvidia/ampere/default.nix
@@ -1,10 +1,10 @@
 {lib, config, ...}:
 let
-  nividiaPackage = config.hardware.nvidia.package;
+  nvidiaPackage = config.hardware.nvidia.package;
 in
 {
   imports = [ ../. ];
 
   # enable the open source drivers if the package supports it
-  hardware.nvidia.open = lib.mkOverride 990 (nividiaPackage ? open && nividiaPackage ? firmware);
+  hardware.nvidia.open = lib.mkOverride 990 (nvidiaPackage ? open && nvidiaPackage ? firmware);
 }
