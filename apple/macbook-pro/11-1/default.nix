12ad8c1bf13ff15ffa6afe82c59b4af0b9226035
Jörg Thalheim <Mic92@users.noreply.github.com>
2024-11-02 11:35:05 +0100

    apple/macbook-pro/11-1: add comment why we include broadcom driver

--- apple/macbook-pro/11-1/default.nix
+++ apple/macbook-pro/11-1/default.nix
@@ -8,6 +8,7 @@
 
   # broadcom-wl
   hardware.enableRedistributableFirmware = lib.mkDefault true;
+  # nixos-generate-config doesn't detect this automatically.
   boot.extraModulePackages = with config.boot.kernelPackages; [ broadcom_sta ];
   boot.kernelModules = [ "wl" ];
 }
