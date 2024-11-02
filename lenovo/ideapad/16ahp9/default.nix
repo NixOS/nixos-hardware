9179e2fd2cfb6cdc6f384b635135dbafcb9a8ab8
Vittorio Robecchi <vittorio.robecchi@gmail.com>
2024-11-02 09:10:53 +0100

    Added Lenovo Ideadpad 16AHP9

---
+++ lenovo/ideapad/16ahp9/default.nix
@@ -0,0 +1,11 @@
+{ lib, ... }:
+
+{
+  imports = [
+    ../../../common/cpu/amd
+    ../../../common/cpu/amd/pstate.nix
+    ../../../common/gpu/amd
+    ../../../common/pc/laptop
+    ../../../common/pc/laptop/ssd
+  ];
+}
