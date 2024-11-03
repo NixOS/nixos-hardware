ac8999ad21ffcfeba6a8d4046e7ec9cdd368fa0c
Relief Melone <relief.melone@gmail.com>
2024-11-03 23:36:46 +0100

    gu605my: modsettings, fn keys and use laptop/ssd

--- asus/zephyrus/gu605my/default.nix
+++ asus/zephyrus/gu605my/default.nix
@@ -6,7 +6,7 @@
     ../../../common/gpu/nvidia/prime.nix
     ../../../common/gpu/nvidia/ada-lovelace
     ../../../common/pc/laptop
-    ../../../common/pc/ssd
+    ../../../common/pc/laptop/ssd
   ];
 
   hardware.nvidia = {
@@ -15,6 +15,7 @@
       nvidiaBusId = "PCI:1:0:0";
     };
 
+    modesetting.enable = lib.mkDefault true;
     dynamicBoost.enable = lib.mkDefault true;
   };
 
@@ -23,7 +24,9 @@
 
     udev.extraHwdb = ''
       evdev:name:*:dmi:bvn*:bvr*:bd*:svnASUS*:pn*:*
-       KEYBOARD_KEY_ff31007c=f20    # fixes mic mute button
+        KEYBOARD_KEY_ff31007c=f20    # fixes mic mute button
+        KEYBOARD_KEY_ff3100b2=home   # Set fn+LeftArrow as Home
+        KEYBOARD_KEY_ff3100b3=end    # Set fn+RightArrow as End
     '';
   };
 }
