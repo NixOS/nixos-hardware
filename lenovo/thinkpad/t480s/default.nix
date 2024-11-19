3d3b0f50bf2dfa5fdb0616ed40dab4a875dbeed2
Gavin John <gavinnjohn@gmail.com>
2024-11-19 14:42:43 -0800

    lenovo/thinkpad/t480s: Automatically downgrade kernel to 6.10

--- lenovo/thinkpad/t480s/default.nix
+++ lenovo/thinkpad/t480s/default.nix
@@ -1,4 +1,9 @@
-{ lib, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 {
   imports = [
@@ -9,4 +14,10 @@
   ];
 
   services.throttled.enable = lib.mkDefault true;
+
+  # There is currently a kernel regression with the psmouse driver. As such, the kernel version is held back to 6.10.
+  # See https://bugzilla.kernel.org/show_bug.cgi?id=219352
+  boot.kernelPackages = lib.mkIf (lib.versionAtLeast config.boot.kernelPackages "6.11") (
+    lib.warn "Linux kernel held back to 6.10 due to regression: https://bugzilla.kernel.org/show_bug.cgi?id=219352" pkgs.linuxPackages_6_10
+  );
 }
