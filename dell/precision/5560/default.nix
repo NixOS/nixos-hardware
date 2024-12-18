6828304671c6a1c4d104585887b6a7ad22208a58
Matei Dibu <contact@mateidibu.dev>
2024-12-18 12:37:13 +0200

    dell-precision-5560: remove redundant i915.modeset=1

--- dell/precision/5560/default.nix
+++ dell/precision/5560/default.nix
@@ -9,10 +9,6 @@
 
   hardware.enableRedistributableFirmware = lib.mkDefault true;
 
-  boot = {
-    kernelParams = [ "i915.modeset=1" ];
-  };
-
   hardware.nvidia.prime = {
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
