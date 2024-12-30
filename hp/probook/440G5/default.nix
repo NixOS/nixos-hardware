ee271c45847e12ec7a031fb95b0c08188c3b066c
Václav Klecanda <vencax@users.noreply.github.com>
2024-12-30 08:57:34 +0100

    yet another link in comment

--- hp/probook/440G5/default.nix
+++ hp/probook/440G5/default.nix
@@ -2,6 +2,7 @@
 
 {
   # https://www.intel.com/content/www/us/en/products/sku/124967/intel-core-i58250u-processor-6m-cache-up-to-3-40-ghz/specifications.html
+  # this one works: https://nixos.wiki/wiki/Accelerated_Video_Playback
   imports = [
     ../../../common/cpu/intel/kaby-lake
     ../../../common/pc
