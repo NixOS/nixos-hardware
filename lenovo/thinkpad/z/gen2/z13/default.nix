8bf07b9e88f5425cf6179c469e6ada9aca2ac121
โทสตัล <toastal@posteo.net>
2024-12-31 12:22:58 +0700

    lenovo-z13-gen2: networking.networkmanager.fccUnlockScripts → networking.modemmanager.fccUnlockScripts
    Fixes evaluation warning

--- lenovo/thinkpad/z/gen2/z13/default.nix
+++ lenovo/thinkpad/z/gen2/z13/default.nix
@@ -7,7 +7,7 @@
 
   environment.etc."asound.conf".source = ./asound.conf;
 
-  networking.networkmanager.fccUnlockScripts = [
+  networking.modemmanager.fccUnlockScripts = [
     {
       id = "2c7c:030a";
       path = "${pkgs.modemmanager}/share/ModemManager/fcc-unlock.available.d/2c7c:030a";
