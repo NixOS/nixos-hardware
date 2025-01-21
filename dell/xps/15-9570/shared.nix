6159e8c42bdc1c29d2d60f688edcb0a02b471a2a
Matthieu <matthieu.cx@gmail.com>
2025-01-21 13:48:34 +0000

    chore(refactor): adhere to more recent profiles, make modules more explicit

---
+++ dell/xps/15-9570/shared.nix
@@ -0,0 +1,24 @@
+{ lib, ... }:
+{
+
+  imports = [ 
+    ../../../../common/cpu/intel/coffee-lake
+    ../../../../common/pc/laptop
+  ];
+
+  # Solution to rcu_sched freezing
+  boot.kernelParams = [ "acpi_rev_override" ];
+  boot.kernelModules = [ "kvm-intel" ];
+
+  # This will save you money and possibly your life!
+  # https://github.com/NixOS/nixos-hardware/pull/127
+  services.thermald.enable = lib.mkDefault true;
+
+  # Includes the Wi-Fi and Bluetooth firmware
+  hardware.enableRedistributableFirmware = lib.mkDefault true;
+
+  hardware.graphics = {
+    enable = lib.mkDefault true;
+    enable32Bit = lib.mkDefault true;
+  };
+}
