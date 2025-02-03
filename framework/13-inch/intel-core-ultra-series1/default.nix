d5c3adbdf655090bc27537110010750b2e2b5662
Nicky Mouha <nmouha@users.noreply.github.com>
2025-02-03 03:40:59 -0500

    Update default.nix

--- framework/13-inch/intel-core-ultra-series1/default.nix
+++ framework/13-inch/intel-core-ultra-series1/default.nix
@@ -19,8 +19,8 @@
         version = "0.0";
 
         firmware = pkgs.fetchurl {
-          url = "https://github.com/intel/linux-npu-driver/raw/v1.2.0/firmware/bin/vpu_${model}_v${version}.bin";
-          hash = "sha256-qGhLLiBnOlmF/BEIGC7DEPjfgdLCaMe7mWEtM9uK1mo=";
+          url = "https://github.com/intel/linux-npu-driver/raw/v1.13.0/firmware/bin/vpu_${model}_v${version}.bin";
+          hash = "sha256-Mpoeq8HrwChjtHALsss/7QsFtDYAoFNsnhllU0xp3os=";
         };
       in
       pkgs.runCommand "intel-vpu-firmware-${model}-${version}" { } ''
