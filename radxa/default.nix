f7f4003e1f8c558941e6045493ea4b41477889d4
Jörg Thalheim <Mic92@users.noreply.github.com>
2025-02-06 04:58:47 +0100

    radxa: add remark about kernel.

--- radxa/default.nix
+++ radxa/default.nix
@@ -14,6 +14,7 @@ in {
     boot = {
       # Currently enable bcachefs automatically set
       # kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+      # TODO: Consider removing this line once, we get an LTS kernel that is newer than 6.12
       kernelPackages = lib.mkOverride 990 pkgs.linuxPackages_latest;
       supportedFilesystems = [ "bcachefs" ];
       loader.systemd-boot.enable = lib.mkDefault true;
