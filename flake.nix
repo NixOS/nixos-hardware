c32f3c83e68b8d7537e7e79f43f0c6f2a28bd47f
ZHANG Yuntian <yt@radxa.com>
2025-02-06 18:46:31 +0800

    radxa/rock-pi-4: init

--- flake.nix
+++ flake.nix
@@ -296,6 +296,7 @@
         raspberry-pi-4 = import ./raspberry-pi/4;
         raspberry-pi-5 = import ./raspberry-pi/5;
         rock-4c-plus = import ./radxa/rock-4c-plus;
+        rock-pi-4 = import ./radxa/rock-pi-4;
         kobol-helios4 = import ./kobol/helios4;
         samsung-np900x3c = import ./samsung/np900x3c;
         slimbook-hero-rpl-rtx = import ./slimbook/hero/rpl-rtx;
