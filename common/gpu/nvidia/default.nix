1ad99b4472bd23115add9db3a47b113e023cb6ff
Manuel Transfeld <auipga@users.noreply.github.com>
2025-02-10 18:07:30 +0100

    remove a default value
    https://github.com/NixOS/nixpkgs/pull/326369 has been merged.

--- common/gpu/nvidia/default.nix
+++ common/gpu/nvidia/default.nix
@@ -3,6 +3,4 @@
 {
   imports = [ ../24.05-compat.nix ];
   services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
-  # TODO: this will be a default after https://github.com/NixOS/nixpkgs/pull/326369
-  hardware.nvidia.modesetting.enable = lib.mkDefault true;
 }
