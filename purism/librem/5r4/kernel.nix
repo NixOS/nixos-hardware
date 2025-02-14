61dbd8326e243a62d29795cfd6d1e5508bb2a9d3
⛧-440729 [sophie] <sophie@999eagle.moe>
2025-02-14 17:17:05 +0100

    purism/librem/5r4: kernel: 6.6.29-librem5 -> 6.6.74-librem5

--- purism/librem/5r4/kernel.nix
+++ purism/librem/5r4/kernel.nix
@@ -6,16 +6,19 @@
 buildLinux (args
   // rec {
   defconfig = "librem5_defconfig";
-  version = "6.6.29-librem5";
+  version = "6.6.74-librem5";
   modDirVersion = version;
   src = fetchFromGitLab {
     domain = "source.puri.sm";
     owner = "Librem5";
     repo = "linux";
-    rev = "pureos/6.6.29pureos1";
-    hash = "sha256-i8HSAJ1/Z2Ux2s3Srse+L0S1Zd/70ldpGBhIgEZ6nBw=";
+    rev = "pureos/6.6.74pureos1";
+    hash = "sha256-qUPY+2fHVu7SFc+Uf8U7QtkQJJsE/4I1SavpLqJ/34c=";
   };
   kernelPatches = [ ];
+  # see https://github.com/NixOS/nixpkgs/pull/366004
+  ignoreConfigErrors = true;
+
   structuredExtraConfig = with lib.kernel; {
     # buildLinux overrides this and defaults to 32, so go back to the value defined librem5_defconfig
     # this is required for millipixels to take photos, otherwise the VIDIOC_REQ_BUFS ioctl returns ENOMEM
