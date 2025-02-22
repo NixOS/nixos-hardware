be6c17d1f52f3b243016ced6fc879de7fb55349e
Nunya Bidness <1012004+socialmediarefugee@users.noreply.github.com>
2025-02-22 03:09:00 -0600

    Removed opinionated setting of thermalda

--- omen/15-ek1013dx/default.nix
+++ omen/15-ek1013dx/default.nix
@@ -7,17 +7,12 @@
     ../../common/gpu/nvidia/ampere
     ../../common/pc/laptop
     ../../common/pc/laptop/ssd
-    ../../common/hidpi.nix
   ];
 
   # Enables ACPI platform profiles
   boot = lib.mkIf (lib.versionAtLeast pkgs.linux.version "6.1") {
     kernelModules = [ "hp-wmi" ];
   };
-
-  # Since this is a laptop, enable the thermal daemon.
-  # NOTE: Users may want to add lm-sensors and cpufreq-utils to system packages
-  services.thermald.enable = lib.mkDefault true;
 
   # Enables Wifi and Bluetooth
   hardware.enableRedistributableFirmware = true;
