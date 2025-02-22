eff85ec0028d4dd19ad4d46154ea34030d4c4bff
Nunya Bidness <1012004+socialmediarefugee@users.noreply.github.com>
2025-02-22 02:18:00 -0600

    Removed hdd, Added hidpi, Organized imports

--- omen/15-ek1013dx/default.nix
+++ omen/15-ek1013dx/default.nix
@@ -3,11 +3,27 @@
 {
   imports = [
     ../../common/cpu/intel/comet-lake
+    ../../common/gpu/nvidia/prime.nix
     ../../common/gpu/nvidia/ampere
     ../../common/pc/laptop
     ../../common/pc/laptop/ssd
-    ../../common/pc/laptop/hdd
+    ../../common/hidpi.nix
   ];
 
-  boot.kernelModules = [ "hp-wmi" ];
+  # Enables ACPI platform profiles
+  boot = lib.mkIf (lib.versionAtLeast pkgs.linux.version "6.1") {
+    kernelModules = [ "hp-wmi" ];
+  };
+
+  # Since this is a laptop, enable the thermal daemon.
+  # NOTE: Users may want to add lm-sensors and cpufreq-utils to system packages
+  services.thermald.enable = lib.mkDefault true;
+
+  # Enables Wifi and Bluetooth
+  hardware.enableRedistributableFirmware = true;
+
+  hardware.nvidia.prime = {
+    nvidiaBusId = "PCI:1:0:0";
+    intelBusId = "PCI:0:2:0";
+  };
 }
