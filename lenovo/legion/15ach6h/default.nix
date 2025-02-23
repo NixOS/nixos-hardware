b16b149e64dc952ed22696aea52cf414c8a6e8d4
Malix <alixbrunetcontact@gmail.com>
2025-02-23 11:39:42 +0100

    feat: lenovo legion 15ach6h hardware.nvidia.open

--- lenovo/legion/15ach6h/default.nix
+++ lenovo/legion/15ach6h/default.nix
@@ -3,6 +3,9 @@
 {
   imports = [ ./hybrid ];
 
+  # Use the open source kernel module drivers recommended by NVidia (>= Turing architecture)
+  hardware.nvidia.open = true;
+
   specialisation.ddg.configuration = {
     # This specialisation is for the case where "DDG" (Dual-Direct GFX, A hardware feature that can enable in bios) is enabled, since the amd igpu is blocked at hardware level and the built-in display is directly connected to the dgpu, we no longer need the amdgpu and prime configuration.
     system.nixos.tags = [ "Dual-Direct-GFX-Mode" ];
