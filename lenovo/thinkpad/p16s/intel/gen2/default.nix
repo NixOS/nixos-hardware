84eb0330aed636ed56ce3f3495cc4a329db45278
Jairo Llopis <yajo.sk8@gmail.com>
2025-03-13 11:15:20 +0000

    feat: add Lenovo ThinkPad P16s Intel Gen 2
    @moduon MT-9339

---
+++ lenovo/thinkpad/p16s/intel/gen2/default.nix
@@ -0,0 +1,18 @@
+{lib, ...}: {
+  imports = [
+    ../.
+    ../../../../../common/cpu/intel/raptor-lake
+    ../../../../../common/gpu/intel/raptor-lake
+    ../../../../../common/gpu/nvidia/ampere
+    ../../../../../common/gpu/nvidia/prime.nix
+  ];
+
+  # DOCS https://wiki.nixos.org/wiki/NVIDIA#Offload_mode
+  hardware.nvidia.prime = {
+    intelBusId = lib.mkDefault "PCI:0:2:0";
+    nvidiaBusId = lib.mkDefault "PCI:3:0:0";
+  };
+
+  # HACK https://github.com/NVIDIA/open-gpu-kernel-modules/issues/472
+  hardware.nvidia.open = lib.mkOverride 993 false;
+}
