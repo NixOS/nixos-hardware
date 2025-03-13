a8079137ba08af38229176515b0ce11f0527bab6
Jairo Llopis <yajo.sk8@gmail.com>
2025-03-13 11:53:24 +0000

    feat(nvidia-prime): automatic battery-saver specialisation
    This enables a boot option that switches off NVIDIA GPU. Allows for battery saving.
    
    @moduon MT-9339

--- common/gpu/nvidia/prime.nix
+++ common/gpu/nvidia/prime.nix
@@ -3,11 +3,32 @@
 {
   imports = [ ./. ];
 
-  hardware.nvidia.prime = {
-    offload = {
-      enable = lib.mkOverride 990 true;
-      enableOffloadCmd = lib.mkIf config.hardware.nvidia.prime.offload.enable true; # Provides `nvidia-offload` command.
+  options = {
+    hardware.nvidia.primeBatterySaverSpecialisation = lib.mkEnableOption "Enable the battery saver specialisation for NVIDIA Prime";
+  };
+
+  config = {
+
+    hardware.nvidia.prime = {
+      offload = {
+        enable = lib.mkOverride 990 true;
+        enableOffloadCmd = lib.mkIf config.hardware.nvidia.prime.offload.enable true; # Provides `nvidia-offload` command.
+      };
+      # Hardware should specify the bus ID for intel/nvidia devices
     };
-    # Hardware should specify the bus ID for intel/nvidia devices
+
+    # A specialisation for optimal battery life
+    specialisation = lib.optionalAttrs config.hardware.nvidia.primeBatterySaverSpecialisation {
+      battery-saver.configuration = {
+        system.nixos.tags = ["battery-saver"];
+        imports = [
+          # Leave only the integrated GPU enabled
+          ./disable.nix
+        ];
+        hardware.nvidia.prime.offload.enable = lib.mkForce false;
+        hardware.nvidia.powerManagement.enable = lib.mkForce false;
+        hardware.nvidia.powerManagement.finegrained = lib.mkForce false;
+      };
+    };
   };
 }
