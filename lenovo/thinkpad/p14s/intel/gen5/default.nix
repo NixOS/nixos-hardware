0e28fa7370228d5773c66d2eded68c777cacc0d6
steeb-k <133536278+steeb-k@users.noreply.github.com>
2025-03-21 17:39:38 -0700

    Lenovo Thinkpad P14s Gen 5 Intel
    With Nvidia Prime basic config

--- lenovo/thinkpad/p14s/intel/gen5/default.nix
+++ lenovo/thinkpad/p14s/intel/gen5/default.nix
@@ -8,11 +8,12 @@
   ];
 
   hardware = {
-    intelgpu.driver = "i915"; #xe driver may be used on newer kernels
+    intelgpu.driver = "i915"; #xe driver may be used on newer kernels 
     nvidia = {
       prime = {
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
+      };
     };
   };
 }
