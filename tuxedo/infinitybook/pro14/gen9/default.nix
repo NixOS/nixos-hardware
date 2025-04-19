f16e0cd51c8bb6df7bfc5b7bbec3b36ed59020ae
Johannes Arnold <jarnold@b1-systems.de>
2025-04-19 12:15:00 +0200

    Add yt6801 driver for TUXEDO InfinityBook Pro 14 - Gen9 - INTEL

---
+++ tuxedo/infinitybook/pro14/gen9/default.nix
@@ -0,0 +1,9 @@
+{ lib, config, ... }: {
+  imports = [
+    ../../.
+  ];
+
+  # Add Motorcomm YT6801 Driver if available
+  boot.extraModulePackages = with config.boot; lib.lists.optional
+  ( kernelPackages ? yt6801 ) kernelPackages.yt6801;
+}
