cdf5d3903274a6d8aea90bedcf56803a2b739aa8
Peter Bittner <django@bittner.it>
2025-05-05 23:42:16 +0200

    Ensure WiFi works out-of-the-box on XPS13
    Inspired by https://github.com/NixOS/nixos-hardware/blob/master/dell/inspiron/3442/default.nix

--- dell/xps/13-9343/default.nix
+++ dell/xps/13-9343/default.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 {
   imports = [
@@ -7,6 +7,14 @@
     ../../../common/pc/ssd
   ];
 
-  # This will save you money and possibly your life!
-  services.thermald.enable = lib.mkDefault true;
+  services = {
+    fwupd.enable = lib.mkDefault true;
+    thermald.enable = lib.mkDefault true;
+  };
+
+  boot = {
+    # needs to be explicitly loaded or else bluetooth/wifi won't work
+    kernelModules = [ "wl" ];
+    extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+  };
 }
