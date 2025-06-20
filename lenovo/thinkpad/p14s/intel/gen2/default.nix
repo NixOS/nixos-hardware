fce822b9bb4f6c4de2954738e7ba7b520fd21d65
Borja Rojo Ilvento <borja.rojo@gmail.com>
2025-06-20 18:26:02 -0400

    Extended the configuration to better match the P51 models because they are more complete for handling the powermanagement settings with PRIME sync.

--- lenovo/thinkpad/p14s/intel/gen2/default.nix
+++ lenovo/thinkpad/p14s/intel/gen2/default.nix
@@ -1,18 +1,43 @@
-{ lib, pkgs, ... }:
-
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 {
   imports = [
-    ../.
     ../../../../../common/gpu/nvidia/prime.nix
     ../../../../../common/gpu/nvidia/turing
+    ../../../../../common/cpu/intel/tiger-lake
+    ../.
   ];
 
   hardware = {
+    enableAllFirmware = lib.mkDefault true;
+
+    graphics = {
+      enable = lib.mkDefault true;
+      enable32Bit = lib.mkDefault true;
+    };
+
     intelgpu.driver = "xe";
 
-    nvidia.prime = {
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
+    nvidia = {
+      prime = {
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+
+      powerManagement = lib.mkIf config.hardware.nvidia.prime.sync.enable {
+        enable = lib.mkDefault true;
+      };
+
+      modesetting = lib.mkIf config.hardware.nvidia.prime.sync.enable {
+        enable = lib.mkDefault true;
+      };
     };
+
   };
+
+  services.thermald.enable = lib.mkDefault true;
 }
