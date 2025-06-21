ee4b11945370973452817cbc9307b1bd334fffb2
Borja Rojo Ilvento <borja.rojo@gmail.com>
2025-06-21 13:01:25 -0400

    Disabling powerManagement by default. Enabled on sync, to match P51. Finegrained is disabled. Modesetting is enabled, as per the wiki.

--- lenovo/thinkpad/p14s/intel/gen2/default.nix
+++ lenovo/thinkpad/p14s/intel/gen2/default.nix
@@ -12,6 +12,8 @@
     ../.
   ];
 
+  # For suspending to RAM to work, set Config -> Power -> Sleep State to "Linux S3" in EFI.
+
   hardware = {
     enableAllFirmware = lib.mkDefault true;
 
@@ -23,14 +25,23 @@
     intelgpu.driver = "xe";
 
     nvidia = {
-      modesetting = lib.mkDefault true;
+      modesetting.enable = lib.mkDefault true;
+
+      # Enable power management to fix suspend/resume screen corruption in sync mode
+      powerManagement = {
+        enable = lib.mkDefault false;
+        finegrained = lib.mkDefault false;
+      };
+
       prime = {
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
     };
-
   };
+
+  # Force use of the thinkpad_acpi driver for backlight control
+  boot.kernelParams = [ "acpi_backlight=vendor" ];
 
   services.thermald.enable = lib.mkDefault true;
 }
