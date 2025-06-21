2f54d4bb2c644f913d78a80d8c84830df22296f5
Borja Rojo Ilvento <borja.rojo@gmail.com>
2025-06-21 14:22:42 -0400

    Removing extranous settings. Trying to hone down on the suspend issue. Seems to persist.

--- lenovo/thinkpad/p14s/intel/gen2/default.nix
+++ lenovo/thinkpad/p14s/intel/gen2/default.nix
@@ -25,23 +25,10 @@
     intelgpu.driver = "xe";
 
     nvidia = {
-      modesetting.enable = lib.mkDefault true;
-
-      # Enable power management to fix suspend/resume screen corruption in sync mode
-      powerManagement = {
-        enable = lib.mkDefault false;
-        finegrained = lib.mkDefault false;
-      };
-
       prime = {
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
     };
   };
-
-  # Force use of the thinkpad_acpi driver for backlight control
-  boot.kernelParams = [ "acpi_backlight=vendor" ];
-
-  services.thermald.enable = lib.mkDefault true;
 }
