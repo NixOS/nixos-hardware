841b8456e33aec9169c9a20c7221398903f2ceda
Yann Büchau <nobodyinperson@posteo.de>
2025-06-25 19:50:49 +0200

    rpi4: Introduce raspberry-pi."4".enable
    Don't enable anything unless explicitly enabled. Otherwise e.g. the specified kernel complicates running a x86_64 vm of a rpi4 NixOS system

--- raspberry-pi/4/cpu-revision.nix
+++ raspberry-pi/4/cpu-revision.nix
@@ -1,24 +1,23 @@
-{
-  hardware.deviceTree.overlays = [
-    {
-      name = "rpi4-cpu-revision";
-      dtsText = ''
-        /dts-v1/;
-        /plugin/;
+{ config, lib, ... }:
+lib.mkIf config.hardware.raspberry-pi."4".enable {
+  hardware.deviceTree.overlays = [{
+    name = "rpi4-cpu-revision";
+    dtsText = ''
+      /dts-v1/;
+      /plugin/;
 
-        / {
-          compatible = "raspberrypi,4-model-b";
+      / {
+        compatible = "raspberrypi,4-model-b";
 
-          fragment@0 {
-            target-path = "/";
-            __overlay__ {
-              system {
-                linux,revision = <0x00d03114>;
-              };
+        fragment@0 {
+          target-path = "/";
+          __overlay__ {
+            system {
+              linux,revision = <0x00d03114>;
             };
           };
         };
-      '';
-    }
-  ];
+      };
+    '';
+  }];
 }
