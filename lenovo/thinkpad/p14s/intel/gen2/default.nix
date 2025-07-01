c7cf8e85d7b69e97d9771fff4d6a461a0d235ed3
Borja Rojo Ilvento <borja.rojo@gmail.com>
2025-07-01 16:23:35 +0200

    Add Lenovo ThinkPad P14s Intel Gen 2 profile

---
+++ lenovo/thinkpad/p14s/intel/gen2/default.nix
@@ -0,0 +1,18 @@
+{
+  imports = [
+    ../../../../../common/gpu/nvidia/prime.nix
+    ../../../../../common/gpu/nvidia/turing
+    ../../../../../common/cpu/intel/tiger-lake
+    ../../../../../common/pc/laptop
+    ../../../../../common/pc/ssd
+    ../../../../../common/pc
+    ../.
+  ];
+
+  # For suspending to RAM to work, set Config -> Power -> Sleep State to "Linux S3" in EFI.
+
+  hardware.nvidia.prime = {
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
+}
