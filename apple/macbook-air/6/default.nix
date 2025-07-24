0632a5e10fa0d4a81a59e9b084d4dc898f0e26ba
Michael Hoang <enzime@users.noreply.github.com>
2025-07-24 13:49:05 +1000

    MacBookAir6,x: fix wireless

--- apple/macbook-air/6/default.nix
+++ apple/macbook-air/6/default.nix
@@ -1,7 +1,11 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 {
   imports = [ ../. ];
+
+  boot.kernelModules = [ "wl" ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+  boot.blacklistedKernelModules = [ "bcma" ];
 
   boot = {
     # Divides power consumption by two.
