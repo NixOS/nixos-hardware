3394443c154c384955c32f5c73eb4d4c5a07c2a0
bashfulrobot <dustin@bashfulrobot.com>
2025-08-08 09:53:18 -0700

    improve: Enhance T14 Intel Gen 6 with common ThinkPad optimizations
    - Add acpi_backlight=native for proper brightness control
    - Include touchpad fix for click registration issues
    - Disable throttled service for modern Intel power management
    - Mark additions with TODO comments seeking community feedback

--- lenovo/thinkpad/t14/intel/gen6/default.nix
+++ lenovo/thinkpad/t14/intel/gen6/default.nix
@@ -17,4 +17,17 @@
 
   # Ensure modern kernel for full Lunar Lake support
   boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.8") pkgs.linuxPackages_latest;
+
+  # TODO: Looking for feedback on these kernel parameters for T14 Gen 6
+  # Force use of the thinkpad_acpi driver for backlight control
+  # This allows the backlight save/load systemd service to work
+  boot.kernelParams = [
+    "acpi_backlight=native"
+    # TODO: Looking for feedback - touchpad fix needed for proper click registration on some T14 models
+    "psmouse.synaptics_intertouch=0"
+  ];
+
+  # TODO: Looking for feedback - modern Intel CPUs don't typically need throttled service
+  # which can interfere with newer power management on Lunar Lake
+  services.throttled.enable = lib.mkDefault false;
 }
