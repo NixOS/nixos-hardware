cfb36e4feb6a9a423510260a7959eb0831461570
Tatesa Uradnik <taduradnik@gmail.com>
2025-08-27 13:04:31 +0200

    fix: add missing import of ideapad 16iah8

--- flake.nix
+++ flake.nix
@@ -166,6 +166,7 @@
           lenovo-ideapad-15ach6 = import ./lenovo/ideapad/15ach6;
           lenovo-ideapad-16ach6 = import ./lenovo/ideapad/16ach6;
           lenovo-ideapad-16ahp9 = import ./lenovo/ideapad/16ahp9;
+          lenovo-ideapad-s5-16iah8 = import ./lenovo/ideapad/16iah8;
           lenovo-ideapad-z510 = import ./lenovo/ideapad/z510;
           lenovo-ideapad-slim-5 = import ./lenovo/ideapad/slim-5;
           lenovo-ideapad-s145-15api = import ./lenovo/ideapad/s145-15api;
