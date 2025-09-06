350fd669eb1b1dd5033d79d9ea29f22c0ead0bac
Tristan Ross <tristan.ross@midstall.com>
2025-09-06 06:27:36 -0700

    Add hydra jobs & flake packages

--- default.nix
+++ default.nix
@@ -1,3 +1,7 @@
 # This file is necessary so nix-env -qa does not break,
 # when nixos-hardware is used as a channel
-{ }
+{ pkgs }:
+import ./toplevel.nix {
+  fn = p: pkgs.callPackages "${builtins.toString p}/all.nix";
+  inherit (pkgs) lib;
+}
