63224688c7a40c8fef0bb8288def57be218272c7
Gavin John <gavinnjohn@gmail.com>
2025-10-09 18:23:24 -0700

    common/gpu/intel/default: install setcap wrapper for intel gpu tools
    If I am not mistaken, this adds support for reading GPU stats as non-root, which makes sense more often that not

--- common/gpu/intel/default.nix
+++ common/gpu/intel/default.nix
@@ -91,6 +91,8 @@
         lib.optionals useIntelVaapiDriver [ intel-vaapi-driver-32 ]
         ++ lib.optionals useIntelMediaDriver [ intel-media-driver-32 ];
 
+      hardware.intel-gpu-tools.enable = true;
+
       assertions = [
         {
           assertion = (
