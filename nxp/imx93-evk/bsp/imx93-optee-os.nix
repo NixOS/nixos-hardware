688ee555decae8a61301c572c0f69d93f50a94dd
Jörg Thalheim <joerg@thalheim.io>
2025-10-29 16:03:39 +0100

    refactor(nxp): parameterize i.MX platform builders to reduce duplication
    This refactoring reduces code duplication across i.MX93, i.MX8MP, and
    i.MX8MQ platforms by extracting common build logic into parameterized
    shared builders. This makes it easier to maintain and add new i.MX
    platforms while ensuring consistency across all platforms.

--- nxp/imx93-evk/bsp/imx93-optee-os.nix
+++ nxp/imx93-evk/bsp/imx93-optee-os.nix
@@ -1,75 +1,17 @@
-{
-  lib,
-  pkgs,
-}:
-let
-  inherit (pkgs.buildPackages) python3;
-  toolchain = pkgs.gccStdenv.cc;
-  binutils = pkgs.gccStdenv.cc.bintools.bintools_bin;
-  cpp = pkgs.gcc;
-in
-pkgs.stdenv.mkDerivation rec {
+{ pkgs }:
+pkgs.callPackage ../../common/bsp/imx-optee-builder.nix {
   pname = "imx93-optee-os";
   version = "lf-6.12.3_1.0.0";
-
-  nativeBuildInputs = [
-    python3
-  ];
-
-  enableParallelBuilding = true;
-
-  propagatedBuildInputs = with python3.pkgs; [
-    pycryptodomex
-    pyelftools
-    cryptography
-  ];
 
   src = pkgs.fetchgit {
     url = "https://github.com/nxp-imx/imx-optee-os.git";
     rev = "8dd180b6d149c1e1314b5869697179f665bd9ca3";
     sha256 = "sha256-PoolRscdyeGevrOa5YymPTQ36edVvReMM5WshRTz+uk=";
   };
-  meta = with lib; {
-    homepage = "https://github.com/nxp-imx/imx-optee-os";
-    license = [ licenses.bsd2 ];
-    maintainers = with maintainers; [ govindsi ];
-    platforms = [ "aarch64-linux" ];
+
+  platformFlavor = "imx-mx93evk";
+
+  meta = {
+    maintainers = with pkgs.lib.maintainers; [ govindsi ];
   };
-
-  postPatch = ''
-    substituteInPlace scripts/arm32_sysreg.py \
-      --replace '/usr/bin/env python3' '${python3}/bin/python'
-    substituteInPlace scripts/gen_tee_bin.py \
-      --replace '/usr/bin/env python3' '${python3}/bin/python'
-    substituteInPlace scripts/pem_to_pub_c.py \
-      --replace '/usr/bin/env python3' '${python3}/bin/python'
-    substituteInPlace ta/pkcs11/scripts/verify-helpers.sh \
-      --replace '/bin/bash' '${pkgs.bash}/bin/bash'
-    substituteInPlace mk/gcc.mk \
-      --replace "\$(CROSS_COMPILE_\$(sm))objcopy" ${binutils}/bin/${toolchain.targetPrefix}objcopy
-    substituteInPlace mk/gcc.mk \
-      --replace "\$(CROSS_COMPILE_\$(sm))objdump" ${binutils}/bin/${toolchain.targetPrefix}objdump
-    substituteInPlace mk/gcc.mk \
-      --replace "\$(CROSS_COMPILE_\$(sm))nm" ${binutils}/bin/${toolchain.targetPrefix}nm
-    substituteInPlace mk/gcc.mk \
-      --replace "\$(CROSS_COMPILE_\$(sm))readelf" ${binutils}/bin/${toolchain.targetPrefix}readelf
-    substituteInPlace mk/gcc.mk \
-      --replace "\$(CROSS_COMPILE_\$(sm))ar" ${binutils}/bin/${toolchain.targetPrefix}ar
-    substituteInPlace mk/gcc.mk \
-      --replace "\$(CROSS_COMPILE_\$(sm))cpp" ${cpp}/bin/cpp
-  '';
-
-  makeFlags = [
-    "PLATFORM=imx-mx93evk"
-    "CFG_ARM64_core=y"
-    "CFG_TEE_TA_LOG_LEVEL=0"
-    "CFG_TEE_CORE_LOG_LEVEL=0"
-    "CROSS_COMPILE=${toolchain}/bin/${toolchain.targetPrefix}"
-    "CROSS_COMPILE64=${toolchain}/bin/${toolchain.targetPrefix}"
-  ];
-
-  installPhase = ''
-    mkdir -p $out
-    cp ./out/arm-plat-imx/core/tee-raw.bin $out/tee.bin
-  '';
 }
--- nxp/imx93-evk/bsp/imx93-uboot.nix
+++ nxp/imx93-evk/bsp/imx93-uboot.nix
@@ -1,93 +1,17 @@
-{
-  stdenv,
-  lib,
-  bison,
-  dtc,
-  fetchgit,
-  flex,
-  gnutls,
-  libuuid,
-  ncurses,
-  openssl,
-  which,
-  perl,
-  buildPackages,
-  efitools,
-}:
-let
-  ubsrc = fetchgit {
+{ pkgs, fetchgit }:
+pkgs.callPackage ../../common/bsp/imx-uboot-builder.nix {
+  pname = "imx93-uboot";
+  version = "2024.04";
+
+  src = fetchgit {
     url = "https://github.com/nxp-imx/uboot-imx.git";
     #lf_v2024.04
     rev = "e3219a5a73445219df605d1492687918d488055c";
     sha256 = "sha256-6pXwgNzq4/XUUEmJ6sGC5pII4J5uMvlDPE9QJxjJJbQ=";
   };
-in
-stdenv.mkDerivation {
-  pname = "imx93-uboot";
-  version = "2024.04";
-  src = ubsrc;
 
-  postPatch = ''
-    patchShebangs tools
-    patchShebangs scripts
-  '';
-
-  nativeBuildInputs = [
-    bison
-    flex
-    openssl
-    which
-    ncurses
-    libuuid
-    gnutls
-    openssl
-    perl
-    efitools
-  ];
-
-  depsBuildBuild = [ buildPackages.stdenv.cc ];
-  hardeningDisable = [ "all" ];
-  enableParallelBuilding = true;
-
-  makeFlags = [
-    "DTC=${lib.getExe buildPackages.dtc}"
-    "CROSS_COMPILE=${stdenv.cc.targetPrefix}"
-  ];
-
-  extraConfig = ''
-    CONFIG_USE_BOOTCOMMAND=y
-    CONFIG_BOOTCOMMAND="setenv ramdisk_addr_r 0x85000000; setenv fdt_addr_r 0x84000000; run distro_bootcmd; "
-    CONFIG_CMD_BOOTEFI_SELFTEST=y
-    CONFIG_CMD_BOOTEFI=y
-    CONFIG_EFI_LOADER=y
-    CONFIG_BLK=y
-    CONFIG_PARTITIONS=y
-    CONFIG_DM_DEVICE_REMOVE=n
-    CONFIG_CMD_CACHE=y
-  '';
-
-  passAsFile = [ "extraConfig" ];
-
-  configurePhase = ''
-    runHook preConfigure
-
-    make imx93_11x11_evk_defconfig
-    cat $extraConfigPath >> .config
-
-    runHook postConfigure
-  '';
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out
-    cp ./u-boot-nodtb.bin $out
-    cp ./spl/u-boot-spl.bin $out
-    cp ./arch/arm/dts/imx93-11x11-evk.dtb $out
-    cp .config  $out
-
-    runHook postInstall
-  '';
-
-  dontStrip = true;
+  defconfig = "imx93_11x11_evk_defconfig";
+  ramdiskAddr = "0x85000000";
+  fdtAddr = "0x84000000";
+  dtbPath = "./arch/arm/dts/imx93-11x11-evk.dtb";
 }
