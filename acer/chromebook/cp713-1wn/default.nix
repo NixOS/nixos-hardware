6c870034bfa7d6dfc6d2185c2faad3c7a9ca05bd
Boom-Hacker <darksun0031@gmail.com>
2025-11-02 23:45:01 +0800

    Revert "fixed bind"
    This reverts commit 9cd06b13d0c4f08879731df5ea4568548bd9b9ad.

--- acer/chromebook/cp713-1wn/default.nix
+++ acer/chromebook/cp713-1wn/default.nix
@@ -28,7 +28,7 @@ in {
   keyboards = {
     # The name is just the name of the configuration file, it does not really matter
     default = {
-      ids = [ "k:n:AT Translated Set 2 keyboard" ]; # what goes into the [id] section, here we select all keyboards
+      ids = [ "*" ]; # what goes into the [id] section, here we select all keyboards
       # Everything but the ID section:
       settings = {
         # The main layer, if you choose to declare it in Nix
