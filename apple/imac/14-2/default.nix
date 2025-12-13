468db525364cf66bcbe8b10c13b301d6e2a9bd41
masrlinu <5259918+masrlinu@users.noreply.github.com>
2025-12-13 02:11:50 +0100

    broadcom-wl: introduce common/broadcom-wifi.nix and import in Apple/Dell profiles

--- apple/imac/14-2/default.nix
+++ apple/imac/14-2/default.nix
@@ -11,26 +11,8 @@
     ../../../common/gpu/nvidia
     ../../../common/gpu/nvidia/kepler
     ../../../common/hidpi.nix
+    ../../../common/broadcom-wifi.nix
   ];
-
-  options = {
-    hardware.broadcom.wifi.enableLegacyDriverWithKnownVulnerabilities = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = ''
-        Enable the legacy Broadcom WiFi driver (wl) with known security vulnerabilities.
-
-        This driver is vulnerable to heap buffer overflows:
-        - CVE-2019-9501 (https://github.com/advisories/GHSA-vjw8-c937-7hwp)
-        - CVE-2019-9502 (https://github.com/advisories/GHSA-4rfg-8q34-prmp)
-
-        Attackers within WiFi range can exploit this vulnerability by sending crafted
-        WiFi packets, even without being connected to the same network. Simply having
-        WiFi enabled makes the system vulnerable to arbitrary code execution or denial-of-service.
-        Only enable if no alternative WiFi solution is available.
-      '';
-    };
-  };
 
   config = {
     boot = {
@@ -49,15 +31,6 @@
         "bcma"
       ];
       kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.0") pkgs.linuxPackages_latest;
-      extraModulePackages =
-        lib.mkIf config.hardware.broadcom.wifi.enableLegacyDriverWithKnownVulnerabilities
-          [
-            (config.boot.kernelPackages.broadcom_sta.overrideAttrs (oldAttrs: {
-              meta = oldAttrs.meta // {
-                knownVulnerabilities = [ ];
-              };
-            }))
-          ];
     };
 
     hardware = {
