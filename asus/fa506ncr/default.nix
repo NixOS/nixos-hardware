33828e593974863a56d7cf80bbc2b282cfc3a359
0x0D <0xOD@proton.me>
2025-12-26 23:48:21 +0100

    Add ASUS TUF Gaming A15 FA506NCR support with configuration files

---
+++ asus/fa506ncr/default.nix
@@ -0,0 +1,15 @@
+{ config, lib, ... }: {
+  imports = [
+    ../../common/cpu/amd
+    ../../common/cpu/amd/pstate.nix
+    ../../common/gpu/nvidia
+    ../../common/pc/laptop
+    ../../common/pc/ssd
+    ../battery.nix
+  ];
+
+  hardware.nvidia = {
+    modesetting.enable = lib.mkDefault true;
+    open = lib.mkIf (lib.versionAtLeast config.hardware.nvidia.package.version "555") true;
+  };
+}
