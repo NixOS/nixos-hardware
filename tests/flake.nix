6366bcfdab95ed6b2d362fc43d67bde1644c1843
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-01-11 09:37:40 +0100

    tests: update stable nixpkgs input to 25.11

--- tests/flake.nix
+++ tests/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixos-unstable-small.url = "github:NixOS/nixpkgs/nixos-unstable-small";
-    nixos-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixos-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixos-unstable-small";
   };
