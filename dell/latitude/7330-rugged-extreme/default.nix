7c5532865960929e02e1a85bb6e66ea8b034f5b2
gitman-101111 <development@spamfolder.xyz>
2026-01-14 15:04:37 -0600

    fix: Use nixfmt to fix formatting

--- dell/latitude/7330-rugged-extreme/default.nix
+++ dell/latitude/7330-rugged-extreme/default.nix
@@ -2,7 +2,8 @@
   lib,
   pkgs,
   ...
-}: {
+}:
+{
   imports = [
     ../../../common/cpu/intel
     ../../../common/pc/laptop
@@ -12,27 +13,49 @@
   boot = {
     initrd = {
       # From OEM Ubuntu Image
-      availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "rtsx_pci_sdmmc"];
-      kernelModules = lib.mkDefault [];
+      availableKernelModules = [
+        "xhci_pci"
+        "thunderbolt"
+        "nvme"
+        "rtsx_pci_sdmmc"
+      ];
+      kernelModules = lib.mkDefault [ ];
     };
-    kernelParams = lib.mkDefault ["mem_sleep_default=deep" "nowatchdog" "i915.enable_guc=3"];
-    kernelModules = ["kvm-intel"];
+    kernelParams = lib.mkDefault [
+      "mem_sleep_default=deep"
+      "nowatchdog"
+      "i915.enable_guc=3"
+    ];
+    kernelModules = [ "kvm-intel" ];
     # r8169 is from OEM Ubuntu Image
-    blacklistedKernelModules = ["r8169" "iTCO_wdt" "intel_oc_wdt"];
-    extraModulePackages = lib.mkDefault [];
+    blacklistedKernelModules = [
+      "r8169"
+      "iTCO_wdt"
+      "intel_oc_wdt"
+    ];
+    extraModulePackages = lib.mkDefault [ ];
     # Need 6.1 at the minimum to ensure reliable Wi-Fi
     kernelPackages = lib.mkDefault (
-      if lib.versionOlder pkgs.linuxPackages.kernel.version "6.1"
-      then pkgs.linuxPackages_6_1
-      else pkgs.linuxPackages
+      if lib.versionOlder pkgs.linuxPackages.kernel.version "6.1" then
+        pkgs.linuxPackages_6_1
+      else
+        pkgs.linuxPackages
     );
   };
 
   # Necessary for accurate dual-battery power-level reading after wake
   systemd.services.upower-resume = {
     description = "Restart UPower after resume to refresh battery stats";
-    wantedBy = ["suspend.target" "hibernate.target" "hybrid-sleep.target"];
-    after = ["systemd-suspend.service" "systemd-hibernate.service" "systemd-hybrid-sleep.service"];
+    wantedBy = [
+      "suspend.target"
+      "hibernate.target"
+      "hybrid-sleep.target"
+    ];
+    after = [
+      "systemd-suspend.service"
+      "systemd-hibernate.service"
+      "systemd-hybrid-sleep.service"
+    ];
     serviceConfig = {
       Type = "oneshot";
       ExecStart = "${pkgs.systemd}/bin/systemctl restart upower";
