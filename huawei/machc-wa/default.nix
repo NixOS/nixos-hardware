0496a800a15379b437092570fc3e67d71877b630
Emre Cebi <emre@cebi.io>
2026-01-30 03:00:13 +0100

    machc-wa: updated the config to match up with the current common modules

--- huawei/machc-wa/default.nix
+++ huawei/machc-wa/default.nix
@@ -7,22 +7,19 @@
 {
   imports = [
     ../../common/cpu/intel/comet-lake
-    ../../common/gpu/nvidia
+    ../../common/gpu/intel/comet-lake
+    ../../common/gpu/nvidia/pascal
     ../../common/gpu/nvidia/prime.nix
-    ../../common/hidpi.nix
     ../../common/pc/laptop
     ../../common/pc/ssd
   ];
 
   hardware.nvidia = {
     modesetting.enable = lib.mkDefault true;
-    open = lib.mkDefault false;
     nvidiaSettings = lib.mkDefault true;
     prime = {
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
   };
-
-  services.power-profiles-daemon.enable = lib.mkDefault true;
 }
