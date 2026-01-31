40439a9a4206382647276d16d80b0b1b9cb4a6b6
Cody McGinnis <cody@mcginnis.dev>
2026-01-31 22:59:31 +0000

    use mkMerge for better formatting

--- lenovo/thinkpad/x13s/default.nix
+++ lenovo/thinkpad/x13s/default.nix
@@ -9,8 +9,6 @@ let
   deviceTree = config.hardware.deviceTree;
 
   cfg = config.hardware.lenovo.x13s;
-
-  inherit (lib) mkDefault;
 in
 {
   imports = [
@@ -40,125 +38,164 @@ in
     };
   };
 
-  config = {
-    boot = {
-      loader.systemd-boot.extraFiles = {
-        "dtbs/${deviceTree.kernelPackage.version}" = "${deviceTree.package}";
-      };
+  config = lib.mkMerge ([
 
-      kernelParams = mkDefault [
-        # needed to boot
-        "dtb=dtbs/${deviceTree.kernelPackage.version}/${deviceTree.name}"
-
-        # jhovold recommended
-        "clk_ignore_unused"
-        "pd_ignore_unused"
-        "arm64.nopauth"
-      ];
-
-      kernelModules = mkDefault [
-        "nvme"
-        "phy-qcom-qmp-pcie"
-        "pcie-qcom"
-
-        "i2c-core"
-        "i2c-hid"
-        "i2c-hid-of"
-        "i2c-qcom-geni"
-
-        "leds_qcom_lpg"
-        "pwm_bl"
-        "qrtr"
-        "pmic_glink_altmode"
-        "gpio_sbu_mux"
-        "phy-qcom-qmp-combo"
-        "gpucc_sc8280xp"
-        "dispcc_sc8280xp"
-        "phy_qcom_edp"
-        "panel-edp"
-        "msm"
-      ];
-    };
-
-    hardware = {
-      enableRedistributableFirmware = mkDefault true;
-      deviceTree = {
-        enable = true;
-        filter = lib.mkDefault "sc8280xp-lenovo-thinkpad-x13s*.dtb";
-        name = lib.mkDefault "qcom/sc8280xp-lenovo-thinkpad-x13s.dtb";
-      };
-    };
-
-    # https://github.com/jhovold/linux/wiki/X13s#bluetooth
-    systemd.services.bluetooth-x13s-mac = {
-      wantedBy = [ "multi-user.target" ];
-      before = [ "bluetooth.service" ];
-      requiredBy = [ "bluetooth.service" ];
-
-      script = ''
-        BLUETOOTH_MAC="${if cfg.bluetoothMac == null then "" else cfg.bluetoothMac}"
-
-        if [ "$BLUETOOTH_MAC" = "" ] ; then
-          # we might be able to use the system serial number but, if we lost machine-id
-          # the system has probably lost the bluetooth device keys anyway
-          RANDOM=$(( $(cat /etc/machine-id | head -c 128 | sed -e 's/[^0-9]//g') % 32767 ))
-
-          # https://datatracker.ietf.org/doc/html/rfc7042#section-2.1
-          # > Two bits within the initial octet of an EUI-48 have special
-          # > significance in MAC addresses: the Group bit (01) and the Local bit
-          # > (02).  OUIs and longer MAC prefixes are assigned with the Local bit
-          # > zero and the Group bit unspecified.  Multicast identifiers may be
-          # > constructed by turning on the Group bit, and unicast identifiers may
-          # > be constructed by leaving the Group bit zero.
-          #
-          # First, and only argument is for passing a file to seed RANDOM.
-          # recommend using `/etc/machine-id` to pin the mac address if needed.
-          
-          BLUETOOTH_MAC="$(printf '%X%X:%02X:%02X:%02X:%02X:%02X' \
-            $[RANDOM%16] $[((RANDOM%4)+1)*4-2] \
-            $[RANDOM%256] \
-            $[RANDOM%256] \
-            $[RANDOM%256] \
-            $[RANDOM%256] \
-            $[RANDOM%256])"
-        fi
-
-        ${pkgs.bluez}/bin/btmgmt --index 0 public-addr $BLUETOOTH_MAC
+    # WiFi
+    (lib.mkIf (cfg.wifiMac != null) {
+      # https://github.com/jhovold/linux/wiki/X13s#wi-fi
+      services.udev.extraRules = ''
+        ACTION=="add", SUBSYSTEM=="net", KERNELS=="0006:01:00.0", RUN+="${pkgs.iproute2}/bin/ip link set dev $name address ${cfg.wifiMac}"
       '';
+    })
 
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
+    # Bluetooth
+    {
+      # https://github.com/jhovold/linux/wiki/X13s#bluetooth
+      systemd.services.bluetooth-x13s-mac = {
+        wantedBy = [ "multi-user.target" ];
+        before = [ "bluetooth.service" ];
+        requiredBy = [ "bluetooth.service" ];
+
+        script = ''
+          BLUETOOTH_MAC="${if cfg.bluetoothMac == null then "" else cfg.bluetoothMac}"
+
+          if [ "$BLUETOOTH_MAC" = "" ] ; then
+            # we might be able to use the system serial number but, if we lost machine-id
+            # the system has probably lost the bluetooth device keys anyway
+            RANDOM=$(( $(cat /etc/machine-id | head -c 128 | sed -e 's/[^0-9]//g') % 32767 ))
+
+            # https://datatracker.ietf.org/doc/html/rfc7042#section-2.1
+            # > Two bits within the initial octet of an EUI-48 have special
+            # > significance in MAC addresses: the Group bit (01) and the Local bit
+            # > (02).  OUIs and longer MAC prefixes are assigned with the Local bit
+            # > zero and the Group bit unspecified.  Multicast identifiers may be
+            # > constructed by turning on the Group bit, and unicast identifiers may
+            # > be constructed by leaving the Group bit zero.
+            #
+            # First, and only argument is for passing a file to seed RANDOM.
+            # recommend using `/etc/machine-id` to pin the mac address if needed.
+            
+            BLUETOOTH_MAC="$(printf '%X%X:%02X:%02X:%02X:%02X:%02X' \
+              $[RANDOM%16] $[((RANDOM%4)+1)*4-2] \
+              $[RANDOM%256] \
+              $[RANDOM%256] \
+              $[RANDOM%256] \
+              $[RANDOM%256] \
+              $[RANDOM%256])"
+          fi
+
+          ${pkgs.bluez}/bin/btmgmt --index 0 public-addr $BLUETOOTH_MAC
+        '';
+
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+        };
       };
-    };
+    }
 
-    # https://github.com/jhovold/linux/wiki/X13s#modem
-    networking.modemmanager.fccUnlockScripts = [
+    # Modem
+    {
+      # https://github.com/jhovold/linux/wiki/X13s#modem
+      networking.modemmanager.fccUnlockScripts = [
+        {
+          id = "105b:e0c3";
+          path = "${pkgs.modemmanager}/share/ModemManager/fcc-unlock.available.d/105b";
+        }
+      ];
+    }
 
-      {
-        id = "105b:e0c3";
-        path = "${pkgs.modemmanager}/share/ModemManager/fcc-unlock.available.d/105b";
-      }
-    ];
+    # Camera
+    {
+      # https://github.com/jhovold/linux/wiki/X13s#camera
+      services.udev.extraRules = ''
+        ACTION=="add", SUBSYSTEM=="dma_heap", KERNEL=="linux,cma", GROUP="video", MODE="0660"
+        ACTION=="add", SUBSYSTEM=="dma_heap", KERNEL=="system", GROUP="video", MODE="0660"
+      '';
+    }
 
-    # https://github.com/jhovold/linux/wiki/X13s#camera
-    services.udev.extraRules = lib.strings.concatLines (
-      [
-        ''
-          ACTION=="add", SUBSYSTEM=="dma_heap", KERNEL=="linux,cma", GROUP="video", MODE="0660"
-          ACTION=="add", SUBSYSTEM=="dma_heap", KERNEL=="system", GROUP="video", MODE="0660"
-        ''
-      ]
-      ++ (
-        if cfg.wifiMac != null then
-          [
-            ''
-              ACTION=="add", SUBSYSTEM=="net", KERNELS=="0006:01:00.0", RUN+="${pkgs.iproute2}/bin/ip link set dev $name address ${cfg.wifiMac}"
-            ''
-          ]
-        else
-          [ ]
+    # Minimum requirements for a running system
+    {
+      hardware = {
+        enableRedistributableFirmware = lib.mkDefault true;
+        deviceTree = {
+          enable = true;
+          filter = lib.mkDefault "sc8280xp-lenovo-thinkpad-x13s*.dtb";
+          name = lib.mkDefault "qcom/sc8280xp-lenovo-thinkpad-x13s.dtb";
+        };
+      };
+
+      boot = {
+        # https://github.com/jhovold/linux/wiki/X13s#kernel-command-line
+        kernelParams = [
+          "clk_ignore_unused"
+          "pd_ignore_unused"
+          "arm64.nopauth"
+          # "efi=noruntime"
+        ];
+
+        kernelModules = [
+          "nvme"
+          "phy-qcom-qmp-pcie"
+          # "pcie-qcom" # this is no longer a module
+
+          "i2c-core"
+          "i2c-hid"
+          "i2c-hid-of"
+          "i2c-qcom-geni"
+
+          "leds_qcom_lpg"
+          "pwm_bl"
+          "qrtr"
+          "pmic_glink_altmode"
+          "gpio_sbu_mux"
+          "phy-qcom-qmp-combo"
+          "gpucc_sc8280xp"
+          "dispcc_sc8280xp"
+          "phy_qcom_edp"
+          "panel-edp"
+          "msm"
+        ];
+      };
+    }
+
+    # boot loader needs to know about and handle the dtb for the system. systemd-boot handles this
+    # automagically when hardware.deviceTree.enable is true since 24.11
+    (lib.mkIf
+      (
+        config.boot.loader.systemd-boot.enable
+        && deviceTree.enable
+        && (lib.versionOlder config.system.stateVersion "24.11")
       )
-    );
-  };
+      {
+        boot = {
+          loader.systemd-boot.extraFiles = {
+            "dtbs/${deviceTree.kernelPackage.version}" = "${deviceTree.package}";
+          };
+
+          kernelParams = [
+            "dtb=dtbs/${deviceTree.kernelPackage.version}/${deviceTree.name}"
+          ];
+        };
+      }
+    )
+
+    # grub does not handle device tree yet
+    # (lib.mkIf (config.boot.loader.grub.enable) {
+    #   warnings = [
+    #     "x13s grub configuration has not been tested"
+    #   ];
+    #   boot = {
+    #     loader.grub = {
+    #       extraConfig = ''
+    #         devicetree /dtbs/${config.hardware.deviceTree.kernelPackage.version}/${config.hardware.deviceTree.name}
+    #       '';
+    #       extraFiles = {
+    #         "dtbs/${config.hardware.deviceTree.kernelPackage.version}" = "${config.hardware.deviceTree.package}";
+    #       };
+    #     };
+    #   };
+    # })
+
+  ]);
 }
