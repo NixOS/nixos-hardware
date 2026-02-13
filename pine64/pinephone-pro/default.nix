6b7ef2e7d7337e88cf91ae17b73410c63a39fa6c
Justin Lovinger <git@justinlovinger.com>
2026-02-13 04:46:15 +0000

    pine64/pinephone-pro: fix audio

--- pine64/pinephone-pro/default.nix
+++ pine64/pinephone-pro/default.nix
@@ -1,24 +1,14 @@
 {
   pkgs,
-  lib,
   config,
   ...
 }:
-let
-  cfg = config.hardware.pinephone-pro;
-in
 {
-  imports = [ ./wifi.nix ];
-  options.hardware.pinephone-pro = {
-    install-ucm2-rules = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = ''
-        Whether to install PinePhone Pro specific UCM2 rules.
-        These provide audio usecaseses for HiFi audio and Voice calls.
-      '';
-    };
-  };
+  imports = [
+    ./audio.nix
+    ./wifi.nix
+  ];
+
   config = {
     nixpkgs.overlays = [
       (final: _prev: {
@@ -27,8 +17,8 @@ in
         linuxPackages_pinephone-pro = pkgs.callPackage ./kernel { inherit (config.boot) kernelPatches; };
       })
     ];
+
     hardware.firmware = [ pkgs.firmware_pinephone-pro ];
-    environment.systemPackages = lib.optionals cfg.install-ucm2-rules [ pkgs.pine64-alsa-ucm ];
 
     boot = {
       kernelPackages = pkgs.linuxPackagesFor pkgs.linuxPackages_pinephone-pro;
