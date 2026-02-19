8caf62ccaec4c5e17a6ba6e457edc3e17e879271
Justin Lovinger <git@justinlovinger.com>
2026-02-19 22:13:44 +0000

    pine64/pinephone-pro: fix for treefmt-check

--- pine64/pinephone-pro/kernel/source.nix
+++ pine64/pinephone-pro/kernel/source.nix
@@ -5,7 +5,7 @@
   src,
   upstream_patch,
   ...
-}@args:
+}:
 # Applies the full patch since last mainline which contains some previous patches causing it to fail
 # which will work here as the builder doesn't use "set -e"
 stdenv.mkDerivation {
