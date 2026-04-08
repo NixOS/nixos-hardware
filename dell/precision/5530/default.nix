e1a65f5c7e6e11db9ae9c17e137ea8dbb6717f00
adjoly <contact@adjoly.fr>
2026-04-08 15:20:57 +0200

    format(dell/precision/5530): formatted code

--- dell/precision/5530/default.nix
+++ dell/precision/5530/default.nix
@@ -27,11 +27,11 @@
     ];
     initrd.availableKernelModules = [
       "xhci_pci"
-        "ahci"
-        "nvme"
-        "usb_storage"
-        "sd_mod"
-        "rtsx_pci_sdmmc"
+      "ahci"
+      "nvme"
+      "usb_storage"
+      "sd_mod"
+      "rtsx_pci_sdmmc"
     ];
   };
 
