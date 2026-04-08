dea068a25ac2945e6cc88557f4c2ac7ce782fd98
Daniel Schaefer <dhs@frame.work>
2026-04-08 09:16:57 +0800

    framework: ultra3: Allow unl0kr in initrd
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework/13-inch/intel-core-ultra-series3/default.nix
+++ framework/13-inch/intel-core-ultra-series3/default.nix
@@ -18,4 +18,13 @@
   boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.17") (
     lib.mkDefault pkgs.linuxPackages_latest
   );
+
+  # Additional modules for touchscreen/touchpad in initrd (for unl0kr on-screen keyboard)
+  boot.initrd.kernelModules = lib.optionals config.boot.initrd.unl0kr.enable [
+    "intel_lpss_pci"
+    "i2c_hid_acpi"
+    "i2c_hid"
+    "hid_multitouch"
+    "hid_generic"
+  ];
 }
