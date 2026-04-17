2e0fc10355a00f2b6120e12ca01df43349650bf0
Nikita Samusev <nikitf777@gmail.com>
2026-04-17 19:23:56 +0300

    Add config for HP Victus 16-e0105nw

--- flake.nix
+++ flake.nix
@@ -209,6 +209,7 @@
           hp-laptop-15s-fq1xxx = import ./hp/laptop/15s-fq1xxx;
           huawei-machc-wa = import ./huawei/machc-wa;
           hp-notebook-14-df0023 = import ./hp/notebook/14-df0023;
+          hp-victus-16-e0105nw = import ./hp/victus/16-e0105nw;
           intel-nuc-5i5ryb = import ./intel/nuc/5i5ryb;
           intel-nuc-8i7beh = import ./intel/nuc/8i7beh;
           intel-nuc-12wshi7 = import ./intel/nuc/12wshi7;
