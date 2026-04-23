73ce456dad3a7d35be1d88e52de788aab24d70e2
Carl Zulauf <carl@unabridgedsoftware.com>
2026-04-22 21:59:18 -0500

    dell/xps/139350: Enable fprintd by default

--- dell/xps/13-9350/default.nix
+++ dell/xps/13-9350/default.nix
@@ -10,6 +10,11 @@
   # The touchpad uses I²C, so PS/2 is unnecessary
   boot.blacklistedKernelModules = [ "psmouse" ];
 
+  # Enable fingerprint sensor located on the power button.
+  # Works out of the box with fprintd and no additional packages/drivers.
+  # Configure for the current user via `fprintd-enroll`.
+  services.fprintd.enable = lib.mkDefault true;
+
   # Recommended in NixOS/nixos-hardware#127
   services.thermald.enable = lib.mkDefault true;
 }
