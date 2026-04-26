ec38c9c3f52572b7182a79336f35782a987242ac
langsjo <104687438+langsjo@users.noreply.github.com>
2026-04-27 00:36:36 +0300

    nvidia/pascal: use legacy_580 drivers by default
    590 and higher have dropped support for Pascal

--- common/gpu/nvidia/pascal/default.nix
+++ common/gpu/nvidia/pascal/default.nix
@@ -1,7 +1,8 @@
-{ lib, ... }:
+{ config, lib, ... }:
 {
   imports = [ ../. ];
 
   # The open source driver does not support Pascal GPUs.
   hardware.nvidia.open = false;
+  hardware.nvidia.package = lib.mkDefault config.boot.kernelPackages.nvidiaPackages.legacy_580;
 }
