f5eb46290a0dbc9bfdda54e56223c391731313f3
Utkarsh Gupta <utkarshgupta137@gmail.com>
2026-04-28 02:24:36 +0100

    legion/16arha7: fix fprint driver

--- lenovo/legion/16arha7/default.nix
+++ lenovo/legion/16arha7/default.nix
@@ -33,7 +33,7 @@ in
     package = pkgs.fprintd-tod;
     tod = {
       enable = true;
-      driver = pkgs.libfprint-2-tod1-elan;
+      driver = pkgs.libfprint-2-tod1-goodix-550a;
     };
   };
 }
