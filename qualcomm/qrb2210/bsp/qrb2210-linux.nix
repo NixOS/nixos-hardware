73a8689b521bca2c5d08f5e3cdaaee58275c4523
Govind Singh <govind.sk85@gmail.com>
2026-06-02 11:51:57 +0400

    qcom: qrb2210: add Linux kernel package
    Signed-off-by: Govind Singh <govind.sk85@gmail.com>

---
+++ qualcomm/qrb2210/bsp/qrb2210-linux.nix
@@ -0,0 +1,33 @@
+{
+  buildLinux,
+  fetchurl,
+  lib,
+  ...
+}@args:
+
+buildLinux (
+  args
+  // rec {
+    pname = "qrb2210-linux";
+    version = "7.0";
+    modDirVersion = "7.0.0";
+
+    src = fetchurl {
+      url = "https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-${version}.tar.xz";
+      hash = "sha256-u39tgLOHx1e30Uu5MCj8uQ95PFwNNnc27oFaEAs4kfA=";
+    };
+
+    defconfig = "defconfig";
+    extraConfig = builtins.readFile ./linux-qrb2210-extra-config;
+    ignoreConfigErrors = true;
+    autoModules = false;
+
+    meta = {
+      description = "Linux kernel for Qualcomm QRB2210/QCM2290 boards";
+      homepage = "https://kernel.org";
+      license = lib.licenses.gpl2Only;
+      platforms = [ "aarch64-linux" ];
+    };
+  }
+)
+
