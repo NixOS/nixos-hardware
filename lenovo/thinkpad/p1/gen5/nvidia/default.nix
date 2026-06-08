f9fbbd15c3219bce06b4812160377cc6c27550cc
Alex Kraker <alex@alexkraker.com>
2026-06-08 11:47:27 -0500

    Add support for ThinkPad Gen 5 Nvidia
    Add support for ThinkPad Gen 5 with optional Nvidia GPU. This gen uses _ampere_
    microarchitecture, so we import that module. We set defaults for the
    Intel and Nvidia hardware bus ID's based on my own system which is this
    hardware configuration.

---
+++ lenovo/thinkpad/p1/gen5/nvidia/default.nix
@@ -0,0 +1,24 @@
+{ lib, ... }:
+{
+  imports = [
+    ../../.
+    ../../../../../common/gpu/nvidia/ampere
+    ../../../../../common/gpu/nvidia/prime.nix
+  ];
+
+  hardware = {
+    graphics = {
+      enable = lib.mkDefault true;
+      enable32Bit = lib.mkDefault true;
+    };
+    nvidia = {
+      modesetting.enable = lib.mkDefault true;
+      prime = {
+        # Bus ID of the Intel GPU.
+        intelBusId = lib.mkDefault "PCI:0:2:0";
+        # Bus ID of the NVIDIA GPU.
+        nvidiaBusId = lib.mkDefault "PCI:1:0:0";
+      };
+    };
+  };
+}
