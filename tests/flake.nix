969f1316449d6551cbf4b7e4be72309ac2bb02bf
Jamie Magee <jamie.magee@gmail.com>
2026-06-13 19:57:53 -0700

    tests: bump nixos-stable from 25.11 to 26.05

--- tests/flake.nix
+++ tests/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixos-unstable-small.url = "https://channels.nixos.org/nixos-unstable-small/nixexprs.tar.xz";
-    nixos-stable.url = "https://channels.nixos.org/nixos-25.11/nixexprs.tar.xz";
+    nixos-stable.url = "https://channels.nixos.org/nixos-26.05/nixexprs.tar.xz";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixos-unstable-small";
   };
