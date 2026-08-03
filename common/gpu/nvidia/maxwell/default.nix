ee14af972b61433e95d5b663f3d9c96ddd8695fa
Chris Dell <cjdell@gmail.com>
2026-08-03 01:26:22 +0100

    Add Dell Precision 5520 (based on 5530)

--- common/gpu/nvidia/maxwell/default.nix
+++ common/gpu/nvidia/maxwell/default.nix
@@ -1,7 +1,16 @@
-{ lib, ... }:
+{ lib, config, ... }:
 {
   imports = [ ../. ];
 
   # The open source driver does not support Maxwell GPUs.
   hardware.nvidia.open = false;
+
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "580.126.18";
+    sha256_64bit = "sha256-p3gbLhwtZcZYCRTHbnntRU0ClF34RxHAMwcKCSqatJ0=";
+    sha256_aarch64 = "sha256-p3gbLhwtZcZYCRTHbnntRU0ClF34RxHAMwcKCSqatJ0=";
+    openSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+    settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+    persistencedSha256 = lib.fakeSha256;
+  };
 }
