a77eb1da4078c414485db27161ba4b79f10bd17c
Rudolf Tucek <tucek.rudolf@gmail.com>
2026-08-09 01:30:25 +0200

    Apply conditional kernel loading to other devices too

--- tuxedo/infinitybook/pro14/gen10/default.nix
+++ tuxedo/infinitybook/pro14/gen10/default.nix
@@ -4,8 +4,10 @@
     ../../.
   ];
 
-  # Add Motorcomm YT6801 Driver if available
+  # Add Motorcomm YT6801 Driver if available and kernel version is below 7.0
   boot.extraModulePackages =
     with config.boot;
-    lib.lists.optional (kernelPackages ? yt6801) kernelPackages.yt6801;
+    lib.lists.optional (
+      (lib.versionOlder kernelPackages.kernel.version "7.0") && (kernelPackages ? yt6801)
+    ) kernelPackages.yt6801;
 }
