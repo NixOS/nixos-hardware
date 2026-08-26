89f684a86ede9fe42d58b41da509716f63703dd3
lks-hrsch <lkshrsch2001@outlook.com>
2026-08-26 02:55:24 +0200

    feat: add lenovo thinkpad e590 module

---
+++ lenovo/thinkpad/e590/default.nix
@@ -0,0 +1,15 @@
+{ ... }:
+
+{
+  imports = [
+    ../.
+    ../../../common/cpu/intel/whiskey-lake
+    ../../../common/pc/ssd
+  ];
+
+  boot.kernelParams = [
+    # Force use of the thinkpad_acpi driver for backlight control.
+    # This allows the backlight save/load systemd service to work.
+    "acpi_backlight=native"
+  ];
+}
